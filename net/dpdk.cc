scylladb/seastar

commit 86a9aa162222e93a8d51c8e089ec1978c6d981a9

dpdk: check the resulting cluster for non-i40e NICs
There is a maximum size of a cluster for non-i40e NICs too.
Check the resulting cluster to span on no more than max_frags
fragments.

Signed-off-by: Vlad Zolotarov <vladz@cloudius-systems.com>

net/dpdk.cc
@@ -652,7 +652,8 @@ build_mbuf_cluster:
             //    - Linearize the packet.
             //    - Build the cluster once again
             //
-            if (p.nr_frags() > 1 && qp.port().is_i40e_device() && i40e_should_linearize(head)) {
+            if (p.nr_frags() > max_frags ||
+                (p.nr_frags() > 1 && qp.port().is_i40e_device() && i40e_should_linearize(head))) {
                 me(head)->recycle();
                 p.linearize();
                 ++qp._stats.tx.linearized;

commit 4183a878029f489bdc85130a93be18c242351748

net::dpdk: actually check the resulting cluster and not the original packet
Fix a typo introduced in the commit 86a9aa162222e93a8d51c8e089ec1978c6d981a9.

Signed-off-by: Vlad Zolotarov <vladz@cloudius-systems.com>

net/dpdk.cc
@@ -652,7 +652,7 @@ build_mbuf_cluster:
             //    - Linearize the packet.
             //    - Build the cluster once again
             //
-            if (p.nr_frags() > max_frags ||
+            if (head->nb_segs > max_frags ||
                 (p.nr_frags() > 1 && qp.port().is_i40e_device() && i40e_should_linearize(head))) {
                 me(head)->recycle();
                 p.linearize();