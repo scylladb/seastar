0db706af157088f03ef56c06bcb4febb6ce55cb0
Avi Kivity <avi@cloudius-systems.com>
2015-09-03 18:54:41 +0300

    fstream: don't trim requested buffer size unnecessarily
    In order to avoid short reads that confuse the read-ahead logic,
    file_data_source_impl trims the buffer size on unaligned reads.  However
    it trimmed the buffer size based on the requested buffer size, rather than
    file::dma_alignment.  This resulted in a shorter read, which caused an extra
    I/O.  Normally it doesn't matter, but for very short streams it can double
    the amount of I/O.
    
    Fix by aligning to file::dma_alignment instead.

--- core/fstream.cc
+++ core/fstream.cc
@@ -65,7 +65,7 @@ private:
         while (_read_buffers.size() < ra) {
             ++_reads_in_progress;
             // if _pos is not dma-aligned, we'll get a short read.  Account for that.
-            auto now = _options.buffer_size - _pos % _options.buffer_size;
+            auto now = _options.buffer_size - _pos % file::dma_alignment;
             _read_buffers.push_back(_file.dma_read_bulk<char>(_pos, now).then_wrapped(
                     [this] (future<temporary_buffer<char>> ret) {
                 issue_read_aheads();
