scylladb/seastar

commit 8f0a2a795a60fc086dfa251afe45e13061001281

stream: properly set exception
The try / catch block is a thing of the past: it doesn't really work with
futures. The proper way to do it is with then_wrapped.

Signed-off-by: Glauber Costa <glommer@cloudius-systems.com>

core/stream.hh
@@ -158,13 +158,15 @@ template <typename... T>
 inline
 future<>
 stream<T...>::produce(T... data) {
-    try {
-        return _sub->_next(std::move(data)...);
-    } catch (...) {
-        _done.set_exception(std::current_exception());
-        // FIXME: tell the producer to stop producing
-        abort();
-    }
+    return _sub->_next(std::move(data)...).then_wrapped([this] (future<> f) {
+        try {
+            f.get();
+        } catch (...) {
+            _done.set_exception(std::current_exception());
+            // FIXME: tell the producer to stop producing
+            abort();
+        }
+    });
 }
 
 template <typename... T>

commit ae737a074a24135df4c4f5dff36e8e99c14d1266

core: take into an account the exceptions thrown directly from _next() as well
fix the breakage introduced in  8f0a2a7 commit

Restore taking into an account the exceptions thrown directly from
_next() in addition to those coming as a returned exceptional future.

Signed-off-by: Vlad Zolotarov <vladz@cloudius-systems.com>

core/stream.hh
@@ -158,7 +158,7 @@ template <typename... T>
 inline
 future<>
 stream<T...>::produce(T... data) {
-    return _sub->_next(std::move(data)...).then_wrapped([this] (future<> f) {
+    return futurize<void>::apply(_sub->_next, std::move(data)...).then_wrapped([this] (auto&& f) {
         try {
             f.get();
         } catch (...) {