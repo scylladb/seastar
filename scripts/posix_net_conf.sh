66569fd25b25a201e61390f5977b436565ce6f36
Vlad Zolotarov <vladz@cloudius-systems.com>
2015-09-22 00:50:47 +0300

    scripts: posix_net_conf.sh: posix_net_conf.sh configure the AWS NIC's IRQs affinities and RPS
    This script configures the AWS NIC that is going to be used in a POSIX mode:
       - Sets all eth0 IRQs to arrive to CPU0.
       - For EC2 Networking:
          - Sets RPS queue N to arrive to CPU[N] for each N except for N=0.
          - RPS queue 0 is disabled.
       - For Enhanced Networking:
          - Set each HW queue to deliver to total_cpus/number_of_HW_queues of cores.
    
    Note: this script requires ethtool to be installed.
    
    Signed-off-by: Vlad Zolotarov <vladz@cloudius-systems.com>

---
+++ scripts/posix_net_conf.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+ec2_rps()
+{
+    local i=0
+    local rps_cpus
+    local mask
+    local rps_queues_count=`ls -1 /sys/class/net/eth0/queues/*/rps_cpus | wc -l`
+
+    for (( i = 1; i < rps_queues_count; i++ ))
+    do
+        rps_cpus="/sys/class/net/eth0/queues/rx-$i/rps_cpus"
+        mask=`printf %x $((1 << i))`
+        echo "Setting mask $mask in $rps_cpus"
+        echo $mask > $rps_cpus
+    done
+}
+
+en_rps()
+{
+    local cpu_num=`cat /proc/cpuinfo | grep processor | wc -l`
+    local rps_queues_count=`ls -1 /sys/class/net/eth0/queues/*/rps_cpus | wc -l`
+
+    local cpus_per_q=$((cpu_num / rps_queues_count))
+    local mask=$(( (1 << cpus_per_q) - 1 ))
+    local rps_cpus
+    for (( i = 0; i < rps_queues_count; i++ ))
+    do
+        rps_cpus="/sys/class/net/eth0/queues/rx-$i/rps_cpus"
+        mask_hex=`printf %x $mask`
+        echo "Setting mask $mask_hex in $rps_cpus"
+        echo $mask_hex > $rps_cpus
+        mask=$((mask << cpus_per_q))
+    done
+}
+
+# bind all NIC IRQs to CPU0
+for irq in `cat  /proc/interrupts | grep eth0 | cut -d":" -f1`
+do
+    echo "Binding IRQ $irq to CPU0"
+    echo 1 > /proc/irq/$irq/smp_affinity
+done
+
+# Setup RPS
+
+if ethtool -i eth0 | grep ixgbevf > /dev/null; then
+    en_rps
+else
+    ec2_rps
+fi
+
