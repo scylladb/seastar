b729e975b1e8bba735916f73b7d9ee52e2fb03fc
Vlad Zolotarov <vladz@cloudius-systems.com>
2015-11-01 15:57:00 +0200

    scripts: posix_net_conf.sh: Use a generic logic for RPS configuring
    Signed-off-by: Vlad Zolotarov <vladz@cloudius-systems.com>

--- scripts/posix_net_conf.sh
+++ scripts/posix_net_conf.sh
@@ -7,22 +7,6 @@
 # !  CPUs as equally as possible.
 # !
 
-ec2_rps()
-{
-    local i=0
-    local rps_cpus
-    local mask
-    local rps_queues_count=`ls -1 /sys/class/net/$IFACE/queues/*/rps_cpus | wc -l`
-
-    for (( i = 1; i < rps_queues_count; i++ ))
-    do
-        rps_cpus="/sys/class/net/$IFACE/queues/rx-$i/rps_cpus"
-        mask=`printf %x $((1 << i))`
-        echo "Setting mask $mask in $rps_cpus"
-        echo $mask > $rps_cpus
-    done
-}
-
 make_hex_mask()
 {
     local val=$1
@@ -39,21 +23,90 @@ make_hex_mask()
     echo -n $res
 }
 
-en_rps()
+#
+#  set_one_mask <queue index> <CPU mask>
+#
+set_one_mask()
 {
-    local cpu_num=`cat /proc/cpuinfo | grep processor | wc -l`
-    local rps_queues_count=`ls -1 /sys/class/net/$IFACE/queues/*/rps_cpus | wc -l`
+    local i=$1
+    local mask=$2
+    local rps_cpus="/sys/class/net/$IFACE/queues/rx-$i/rps_cpus"
+    local mask_hex=`make_hex_mask $mask`
+    echo "Setting mask $mask_hex in $rps_cpus"
+    echo $mask_hex > $rps_cpus
+}
 
+#
+# Bind RPS queues to CPUs other than CPU0
+#
+# If there is a single CPU in the system - don't enable RPS
+#
+# If there is more CPUs than (RPS queues plus 1) then every queue will get 
+# (NCPUS - 1)/NQUEUES CPUs and (NCPUS - 1) % NQUEUES lower queues will get one 
+# additional CPU.
+# For instance, if NCPUs=15 and NQUEUES=3, then configuration will be as follows:
+# Q0: {1,2,3,4,5}, Q1: {6,7,8,9,10} Q2: {11,12,13,14}
+#
+# If there is less CPUs than (RPS queues plus 1) when each queue will get a single CPU and 
+# CPUs will be used in a round robin way.
+# For instance, if NCPUs=3 and NQUEUES=5, then configuration will be as follows:
+# Q0: {1}, Q1: {2} Q2: {1}, Q3: {2}, Q4: {1}
+#
+# If CPUs number equals to number of RPS queues plus 1 then each queue will get 
+# a separate CPU.
+#
+setup_rps()
+{
+    # If we are in a single CPU environment - there is no point in configuring RPS
+    [[ $CPU_NUM -eq 1 ]] && return
+
+    # CPU0 will not be used since we are going to bind HW IRQs to it
+    local cpu_num=$(( CPU_NUM - 1 ))
+
+    local rps_queues_count=`ls -1 /sys/class/net/$IFACE/queues/*/rps_cpus | wc -l`
     local cpus_per_q=$((cpu_num / rps_queues_count))
-    local mask=$(( (1 << cpus_per_q) - 1 ))
-    local rps_cpus
+
+    # 
+    # If for some strange reason HW driver enabled more HW queues than a number 
+    # of CPUs bind each HW queue to a single CPU. 
+    # We will have more than a single queue per CPU in this case.
+    #
+    [[ $cpus_per_q -eq 0 ]] && cpus_per_q=1
+
+    declare -a local cpus_per_q_ar
+    local i
+
+    # Fill the array with "cpus_per_q" values first
     for (( i = 0; i < rps_queues_count; i++ ))
     do
-        rps_cpus="/sys/class/net/$IFACE/queues/rx-$i/rps_cpus"
-        mask_hex=`make_hex_mask $(( mask & ~1 ))`
-        echo "Setting mask $mask_hex in $rps_cpus"
-        echo $mask_hex > $rps_cpus
-        mask=$((mask << cpus_per_q))
+        cpus_per_q_ar[i]=$cpus_per_q
+    done
+
+    # Spread the "cpu_num - cpus_per_q * rps_queues_count" among queues 0, 1, 2,...
+    local left_cpus=$(( cpu_num - cpus_per_q * rps_queues_count ))
+
+    if [[ $left_cpus -gt 0 ]]; then
+        for (( i = 0; i < rps_queues_count; i++ ))
+        do
+            cpus_per_q_ar[i]=$(( cpus_per_q_ar[i] + 1))
+            left_cpus=$(( left_cpus - 1 ))
+            [[ $left_cpus -eq 0 ]] && break
+        done
+    fi
+
+    local mask
+    local cur_mask_shift=0
+    for (( i = 0; i < rps_queues_count; i++ ))
+    do
+        # Build a mask for a current queue, skip CPU0
+        mask=$(( ((1 << cpus_per_q_ar[i]) - 1) << (1 + cur_mask_shift) ))
+
+        cur_mask_shift=$(( cur_mask_shift + cpus_per_q_ar[i] ))
+        if [[ $cur_mask_shift -ge $cpu_num ]]; then
+            cur_mask_shift=$(( cur_mask_shift - cpu_num ))
+        fi
+
+        set_one_mask $i $mask
     done
 }
 
@@ -63,6 +116,8 @@ else
     IFACE=$1
 fi
 
+CPU_NUM=`cat /proc/cpuinfo | grep processor | wc -l`
+CPUS_MASK=$(( (1 << CPU_NUM) - 1 ))
 
 # bind all NIC IRQs to CPU0
 for irq in `cat  /proc/interrupts | grep $IFACE | cut -d":" -f1`
@@ -72,10 +127,5 @@ do
 done
 
 # Setup RPS
-
-if ethtool -i $IFACE | grep ixgbevf > /dev/null; then
-    en_rps
-else
-    ec2_rps
-fi
+setup_rps
 
