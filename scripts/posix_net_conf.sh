3c24426a6e90aa6ad9f0e54a5c40239316c311d6
Vlad Zolotarov <vladz@cloudius-systems.com>
2015-11-01 15:56:59 +0200

    scripts: posix_net_conf.sh: allow passing a NIC name as a parameter
    Signed-off-by: Vlad Zolotarov <vladz@cloudius-systems.com>

--- scripts/posix_net_conf.sh
+++ scripts/posix_net_conf.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
+# !
+# !  Usage: posix_net_conf.sh [iface name, eth0 by default]
+# !
+# !  Sets all IRQs of a given NIC to CPU0 and configures RPS to spreads NAPIs'
+# !  handling between other CPUs in the way that NAPIs are distributed between 
+# !  CPUs as equally as possible.
+# !
 
 ec2_rps()
 {
     local i=0
     local rps_cpus
     local mask
-    local rps_queues_count=`ls -1 /sys/class/net/eth0/queues/*/rps_cpus | wc -l`
+    local rps_queues_count=`ls -1 /sys/class/net/$IFACE/queues/*/rps_cpus | wc -l`
 
     for (( i = 1; i < rps_queues_count; i++ ))
     do
-        rps_cpus="/sys/class/net/eth0/queues/rx-$i/rps_cpus"
+        rps_cpus="/sys/class/net/$IFACE/queues/rx-$i/rps_cpus"
         mask=`printf %x $((1 << i))`
         echo "Setting mask $mask in $rps_cpus"
         echo $mask > $rps_cpus
@@ -35,14 +42,14 @@ make_hex_mask()
 en_rps()
 {
     local cpu_num=`cat /proc/cpuinfo | grep processor | wc -l`
-    local rps_queues_count=`ls -1 /sys/class/net/eth0/queues/*/rps_cpus | wc -l`
+    local rps_queues_count=`ls -1 /sys/class/net/$IFACE/queues/*/rps_cpus | wc -l`
 
     local cpus_per_q=$((cpu_num / rps_queues_count))
     local mask=$(( (1 << cpus_per_q) - 1 ))
     local rps_cpus
     for (( i = 0; i < rps_queues_count; i++ ))
     do
-        rps_cpus="/sys/class/net/eth0/queues/rx-$i/rps_cpus"
+        rps_cpus="/sys/class/net/$IFACE/queues/rx-$i/rps_cpus"
         mask_hex=`make_hex_mask $(( mask & ~1 ))`
         echo "Setting mask $mask_hex in $rps_cpus"
         echo $mask_hex > $rps_cpus
@@ -50,8 +57,15 @@ en_rps()
     done
 }
 
+if [[ $# -eq 0 ]]; then 
+    IFACE="eth0"
+else
+    IFACE=$1
+fi
+
+
 # bind all NIC IRQs to CPU0
-for irq in `cat  /proc/interrupts | grep eth0 | cut -d":" -f1`
+for irq in `cat  /proc/interrupts | grep $IFACE | cut -d":" -f1`
 do
     echo "Binding IRQ $irq to CPU0"
     echo 1 > /proc/irq/$irq/smp_affinity
@@ -59,7 +73,7 @@ done
 
 # Setup RPS
 
-if ethtool -i eth0 | grep ixgbevf > /dev/null; then
+if ethtool -i $IFACE | grep ixgbevf > /dev/null; then
     en_rps
 else
     ec2_rps
