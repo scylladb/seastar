scylladb/seastar

commit e5cbee3d54818244502bea83866f58e791898094

tcp: don't need to choose another core since only one core
Signed-off-by: Haomai Wang <haomai@xsky.com>
Message-Id: <1455628351-19283-1-git-send-email-haomai@xsky.com>

net/net.hh
@@ -131,6 +131,7 @@ public:
     void register_packet_provider(l3_protocol::packet_provider_type func) {
         _pkt_providers.push_back(std::move(func));
     }
+    uint16_t hw_queues_count() { return _dev->hw_queues_count(); }
     const rss_key_type& rss_key() const;
     friend class l3_protocol;
 };

net/tcp.hh
@@ -673,8 +673,9 @@ future<typename tcp<InetTraits>::connection> tcp<InetTraits>::connect(socket_add
     do {
         src_port = _port_dist(_e);
         id = connid{src_ip, dst_ip, src_port, dst_port};
-    } while (_inet._inet.netif()->hash2cpu(id.hash(_inet._inet.netif()->rss_key())) != engine().cpu_id()
-            || _tcbs.find(id) != _tcbs.end());
+    } while (_inet._inet.netif()->hw_queues_count() > 1 &&
+             (_inet._inet.netif()->hash2cpu(id.hash(_inet._inet.netif()->rss_key())) != engine().cpu_id()
+              || _tcbs.find(id) != _tcbs.end()));
 
     auto tcbp = make_lw_shared<tcb>(*this, id);
     _tcbs.insert({id, tcbp});

commit 86790339153d8ee301a0ab6a14ca2d67fbd444fb

net: fix compilation problem introduced after e5cbee3
gcc 5.1.1 complains about the following:
/data/scylla/seastar/net/net.hh:134:45: error: invalid use of incomplete type ‘class net::device’
     uint16_t hw_queues_count() { return _dev->hw_queues_count(); }

Fix it by moving implementation of hw_queues_count() to net/net.cc

Signed-off-by: Raphael S. Carvalho <raphaelsc@scylladb.com>
Message-Id: <1455644668-27177-1-git-send-email-raphaelsc@scylladb.com>

net/net.cc
@@ -311,6 +311,10 @@ unsigned interface::hash2cpu(uint32_t hash) {
     return _dev->hash2cpu(hash);
 }
 
+uint16_t interface::hw_queues_count() {
+    return _dev->hw_queues_count();
+}
+
 const rss_key_type& interface::rss_key() const {
     return _dev->rss_key();
 }

net/net.hh
@@ -131,7 +131,7 @@ public:
     void register_packet_provider(l3_protocol::packet_provider_type func) {
         _pkt_providers.push_back(std::move(func));
     }
-    uint16_t hw_queues_count() { return _dev->hw_queues_count(); }
+    uint16_t hw_queues_count();
     const rss_key_type& rss_key() const;
     friend class l3_protocol;
 };