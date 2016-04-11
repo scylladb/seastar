92820667b9c127572f557432729c1b1e366858bd
Sam Kottler <shk@linux.com>
2016-04-11 14:54:04 -0400

    Fix shebang to use env(1)

--- configure.py
+++ configure.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 #
 # This file is open source software, licensed to you under the terms
 # of the Apache License, Version 2.0 (the "License").  See the NOTICE file
