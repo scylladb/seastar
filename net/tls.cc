e43663820ed51ab576977ca5fb955f2c927509bb
Duarte Nunes <duarte@scylladb.com>
2016-04-18 19:28:31 +0200

    tls: Add support for seastar::socket
    This patch enables TLS connections to be established from a
    seastar::socket.
    
    Signed-off-by: Duarte Nunes <duarte@scylladb.com>

--- net/tls.cc
+++ net/tls.cc
@@ -858,6 +858,24 @@ private:
     ::server_socket _sock;
 };
 
+class tls_socket_impl : public net::socket_impl {
+    ::shared_ptr<certificate_credentials> _cred;
+    sstring _name;
+    seastar::socket _socket;
+public:
+    tls_socket_impl(::shared_ptr<certificate_credentials> cred, sstring name)
+            : _cred(cred), _name(std::move(name)), _socket(engine().net().socket()) {
+    }
+    virtual future<connected_socket> connect(socket_address sa, socket_address local) override {
+        return _socket.connect(sa, local).then([cred = std::move(_cred), name = std::move(_name)](::connected_socket s) mutable {
+            return wrap_client(cred, std::move(s), std::move(name));
+        });
+    }
+    virtual void shutdown() override {
+        _socket.shutdown();
+    }
+};
+
 }
 }
 
@@ -880,6 +898,10 @@ future<::connected_socket> seastar::tls::connect(::shared_ptr<certificate_creden
     return engine().connect(sa, local).then([cred = std::move(cred), name = std::move(name)](::connected_socket s) mutable {
         return wrap_client(cred, std::move(s), std::move(name));
     });
+}
+
+seastar::socket seastar::tls::socket(::shared_ptr<certificate_credentials> cred, sstring name) {
+    return seastar::socket(std::make_unique<tls_socket_impl>(std::move(cred), std::move(name)));
 }
 
 future<::connected_socket> seastar::tls::wrap_client(::shared_ptr<certificate_credentials> cred, ::connected_socket&& s, sstring name) {
