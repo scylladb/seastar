cdb466ca50d5b287ce2bbc82313d5ccfa84891b6
eric yu <eric.yutao@gmail.com>
2016-04-28 18:28:05 +0800

    when get a item, touch it for lru position

--- apps/memcached/memcache.cc
+++ apps/memcached/memcache.cc
@@ -237,6 +237,11 @@ public:
         return _ref_count == 1;
     }
 
+    // touch item for lru position
+    void touch() {
+      slab->touch(this);
+    }
+
     friend bool operator==(const item &a, const item &b) {
          return (a._key_hash == b._key_hash) &&
             (a._key_size == b._key_size) &&
@@ -598,6 +603,7 @@ public:
             _stats._get_misses++;
             return nullptr;
         }
+        i->touch();
         _stats._get_hits++;
         auto& item_ref = *i;
         return item_ptr(&item_ref);
