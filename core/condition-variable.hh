2729975b3366c78b920ede29b6f750d2b51a55c5
Gleb Natapov <gleb@scylladb.com>
2016-05-18 12:53:44 +0300

    condition_variable: add multiple waiters and wait with timeout

--- core/condition-variable.hh
+++ core/condition-variable.hh
@@ -22,6 +22,7 @@
 #pragma once
 
 #include "core/future-util.hh"
+#include "core/semaphore.hh"
 
 /// \addtogroup fiber-module
 /// @{
@@ -32,7 +33,17 @@ class broken_condition_variable : public std::exception {
 public:
     /// Reports the exception reason.
     virtual const char* what() const noexcept {
-        return "Conditional variable is broken";
+        return "Condition variable is broken";
+    }
+};
+
+/// Exception thrown when wait() operation times out
+/// \ref condition_variable::wait(time_point timeout).
+class condition_variable_timed_out : public std::exception {
+public:
+    /// Reports the exception reason.
+    virtual const char* what() const noexcept {
+        return "Condition variable timed out";
     }
 };
 
@@ -49,31 +60,55 @@ public:
 /// This issimilar to POSIX's `pthread_cancel()`, with \ref wait()
 /// acting as a cancellation point.
 
-/// FIXME: Support multiple waiters
 class condition_variable {
-    promise<> _p;
-    bool _signaled = false;
-    bool _broken = false;
+    using duration = semaphore::duration;
+    using clock = semaphore::clock;
+    using time_point = semaphore::time_point;
+    struct condition_variable_exception_factory {
+        static condition_variable_timed_out timeout() {
+            return condition_variable_timed_out();
+        }
+        static broken_condition_variable broken() {
+            return broken_condition_variable();
+        }
+    };
+    basic_semaphore<condition_variable_exception_factory> _sem;
 public:
     /// Waits until condition variable is signaled, may wake up without condition been met
     ///
     /// \return a future that becomes ready when \ref signal() is called
-    ///         If the condition variable was \ref broken(), may contain an exception.
+    ///         If the condition variable was \ref broken() will return \ref broken_condition_variable
+    ///         exception.
     future<> wait() {
-        if (_broken) {
-            return make_exception_future<>(broken_condition_variable());
-        }
-        if (_signaled) {
-            _p = promise<>();
-            _signaled = false;
-            return make_ready_future<>();
-        }
-        return _p.get_future().then([this] {
-            return wait();
-        });
+        return _sem.wait();
     }
+
+    /// Waits until condition variable is signaled or timeout is reached
+    ///
+    /// \param timeout time point at which wait will exit with a timeout
+    ///
+    /// \return a future that becomes ready when \ref signal() is called
+    ///         If the condition variable was \ref broken() will return \ref broken_condition_variable
+    ///         exception. If timepoint is reached will return \ref condition_variable_timed_out exception.
+    future<> wait(time_point timeout) {
+        return _sem.wait(timeout);
+    }
+
+    /// Waits until condition variable is signaled or timeout is reached
+    ///
+    /// \param timeout duration after which wait will exit with a timeout
+    ///
+    /// \return a future that becomes ready when \ref signal() is called
+    ///         If the condition variable was \ref broken() will return \ref broken_condition_variable
+    ///         exception. If timepoint is passed will return \ref condition_variable_timed_out exception.
+    future<> wait(duration timeout) {
+        return _sem.wait(timeout);
+    }
+
     /// Waits until condition variable is notified and pred() == true, otherwise
     /// wait again.
+    ///
+    /// \param pred predicate that checks that awaited condition is true
     ///
     /// \return a future that becomes ready when \ref signal() is called
     ///         If the condition variable was \ref broken(), may contain an exception.
@@ -83,19 +118,53 @@ public:
             return wait();
         });
     }
+
+    /// Waits until condition variable is notified and pred() == true or timeout is reached, otherwise
+    /// wait again.
+    ///
+    /// \param timeout time point at which wait will exit with a timeout
+    /// \param pred predicate that checks that awaited condition is true
+    ///
+    /// \return a future that becomes ready when \ref signal() is called
+    ///         If the condition variable was \ref broken() will return \ref broken_condition_variable
+    ///         exception. If timepoint is reached will return \ref condition_variable_timed_out exception.
+    /// \param
+    template<typename Pred>
+    future<> wait(time_point timeout, Pred&& pred) {
+        return do_until(std::forward<Pred>(pred), [this, timeout] () mutable {
+            return wait(timeout);
+        });
+    }
+
+    /// Waits until condition variable is notified and pred() == true or timeout is reached, otherwise
+    /// wait again.
+    ///
+    /// \param timeout duration after which wait will exit with a timeout
+    /// \param pred predicate that checks that awaited condition is true
+    ///
+    /// \return a future that becomes ready when \ref signal() is called
+    ///         If the condition variable was \ref broken() will return \ref broken_condition_variable
+    ///         exception. If timepoint is passed will return \ref condition_variable_timed_out exception.
+    template<typename Pred>
+    future<> wait(duration timeout, Pred&& pred) {
+        return wait(clock::now() + timeout, std::forward<Pred>(pred));
+    }
     /// Notify variable and wake up a waiter if there is one
     void signal() {
-        if (!_signaled) {
-            _p.set_value();
-            _signaled = true;
+        if (_sem.waiters()) {
+            _sem.signal();
         }
     }
+    /// Notify variable and wake up all waiter
+    void broadcast() {
+        _sem.signal(_sem.waiters());
+    }
+
     /// Signal to waiters that an error occurred.  \ref wait() will see
     /// an exceptional future<> containing the provided exception parameter.
     /// The future is made available immediately.
     void broken() {
-        _broken = true;
-        signal();
+        _sem.broken();
     }
 };
 
