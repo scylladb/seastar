2c73ddc628eb846a2a361fd2cd46f842934af0d8
fenglin <smy19890720@gmail.com>
2016-05-19 22:23:10 +0800

    fenglin;thread 注释

--- core/thread.hh
+++ core/thread.hh
@@ -43,14 +43,18 @@
 /// Seastar threads are not the same as operating system threads:
 ///   - seastar threads are cooperative; they are never preempted except
 ///     at blocking points (see below)
+///     seastar 线程是合作的式,除了租塞以外,它从来都不会占用 cpu 资源
 ///   - seastar threads always run on the same core they were launched on
+///     seastar 线程被运行在相同的核
 ///
 /// Like other seastar code, seastar threads may not issue blocking system calls.
-///
+/// 类似于其他的 seastar 代码一样,seastar 线程不会在系统调用上被租塞
 /// A seastar thread blocking point is any function that returns a \ref future<>.
 /// you block by calling \ref future<>::get(); this waits for the future to become
 /// available, and in the meanwhile, other seastar threads and seastar non-threaded
 /// code may execute.
+/// seastar唯一能租塞的地方就是当对 future 进行 get 操作的时候,这个时候线程就会被阻塞,
+// 但是其他的线程和核是不会收到影响的;
 ///
 /// Example:
 /// \code
@@ -63,6 +67,8 @@
 /// result from this execution is by using the \ref seastar::async() function.
 /// The result is returned as a future, so that non-threaded code can wait for
 /// the thread to terminate and yield a result.
+/// 一个简单的方式可以让一个线程运行一些复杂的计算任务,通过 seastar::async 返回把结果返回;
+/// 这个结果是 future, 所以非线程代码可能等待线程终止或者等待结果
 
 /// Seastar API namespace
 namespace seastar {
@@ -88,7 +94,12 @@ public:
 };
 
 namespace thread_impl {
-
+ /**
+  * 1. get 获得线程上下文
+  * 2. switch_in 线程切入
+  * 3. switch_out 线程切出
+  * 4. init 初始化
+  */
 thread_context* get();
 void switch_in(thread_context* to);
 void switch_out(thread_context* from);
@@ -142,22 +153,24 @@ public:
 
 
 /// \brief thread - stateful thread of execution
-///
+/// 运行中的线程
 /// Threads allow using seastar APIs in a blocking manner,
 /// by calling future::get() on a non-ready future.  When
 /// this happens, the thread is put to sleep until the future
 /// becomes ready.
 class thread {
+    // 线程上下文
     std::unique_ptr<thread_context> _context;
     static thread_local thread* _current;
 public:
     /// \brief Constructs a \c thread object that does not represent a thread
-    /// of execution.
+    /// of execution.(默认初始化的 thread 表示不是可以运行状态的 thread)
     thread() = default;
     /// \brief Constructs a \c thread object that represents a thread of execution
     ///
     /// \param func Callable object to execute in thread.  The callable is
     ///             called immediately.
+    /// 放回一个 func 对象,让 thread 立即运行
     template <typename Func>
     thread(Func func);
     /// \brief Constructs a \c thread object that represents a thread of execution
@@ -165,6 +178,7 @@ public:
     /// \param attr Attributes describing the new thread.
     /// \param func Callable object to execute in thread.  The callable is
     ///             called immediately.
+    /// 线程描述信息+ callable 函数
     template <typename Func>
     thread(thread_attributes attr, Func func);
     /// \brief Moves a thread object.
@@ -174,31 +188,37 @@ public:
     /// \brief Destroys a \c thread object.
     ///
     /// The thread must not represent a running thread of execution (see join()).
+    /// 判断上下文为空 || 上下文可等待
     ~thread() { assert(!_context || _context->_joined); }
     /// \brief Waits for thread execution to terminate.
     ///
     /// Waits for thread execution to terminate, and marks the thread object as not
     /// representing a running thread of execution.
+    /// 等待其他线程中止,并且设置当前线程为不可运行状态
     future<> join();
     /// \brief Voluntarily defer execution of current thread.
-    ///
+    /// 自动退出当前线程的执行,让出时间片
     /// Gives other threads/fibers a chance to run on current CPU.
     /// The current thread will resume execution promptly.
+    /// 把当前的 cpu 让出来给别的 thread 使用,当前线程会在很快被恢复
     static void yield();
     /// \brief Checks whether this thread ought to call yield() now.
-    ///
+    /// 检测当前线程是否应该 yield;
     /// Useful where we cannot call yield() immediately because we
     /// Need to take some cleanup action first.
+    ///这个函数对下面这种情况下是很有用的; 比如因为我们需要清理一些操作以至于我们不能直接调用 yield 函数
     static bool should_yield();
 
+    //判断当前线程是否处于运行状态
     static bool running_in_thread() {
         return seastar::thread_impl::get() != nullptr;
     }
 };
 
+    //调度组
 class thread_scheduling_group {
-    std::chrono::nanoseconds _period;
-    std::chrono::nanoseconds _quota;
+    std::chrono::nanoseconds _period;//周期
+    std::chrono::nanoseconds _quota; //
     std::chrono::time_point<thread_clock> _this_period_ends = {};
     std::chrono::time_point<thread_clock> _this_run_start = {};
     std::chrono::nanoseconds _this_period_remain = {};
@@ -226,7 +246,9 @@ thread::thread(Func func)
 inline
 future<>
 thread::join() {
+    //设置当前线程的 join = true
     _context->_joined = true;
+    //返回一个 promise 的 future
     return _context->_done.get_future();
 }
 
