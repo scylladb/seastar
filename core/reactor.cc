c2b30a0118b320aea8f594aa666d7e82abd4bb40
Avi Kivity <avi@scylladb.com>
2016-07-04 23:32:39 +0300

    reactor: more fix for smp poll with older boost
    Unfortunately there is no workaround for write_available(), so we must
    return true, which is a safe default (killing the idle worker).

--- core/reactor.cc
+++ core/reactor.cc
@@ -50,6 +50,7 @@
 #include <boost/iterator/counting_iterator.hpp>
 #include <boost/range/numeric.hpp>
 #include <boost/range/adaptor/transformed.hpp>
+#include <boost/version.hpp>
 #include <atomic>
 #include <dirent.h>
 #include <linux/types.h> // for xfs, below
@@ -2172,7 +2173,11 @@ void smp_message_queue::move_pending() {
 }
 
 bool smp_message_queue::pure_poll_tx() const {
+#if BOOST_VERSION >= 15600
     return !_tx.a.pending_fifo.empty() && _pending.write_available();
+#else
+    return true;
+#endif
 }
 
 void smp_message_queue::submit_item(smp_message_queue::work_item* item) {
