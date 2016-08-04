scylladb/seastar

commit 5b1051ce0de5b772f22444abe6a1d97076b49b1f

Add --abort-on-seastar-bad-alloc option
Useful for forcing a coredump on bad_alloc from seastar allocator,
which can be then analyzed.

Message-Id: <1470302656-6658-1-git-send-email-tgrabiec@scylladb.com>

core/memory.cc
@@ -58,6 +58,7 @@
 #include "align.hh"
 #include "posix.hh"
 #include "shared_ptr.hh"
+#include "reactor.hh"
 #include <new>
 #include <cstdint>
 #include <algorithm>
@@ -75,6 +76,19 @@
 #endif
 
 namespace memory {
+
+static std::atomic<bool> abort_on_allocation_failure{false};
+
+void enable_abort_on_allocation_failure() {
+    abort_on_allocation_failure.store(true, std::memory_order_seq_cst);
+}
+
+static void on_allocation_failure(size_t size) {
+    if (abort_on_allocation_failure.load(std::memory_order_relaxed)) {
+        seastar_logger.error("Failed to allocate {} bytes", size);
+        abort();
+    }
+}
 
 static constexpr unsigned cpu_id_shift = 36; // FIXME: make dynamic
 static constexpr unsigned max_cpus = 256;
@@ -951,11 +965,16 @@ void* allocate(size_t size) {
     if (size <= sizeof(free_object)) {
         size = sizeof(free_object);
     }
+    void* ptr;
     if (size <= max_small_allocation) {
-        return cpu_mem.allocate_small(size);
+        ptr = cpu_mem.allocate_small(size);
     } else {
-        return allocate_large(size);
+        ptr = allocate_large(size);
     }
+    if (!ptr) {
+        on_allocation_failure(size);
+    }
+    return ptr;
 }
 
 void* allocate_aligned(size_t align, size_t size) {
@@ -964,14 +983,19 @@ void* allocate_aligned(size_t align, size_t size) {
     if (size <= sizeof(free_object)) {
         size = sizeof(free_object);
     }
+    void* ptr;
     if (size <= max_small_allocation && align <= page_size) {
         // Our small allocator only guarantees alignment for power-of-two
         // allocations which are not larger than a page.
         size = 1 << log2ceil(size);
-        return cpu_mem.allocate_small(size);
+        ptr = cpu_mem.allocate_small(size);
     } else {
-        return allocate_large_aligned(align, size);
+        ptr = allocate_large_aligned(align, size);
     }
+    if (!ptr) {
+        on_allocation_failure(size);
+    }
+    return ptr;
 }
 
 void free(void* obj) {
@@ -1091,6 +1115,7 @@ void* malloc(size_t n) throw () {
     try {
         return allocate(n);
     } catch (std::bad_alloc& ba) {
+        on_allocation_failure(n);
         return nullptr;
     }
 }
@@ -1174,6 +1199,7 @@ int posix_memalign(void** ptr, size_t align, size_t size) {
         }
         return 0;
     } catch (std::bad_alloc&) {
+        on_allocation_failure(size);
         return ENOMEM;
     }
 }

core/memory.hh
@@ -60,6 +60,8 @@ static constexpr size_t huge_page_size = 512 * page_size; // 2M
 void configure(std::vector<resource::memory> m,
         std::experimental::optional<std::string> hugetlbfs_path = {});
 
+void enable_abort_on_allocation_failure();
+
 void* allocate_reclaimable(size_t size);
 
 enum class reclaiming_result {

core/reactor.cc
@@ -2520,6 +2520,7 @@ reactor::get_options_description() {
         ("max-task-backlog", bpo::value<unsigned>()->default_value(1000), "Maximum number of task backlog to allow; above this we ignore I/O")
         ("relaxed-dma", "allow using buffered I/O if DMA is not available (reduces performance)")
         ("overprovisioned", "run in an overprovisioned environment (such as docker or a laptop); equivalent to --idle-poll-time-us 0 --thread-affinity 0 --poll-aio 0")
+        ("abort-on-seastar-bad-alloc", "abort when seastar allocator cannot allocate memory")
         ;
     opts.add(network_stack_registry::options_description());
     return opts;
@@ -2746,6 +2747,10 @@ void smp::configure(boost::program_options::variables_map configuration)
         smp::pin(allocations[0].cpu_id);
     }
     memory::configure(allocations[0].mem, hugepages_path);
+
+    if (configuration.count("abort-on-seastar-bad-alloc")) {
+        memory::enable_abort_on_allocation_failure();
+    }
 
 #ifdef HAVE_DPDK
     if (smp::_using_dpdk) {

commit 1262ce78bae33e96e57d32a5d1fa23c18846e9f8

Fix build failure introduced by 5b1051ce0de5b772f22444abe6a1d97076b49b1f
Changes:
  - added all 'core' objects to dependencies of allocator_test.
    memory.cc needs seastar_logger to be defined.

  - added missing implementation of enable_abort_on_allocation_failure()
    for when seastart allocator is compiled out (debug mode).

Message-Id: <1470315649-13867-1-git-send-email-tgrabiec@scylladb.com>

configure.py
@@ -384,7 +384,7 @@ deps = {
     'tests/blkdiscard_test': ['tests/blkdiscard_test.cc'] + core,
     'tests/sstring_test': ['tests/sstring_test.cc'] + core,
     'tests/httpd': ['tests/httpd.cc'] + http + core + boost_test_lib,
-    'tests/allocator_test': ['tests/allocator_test.cc', 'core/memory.cc', 'core/posix.cc'],
+    'tests/allocator_test': ['tests/allocator_test.cc'] + core,
     'tests/output_stream_test': ['tests/output_stream_test.cc'] + core + libnet + boost_test_lib,
     'tests/udp_zero_copy': ['tests/udp_zero_copy.cc'] + core + libnet,
     'tests/shared_ptr_test': ['tests/shared_ptr_test.cc'] + core,

core/memory.cc
@@ -51,6 +51,7 @@
 // by size.  When spans are broken up or coalesced, they may move into new lists.
 
 #include "memory.hh"
+#include "reactor.hh"
 
 #ifndef DEFAULT_ALLOCATOR
 
@@ -58,7 +59,6 @@
 #include "align.hh"
 #include "posix.hh"
 #include "shared_ptr.hh"
-#include "reactor.hh"
 #include <new>
 #include <cstdint>
 #include <algorithm>
@@ -1382,6 +1382,10 @@ void operator delete[](void* ptr, with_alignment wa) {
 #else
 
 namespace memory {
+
+void enable_abort_on_allocation_failure() {
+    seastar_logger.warn("Seastar compiled with default allocator, will not abort on bad_alloc");
+}
 
 reclaimer::reclaimer(reclaim_fn reclaim, reclaimer_scope) {
 }