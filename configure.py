24153780d6a55395918dbc6b7f921d68874abd70
Tomasz Grabiec <tgrabiec@scylladb.com>
2016-09-23 16:58:51 +0200

    util: Introduce backtrace obtaining utility based on libunwind

--- configure.py
+++ configure.py
@@ -313,7 +313,7 @@ boost_test_lib = [
 ]
 
 defines = ['FMT_HEADER_ONLY']
-libs = '-laio -lboost_program_options -lboost_system -lboost_filesystem -lstdc++ -lm -lboost_unit_test_framework -lboost_thread -lcryptopp -lrt -lgnutls -lgnutlsxx -llz4 -lprotobuf -ldl'
+libs = '-laio -lboost_program_options -lboost_system -lboost_filesystem -lstdc++ -lm -lboost_unit_test_framework -lboost_thread -lcryptopp -lrt -lgnutls -lgnutlsxx -llz4 -lprotobuf -ldl -lunwind'
 hwloc_libs = '-lhwloc -lnuma -lpciaccess -lxml2 -lz'
 xen_used = False
 def have_xen():
