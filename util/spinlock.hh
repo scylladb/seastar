3b3a88d27fe9237f524706e61f10767c546ce021
Tomasz Grabiec <tgrabiec@scylladb.com>
2016-09-23 16:59:42 +0200

    util: Add spinlock implementation

---
+++ util/spinlock.hh
@@ -0,0 +1,49 @@
+/*
+ * This file is open source software, licensed to you under the terms
+ * of the Apache License, Version 2.0 (the "License").  See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership.  You may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/*
+ * Copyright 2016 ScyllaDB
+ */
+
+#pragma once
+
+#include <atomic>
+#include <xmmintrin.h>
+
+namespace util {
+
+// Spin lock implementation.
+// BasicLockable.
+// Async-signal safe.
+// unlock() "synchronizes with" lock().
+class spinlock {
+    std::atomic<bool> _busy = { false };
+public:
+    spinlock() = default;
+    spinlock(const spinlock&) = delete;
+    ~spinlock() { assert(!_busy.load(std::memory_order_relaxed)); }
+    void lock() noexcept {
+        while (_busy.exchange(true, std::memory_order_acquire)) {
+            _mm_pause();
+        }
+    }
+    void unlock() noexcept {
+        _busy.store(false, std::memory_order_release);
+    }
+};
+
+}
