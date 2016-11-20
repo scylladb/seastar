scylladb/seastar

commit 83549367df6936fe844c8a6635baeeac0505a427

scollectd: Change the scollectd to use the metrics API
The metircs layer gives a cleaner API for registering metrics,
eventually metrics will be register using the metrics API, but
currently, the collectd API is used throughout the system.

This patch is for the transition phase. It changes the collectd to use
the metrics API to get it's value. For the current code to continue
working it uses the collectd registration method to register the metrics
on the metrics layer.

When all the registration in the system will be replaced to use the
metrics layer, the collectd will be cleaned removing all the
registration related code from it.

core/scollectd.cc
@@ -54,6 +54,15 @@ namespace scollectd {
 seastar::logger logger("scollectd");
 thread_local unsigned type_instance_id::_next_truncated_idx = 0;
 
+registration::~registration() {
+    unregister();
+}
+
+seastar::metrics::impl::metric_id to_metrics_id(const type_instance_id & id) {
+    return std::move(seastar::metrics::impl::metric_id(id.plugin(), id.plugin_instance(), id.type(), id.type_instance()));
+}
+
+
 const plugin_instance_id per_cpu_plugin_instance("#cpu");
 
 static const size_t payload_size = 1024;
@@ -188,7 +197,19 @@ struct cpwriter {
         }
         return *this;
     }
-    cpwriter & put(const sstring & host, const type_instance_id & id) {
+
+    cpwriter & put(part_type type, const seastar::metrics::impl::registered_metric & v) {
+        auto sz = 7 +  sizeof(uint64_t);
+        if (check(sz)) {
+            write(uint16_t(type));
+            write(uint16_t(sz));
+            write(uint16_t(1));
+            write(static_cast<uint8_t>(v.get_type()));
+            write(v().ui());
+        }
+        return *this;
+    }
+    cpwriter & put(const sstring & host, const seastar::metrics::impl::metric_id & id) {
         const auto ts = std::chrono::system_clock::now().time_since_epoch();
         const auto lrts =
                 std::chrono::duration_cast<std::chrono::seconds>(ts).count();
@@ -198,19 +219,32 @@ struct cpwriter {
         // Seems hi-res timestamp does not work very well with
         // at the very least my default collectd in fedora (or I did it wrong?)
         // Use lo-res ts for now, it is probably quite sufficient.
-        put_cached(part_type::Plugin, id.plugin());
+        put_cached(part_type::Plugin, id.group_name());
         // Optional
         put_cached(part_type::PluginInst,
-                id.plugin_instance() == per_cpu_plugin_instance ?
-                        to_sstring(engine().cpu_id()) : id.plugin_instance());
-        put_cached(part_type::Type, id.type());
+                id.instance_id() == per_cpu_plugin_instance ?
+                        to_sstring(engine().cpu_id()) : id.instance_id());
+        put_cached(part_type::Type, id.measurement());
         // Optional
-        put_cached(part_type::TypeInst, id.type_instance());
+        put_cached(part_type::TypeInst, id.sub_measurement());
         return *this;
     }
     cpwriter & put(const sstring & host,
             const duration & period,
             const type_instance_id & id, const value_list & v) {
+        const auto ps = std::chrono::duration_cast<collectd_hres_duration>(
+                        period).count();
+            put(host, to_metrics_id(id));
+            put(part_type::Values, v);
+            if (ps != 0) {
+                put(part_type::IntervalHr, ps);
+            }
+            return *this;
+    }
+
+    cpwriter & put(const sstring & host,
+            const duration & period,
+            const seastar::metrics::impl::metric_id & id, const seastar::metrics::impl::registered_metric & v) {
         const auto ps = std::chrono::duration_cast<collectd_hres_duration>(
                 period).count();
         put(host, id);
@@ -222,22 +256,15 @@ struct cpwriter {
     }
 };
 
-impl::value_list_map& impl::get_value_list_map() {
-    return _values;
-}
-
 void impl::add_polled(const type_instance_id & id,
         const shared_ptr<value_list> & values, bool enable) {
-    values->set_enabled(enable);
-    _values[id] = values;
+    // do nothing
+    // add_polled is now implemented on the metrics layer
 
 }
 
 void impl::remove_polled(const type_instance_id & id) {
-    auto i = _values.find(id);
-    if (i != _values.end()) {
-        i->second = nullptr;
-    }
+    seastar::metrics::impl::unregister_metric(to_metrics_id(id));
 }
 
 // explicitly send a type_instance value list (outside polling)
@@ -254,7 +281,7 @@ future<> impl::send_metric(const type_instance_id & id,
 future<> impl::send_notification(const type_instance_id & id,
         const sstring & msg) {
     cpwriter out;
-    out.put(_host, id);
+    out.put(_host, to_metrics_id(id));
     out.put(part_type::Message, msg);
     return _chan.send(_addr, net::packet(out.data(), out.size()));
 }
@@ -283,7 +310,7 @@ void impl::start(const sstring & host, const ipv4_addr & addr, const duration pe
         // latency          value:GAUGE:0:U
         add_polled_metric(
                 type_instance_id("scollectd", per_cpu_plugin_instance,
-                        "latency"), _avg),
+                        "latency"), make_typed(data_type::GAUGE, _avg)),
         // total_time_in_ms    value:DERIVE:0:U
         add_polled_metric(
                 type_instance_id("scollectd", per_cpu_plugin_instance,
@@ -294,13 +321,13 @@ void impl::start(const sstring & host, const ipv4_addr & addr, const duration pe
         add_polled_metric(
                 type_instance_id("scollectd", per_cpu_plugin_instance,
                         "total_values"),
-                make_typed(data_type::DERIVE, std::bind(&value_list_map::size, &_values))
+                make_typed(data_type::DERIVE, [this] {return values().size();})
         ),
         // records          value:GAUGE:0:U
         add_polled_metric(
                 type_instance_id("scollectd", per_cpu_plugin_instance,
                         "records"),
-                make_typed(data_type::GAUGE, std::bind(&value_list_map::size, &_values))
+                make_typed(data_type::GAUGE, [this] {return values().size();})
         ),
     };
 
@@ -323,7 +350,7 @@ void impl::arm() {
 }
 
 void impl::run() {
-    typedef value_list_map::iterator iterator;
+    typedef value_list_map::const_iterator iterator;
     typedef std::tuple<iterator, cpwriter> context;
 
     auto ctxt = make_lw_shared<context>();
@@ -331,10 +358,10 @@ void impl::run() {
     // note we're doing this unsynced since we assume
     // all registrations to this instance will be done on the
     // same cpu, and without interuptions (no wait-states)
-    std::get<iterator>(*ctxt) = _values.begin();
+    std::get<iterator>(*ctxt) = values().begin();
 
     auto stop_when = [this, ctxt]() {
-        auto done = std::get<iterator>(*ctxt) == _values.end();
+        auto done = std::get<iterator>(*ctxt) == values().end();
         return done;
     };
     // append as many values as we can fit into a packet (1024 bytes)
@@ -345,12 +372,7 @@ void impl::run() {
 
         out.clear();
 
-        while (i != _values.end()) {
-            // nullptr value list means removed value. so remove.
-            if (!i->second) {
-                i = _values.erase(i);
-                continue;
-            }
+        while (i != values().end()) {
             if (i->second->is_enabled()) {
                 auto m = out.mark();
                 out.put(_host, _period, i->first, *i->second);
@@ -387,14 +409,9 @@ void impl::run() {
     });
 }
 
-shared_ptr<value_list> impl::get_values(const type_instance_id & id) const {
-    auto i = _values.find(id);
-    return i != _values.end() ? i->second : nullptr;
-}
-
 std::vector<type_instance_id> impl::get_instance_ids() const {
     std::vector<type_instance_id> res;
-    for (auto i: _values) {
+    for (auto i: values()) {
         // Need to check for empty value_list, since unreg is two-stage.
         // Not an issue for most uses, but unit testing etc that would like
         // fully deterministic operation here would like us to only return
@@ -461,44 +478,22 @@ boost::program_options::options_description get_options_description() {
     return opts;
 }
 
-static std::vector<collectd_value> raw_to_value(shared_ptr<value_list> raw_types) {
-    std::vector<collectd_value> res_values;
-    if (raw_types == nullptr) {
-        return res_values;
-    }
-    std::vector<data_type> types(raw_types->size());
-    raw_types->types(&(*types.begin()));
-    std::vector<net::packed<uint64_t>> value(raw_types->size());
-    raw_types->values(&(*value.begin()));
-
-    auto i = value.begin();
-    for (auto t : types) {
-        if (t == data_type::GAUGE) {
-            collectd_value c(t, static_cast<uint64_t>(*i));
-            res_values.push_back(c);
-        } else {
-            collectd_value c(t, htonq(static_cast<uint64_t>(*i)));
-            res_values.push_back(c);
-
-        }
-        ++i;
-    }
-    return res_values;
-}
-
 std::vector<collectd_value> get_collectd_value(
         const scollectd::type_instance_id& id) {
-    return raw_to_value(get_impl().get_values(id));
+    std::vector<collectd_value> vals;
+    const seastar::metrics::impl::registered_metric& val = *seastar::metrics::impl::get_value_map().at(to_metrics_id(id));
+    vals.push_back(val());
+    return vals;
 }
 
 std::vector<data_type> get_collectd_types(
         const scollectd::type_instance_id& id) {
-    auto res = get_impl().get_values(id);
+    auto res = seastar::metrics::impl::get_value_map().at(to_metrics_id(id));
     if (res == nullptr) {
         return std::vector<data_type>();
     }
-    std::vector<data_type> vals(res->size());
-    res->types(&(*vals.begin()));
+    std::vector<data_type> vals;
+    vals.push_back(res->get_type());
     return vals;
 }
 
@@ -507,16 +502,16 @@ std::vector<scollectd::type_instance_id> get_collectd_ids() {
 }
 
 sstring get_collectd_description_str(const scollectd::type_instance_id& id) {
-    auto v = get_impl().get_values(id);
-    return v != nullptr ? v->desc().str() : sstring();
+    auto v = seastar::metrics::impl::get_value_map().at(to_metrics_id(id));
+    return v != nullptr ? v->get_description().str() : sstring();
 }
 
 bool is_enabled(const scollectd::type_instance_id& id) {
-    return get_impl().get_value_list_map()[id]->is_enabled();
+    return seastar::metrics::impl::get_value_map().at(to_metrics_id(id))->is_enabled();
 }
 
 void enable(const scollectd::type_instance_id& id, bool enable) {
-    get_impl().get_value_list_map()[id]->set_enabled(enable);
+    seastar::metrics::impl::get_value_map().at(to_metrics_id(id))->set_enabled(enable);
 }
 
 type_instance_id plugin_instance_metrics::add_impl(const typed_value& v) {
@@ -899,14 +894,8 @@ type_id type_id_for(known_type t) {
     }
 }
 
-value_map get_value_map() {
-    value_map res;
-    for (auto i : get_impl().get_value_list_map()) {
-        if (i.second->is_enabled()) {
-            res[i.first] = raw_to_value(i.second);
-        }
-    }
-    return res;
+seastar::metrics::impl::value_map get_value_map() {
+    return seastar::metrics::impl::get_value_map();
 }
 
 }

core/scollectd-impl.hh
@@ -23,6 +23,7 @@
 
 #include "core/scollectd.hh"
 #include "core/reactor.hh"
+#include "core/metrics_api.hh"
 
 namespace scollectd {
 
@@ -45,7 +46,7 @@ class impl {
     double _avg = 0;
 
 public:
-    typedef std::map<type_instance_id, shared_ptr<value_list> > value_list_map;
+    typedef seastar::metrics::impl::value_map value_list_map;
     typedef value_list_map::value_type value_list_pair;
 
     void add_polled(const type_instance_id & id,
@@ -74,7 +75,9 @@ public:
     std::vector<type_instance_id> get_instance_ids() const;
     sstring get_collectd_description_str(const scollectd::type_instance_id&) const;
 private:
-    value_list_map _values;
+    const value_list_map& values() const {
+        return seastar::metrics::impl::get_value_map();
+    }
     registrations _regs;
 };
 

core/scollectd.hh
@@ -41,6 +41,8 @@
 #include "core/print.hh"
 #include "util/log.hh"
 
+#include "core/metrics_api.hh"
+
 /**
  * Implementation of rudimentary collectd data gathering.
  *
@@ -83,13 +85,7 @@ namespace scollectd {
 
 extern seastar::logger logger;
 
-// The value binding data types
-enum class data_type : uint8_t {
-    COUNTER, // unsigned int 64
-    GAUGE, // double
-    DERIVE, // signed int 64
-    ABSOLUTE, // unsigned int 64
-};
+using data_type = seastar::metrics::impl::data_type;
 
 enum class known_type {
     // from types.db. Defined collectd types (type_id) selection.
@@ -291,38 +287,14 @@ static inline typed<T> make_typed(data_type type, T&& t) {
     return typed<T>(type, std::forward<T>(t));
 }
 
-typedef sstring plugin_id;
-typedef sstring plugin_instance_id;
-typedef sstring type_id;
-typedef sstring type_instance;
+using plugin_id = seastar::metrics::group_name_type;
+using plugin_instance_id = seastar::metrics::instance_id_type;
+using type_id = seastar::metrics::measurement_type;
+using type_instance = seastar::metrics::sub_measurement_type;
 
 type_id type_id_for(known_type);
 
-/*
- * Human-readable description of a metric/group.
- * Uses a separate class to deal with type resolution
- *
- * Add this to either typed wrappers or raw counter
- * registration:
- *
- * <code>
- * add_polled_metric(type_instance_id("my_plug", "my_inst", "total_operations", "value"),
- *  description("This is a counter description that should have content"),
- *  make_typed(data_type::DERIVE, _my_counter_var)
- *  );
- * </code>
- *
- */
-class description {
-public:
-    description(sstring s = sstring()) : _s(std::move(s))
-    {}
-    const sstring& str() const {
-        return _s;
-    }
-private:
-    sstring _s;
-};
+using description = seastar::metrics::description;
 
 static constexpr unsigned max_collectd_field_text_len = 63;
 
@@ -350,6 +322,10 @@ public:
         truncate(_plugin_instance, "plugin_instance");
         truncate(_type, "type");
         truncate(_type_instance, "type_instance");
+    }
+    type_instance_id(const seastar::metrics::impl::metric_id &id) : _plugin(id.group_name()),
+            _plugin_instance(id.instance_id()), _type(id.measurement()),
+            _type_instance(id.sub_measurement()) {
     }
     type_instance_id(type_instance_id &&) = default;
     type_instance_id(const type_instance_id &) = default;
@@ -408,9 +384,7 @@ struct registration {
     }
     registration(const registration&) = delete;
     registration(registration&&) = default;
-    ~registration() {
-        unregister();
-    }
+    ~registration();
     registration & operator=(const registration&) = delete;
     registration & operator=(registration&&) = default;
 
@@ -802,35 +776,36 @@ static notify_function create_explicit_metric(const plugin_id & plugin,
             type_instance_id(plugin, plugin_instance, type, type_instance),
             std::forward<_Args>(args)...);
 }
-template<typename ... _Args>
-static type_instance_id add_polled_metric(const type_instance_id & id,
-        _Args&& ... args) {
-    return add_polled_metric(id, description(), std::forward<_Args>(args)...);
-}
-template<typename ... _Args>
+
+seastar::metrics::impl::metric_id to_metrics_id(const type_instance_id & id);
+
+template<typename Arg>
 static type_instance_id add_polled_metric(const type_instance_id & id, description d,
-        _Args&& ... args) {
-    typedef decltype(make_type_instance(std::move(d), std::forward<_Args>(args)...)) impl_type;
-    add_polled(id,
-            ::make_shared<impl_type>(
-                    make_type_instance(std::move(d), std::forward<_Args>(args)...)));
+        Arg&& arg, bool enabled = true) {
+    namespace sm = seastar::metrics::impl;
+    shared_ptr<sm::registered_metric> rm =
+                ::make_shared<sm::registered_metric>(arg.type, sm::make_function(arg.value, arg.type), d, enabled);
+    seastar::metrics::impl::get_local_impl().add_registration(to_metrics_id(id), rm);
     return id;
 }
 
-template<typename ... Args>
+template<typename Arg>
+static type_instance_id add_polled_metric(const type_instance_id & id,
+        Arg&& arg) {
+    return std::move(add_polled_metric(id, description(), std::forward<Arg>(arg)));
+}
+
+
+template<typename Args>
 static type_instance_id add_disabled_polled_metric(const type_instance_id & id, description d,
-        Args&& ... args) {
-    typedef decltype(make_type_instance(std::move(d), std::forward<Args>(args)...)) impl_type;
-    add_polled(id,
-            ::make_shared<impl_type>(
-                    make_type_instance(std::move(d), std::forward<Args>(args)...)), false);
-    return id;
+        Args&& arg) {
+    return add_polled_metric(id, d, std::forward<Args>(arg), false);
 }
 
-template<typename ... Args>
+template<typename Args>
 static type_instance_id add_disabled_polled_metric(const type_instance_id & id,
-        Args&& ... args) {
-    return add_disabled_polled_metric(id, description(), std::forward<Args>(args)...);
+        Args&& args) {
+    return add_disabled_polled_metric(id, description(), std::forward<Args>(args));
 }
 
 // "Explicit" metric sends. Sends a single value list as a message.

core/scollectd_api.hh
@@ -6,63 +6,11 @@
 #define CORE_SCOLLECTD_API_HH_
 
 #include "core/scollectd.hh"
+#include "core/metrics_api.hh"
 
 namespace scollectd {
 
-struct collectd_value {
-    union {
-        double _d;
-        uint64_t _ui;
-        int64_t _i;
-    } u;
-    scollectd::data_type _type;
-    collectd_value()
-            : _type(data_type::GAUGE) {
-    }
-    collectd_value(data_type t, uint64_t i)
-            : _type(t) {
-        u._ui = i;
-    }
-
-    scollectd::data_type type() const {
-        return _type;
-    }
-
-    double d() const {
-        return u._d;
-    }
-
-    uint64_t ui() const {
-        return u._ui;
-    }
-    int64_t i() const {
-        return u._i;
-    }
-
-    collectd_value& operator=(const collectd_value& c) = default;
-
-    collectd_value& operator+=(const collectd_value& c) {
-        *this = *this + c;
-        return *this;
-    }
-
-    collectd_value operator+(const collectd_value& c) {
-        collectd_value res(*this);
-        switch (_type) {
-        case data_type::GAUGE:
-            res.u._d += c.u._d;
-            break;
-        case data_type::DERIVE:
-            res.u._i += c.u._i;
-            break;
-        default:
-            res.u._ui += c.u._ui;
-            break;
-        }
-        return res;
-    }
-};
-typedef std::map<type_instance_id, std::vector<collectd_value> > value_map;
+using collectd_value  = seastar::metrics::impl::metric_value;
 
 std::vector<collectd_value> get_collectd_value(
         const scollectd::type_instance_id& id);
@@ -80,7 +28,7 @@ bool is_enabled(const scollectd::type_instance_id& id);
 void enable(const scollectd::type_instance_id& id, bool enable);
 
 
-value_map get_value_map();
+seastar::metrics::impl::value_map get_value_map();
 }
 
 #endif /* CORE_SCOLLECTD_API_HH_ */

commit d3244fa07f1c1b85b3654e5545cc18be2b9724be

scollectd: Fix serialization of GAUGE-typed values
According to the spec [1], GAGUE-typed values should be serialized as
little endian, not big endian like the rest.

Broken in commit 83549367df6936fe844c8a6635baeeac0505a427.

Refs scylladb/scylla#1858

Message-Id: <1479501756-23004-1-git-send-email-tgrabiec@scylladb.com>

core/scollectd.cc
@@ -32,6 +32,7 @@
 #include "core/future-util.hh"
 #include "scollectd_api.hh"
 #include "core/metrics_api.hh"
+#include "core/byteorder.hh"
 
 bool scollectd::type_instance_id::operator<(
         const scollectd::type_instance_id& id2) const {
@@ -158,6 +159,28 @@ struct cpwriter {
         write(p, e);
         return *this;
     }
+    template<typename T>
+    typename std::enable_if<std::is_integral<T>::value, cpwriter &>::type write_le(const T & t) {
+        T tmp = cpu_to_le(t);
+        auto * p = reinterpret_cast<const uint8_t *>(&tmp);
+        auto * e = p + sizeof(T);
+        write(p, e);
+        return *this;
+    }
+    void write_value(const seastar::metrics::impl::metric_value& v) {
+        switch (v.type()) {
+            case data_type::GAUGE:
+                write_le(v.ui());
+                break;
+            case data_type::COUNTER:
+            case data_type::DERIVE:
+            case data_type::ABSOLUTE:
+                write(v.ui()); // big endian
+                break;
+            default:
+                assert(0);
+        }
+    }
     cpwriter & write(const sstring & s) {
         write(s.begin(), s.end() + 1); // include \0
         return *this;
@@ -206,7 +229,7 @@ struct cpwriter {
             write(uint16_t(sz));
             write(uint16_t(1));
             write(static_cast<uint8_t>(v.type()));
-            write(v.ui());
+            write_value(v);
         }
         return *this;
     }