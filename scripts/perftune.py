scylladb/seastar

commit df5f74ac834850d493a854d53112ddc431372bda

perftune: fix perftune.py IndexError when NIC uses less IRQs than requested.
This patch fixes an IndexError exception raised when NICs requested more
IRQs (typically MSI ones) than they finally use.

In this case the 'all_irqs' list will reference IRQs that will not be
present on the 'irqs2procline' dict keys generated from /proc/interrupts
and will raise an IndexError exception when we filter them out using
regular expressions.

Signed-off-by: Alexys Jacob <ultrabug@gentoo.org>
Message-Id: <20170509072537.12545-1-ultrabug@gentoo.org>

scripts/perftune.py
@@ -476,7 +476,8 @@ class NetPerfTuner(PerfTunerBase):
         Otherwise, we will use only IRQs which names fit one of the patterns above.
         """
         irqs2procline = get_irqs2procline_map()
-        all_irqs = learn_all_irqs_one("/sys/class/net/{}/device".format(iface), irqs2procline, iface)
+        # filter 'all_irqs' to only reference valid keys from 'irqs2procline' and avoid an IndexError on the 'irqs' search below
+        all_irqs = set(learn_all_irqs_one("/sys/class/net/{}/device".format(iface), irqs2procline, iface)).intersection(irqs2procline.keys())
         fp_irqs_re = re.compile("\-TxRx\-|\-fp\-|\-Tx\-Rx\-")
         irqs = list(filter(lambda irq : fp_irqs_re.search(irqs2procline[irq]), all_irqs))
         if len(irqs) > 0:

commit cb003558eb8c7765e9e0c914915edb3500a8798e

perftune.py: fix regression introduced in df5f74ac
While fixing "IndexError when NIC uses less IRQs than requested" I introduced
a regression by changing the type of 'all_irqs' from 'list' to 'set'.

This raises a "TypeError: 'set' object does not support indexing" exception
on NetPerfTuner.__setup_one_hw_iface method when trying to tune the network.

Signed-off-by: Alexys Jacob <ultrabug@gentoo.org>
Message-Id: <20170518151219.17738-1-ultrabug@gentoo.org>

scripts/perftune.py
@@ -488,7 +488,7 @@ class NetPerfTuner(PerfTunerBase):
         if len(irqs) > 0:
             return irqs
         else:
-            return all_irqs
+            return list(all_irqs)
 
     def __learn_irqs(self):
         """