326f0cc8bc08cac0edbbc65dca527a76581ca88d
Gleb Natapov <gleb@scylladb.com>
2017-07-30 12:38:30 +0300

    configure: disable exception scalability hack on debug build
    ASAN uses dl_iterate_phdr for its black magic, so leave it alone.
    
    Message-Id: <20170730093830.GS20001@scylladb.com>

--- configure.py
+++ configure.py
@@ -190,7 +190,7 @@ modes = {
     'debug': {
         'sanitize': '-fsanitize=address -fsanitize=leak -fsanitize=undefined',
         'sanitize_libs': '-lasan -lubsan',
-        'opt': '-O0 -DDEBUG -DDEBUG_SHARED_PTR -DDEFAULT_ALLOCATOR -DSEASTAR_THREAD_STACK_GUARDS',
+        'opt': '-O0 -DDEBUG -DDEBUG_SHARED_PTR -DDEFAULT_ALLOCATOR -DSEASTAR_THREAD_STACK_GUARDS -DNO_EXCEPTION_HACK',
         'libs': '',
         'cares_opts': '-DCARES_STATIC=ON -DCARES_SHARED=OFF -DCMAKE_BUILD_TYPE=Debug',
     },
