021a2209016433b32e4b02c4ccb64363d90fc6f6
Amnon Heiman <amnon@scylladb.com>
2017-12-07 08:47:43 +0200

    http: api_docs add swagger 2.0 support
    The main difference between swagger 1.2 and swagger 2.0 is the use of a
    single file for the swagger definition.
    
    A single file makes the API hard to maintain and makes it harder to
    enable a bunch of APIs together, a feature that is useful during
    startup.
    
    In Swagger 1.2, the api_docs class generates the swagger file for the
    API table of content. The Swagger-UI and other clients read the table of
    content file and use it to fetch the rest of the API.
    
    This patch adds a swagger 2.0 support that mimic this behavior.
    
    In the swagger 2.0 all the apis are under the paths and all the type
    definitions are under "definitions".
    
    This patch create a parallel class hierarchy for swagger 2.0 were
    mutual functionality was put in a base classes.
    
    api_registry_20 is a handler class that generates a unified file, it
    uses api_docs_20 to holds the different doc entry.
    
    The registration in 2.0 is done with a doc_entry which is a
    noncopyable_function. This also adds dynamic API support, allowing
    swagger definition generation dynamically.
    
    Signed-off-by: Amnon Heiman <amnon@scylladb.com>
    Message-Id: <20171207064743.19573-1-amnon@scylladb.com>

--- http/api_docs.hh
+++ http/api_docs.hh
@@ -26,6 +26,7 @@
 #include "routes.hh"
 #include "transformers.hh"
 #include <string>
+#include "util/noncopyable_function.hh"
 
 namespace seastar {
 
@@ -93,19 +94,34 @@ struct api_docs : public json::json_base {
     }
 };
 
-class api_registry : public handler_base {
+class api_registry_base : public handler_base {
+protected:
     sstring _base_path;
     sstring _file_directory;
-    api_docs _docs;
     routes& _routes;
 
 public:
-    api_registry(routes& routes, const sstring& file_directory,
+    api_registry_base(routes& routes, const sstring& file_directory,
             const sstring& base_path)
             : _base_path(base_path), _file_directory(file_directory), _routes(
                     routes) {
-        _routes.put(GET, _base_path, this);
     }
+
+    void set_route(handler_base* h) {
+        _routes.put(GET, _base_path, h);
+    }
+    virtual ~api_registry_base() = default;
+};
+
+class api_registry : public api_registry_base {
+    api_docs _docs;
+public:
+    api_registry(routes& routes, const sstring& file_directory,
+            const sstring& base_path)
+            : api_registry_base(routes, file_directory, base_path) {
+        set_route(this);
+    }
+
     future<std::unique_ptr<reply>> handle(const sstring& path,
             std::unique_ptr<request> req, std::unique_ptr<reply> rep) override {
         rep->_content = json::formatter::to_json(_docs);
@@ -128,17 +144,24 @@ public:
     }
 };
 
-class api_registry_builder {
+class api_registry_builder_base {
+protected:
     sstring _file_directory;
     sstring _base_path;
-
-public:
     static const sstring DEFAULT_DIR;
     static const sstring DEFAULT_PATH;
-
-    api_registry_builder(const sstring& file_directory = DEFAULT_DIR,
+public:
+    api_registry_builder_base(const sstring& file_directory = DEFAULT_DIR,
             const sstring& base_path = DEFAULT_PATH)
             : _file_directory(file_directory), _base_path(base_path) {
+    }
+};
+
+class api_registry_builder : public api_registry_builder_base {
+public:
+    api_registry_builder(const sstring& file_directory = DEFAULT_DIR,
+            const sstring& base_path = DEFAULT_PATH)
+            : api_registry_builder_base(file_directory, base_path) {
     }
 
     void set_api_doc(routes& r) {
@@ -154,6 +177,159 @@ public:
             static_cast<api_registry*>(h)->reg(api, description, alternative_path);
         };
     }
+};
+
+using doc_entry = noncopyable_function<future<>(output_stream<char>&)>;
+
+/*!
+ * \brief a helper function that creates a reader from a file
+ */
+
+doc_entry get_file_reader(sstring file_name);
+
+/*!
+ * \brief An api doc that support swagger version 2.0
+ *
+ * The result is a unified JSON file with the swagger definitions.
+ *
+ * The file content is a concatenation of the doc_entry by the order of
+ * their entry.
+ *
+ * Definitions will be added under the definition section
+ *
+ * typical usage:
+ *
+ * First entry:
+ *
+  {
+  "swagger": "2.0",
+  "host": "localhost:10000",
+  "basePath": "/v2",
+  "paths": {
+
+ * entry:
+ "/config/{id}": {
+      "get": {
+        "description": "Return a config value",
+        "operationId": "findConfigId",
+        "produces": [
+          "application/json"
+        ],
+        }
+        }
+ *
+ * Closing the entries:
+  },
+
+  "definitions": {
+  .....
+
+  .....
+  }
+}
+ *
+ */
+class api_docs_20 {
+    std::vector<doc_entry> _apis;
+    content_replace _transform;
+    std::vector<doc_entry> _definitions;
+
+public:
+    future<> write(output_stream<char>&&, std::unique_ptr<request> req);
+
+    void add_api(doc_entry&& f) {
+        _apis.emplace_back(std::move(f));
+    }
+
+    void add_definition(doc_entry&& f) {
+        _definitions.emplace_back(std::move(f));
+    }
+};
+
+class api_registry_20 : public api_registry_base {
+    api_docs_20 _docs;
+public:
+    api_registry_20(routes& routes, const sstring& file_directory,
+            const sstring& base_path)
+            : api_registry_base(routes, file_directory, base_path) {
+        set_route(this);
+    }
+
+    future<std::unique_ptr<reply>> handle(const sstring& path,
+            std::unique_ptr<request> req, std::unique_ptr<reply> rep) override {
+        rep->write_body("json", [this, req = std::move(req)] (output_stream<char>&& os) mutable {
+            return _docs.write(std::move(os), std::move(req));
+        });
+        return make_ready_future<std::unique_ptr<reply>>(std::move(rep));
+    }
+
+    virtual void reg(doc_entry&& f) {
+        _docs.add_api(std::move(f));
+    }
+
+    virtual void add_definition(doc_entry&& f) {
+        _docs.add_definition(std::move(f));
+    }
+};
+
+class api_registry_builder20 : public api_registry_builder_base {
+    api_registry_20* get_register_base(routes& r) {
+        auto h = r.get_exact_match(GET, _base_path);
+        if (h) {
+            // if a handler is found, it was added there by the api_registry_builder
+            // with the set_api_doc method, so we know it's the type
+            return static_cast<api_registry_20*>(h);
+        }
+        return nullptr;
+    }
+
+public:
+    api_registry_builder20(const sstring& file_directory = DEFAULT_DIR,
+            const sstring& base_path = DEFAULT_PATH)
+            : api_registry_builder_base(file_directory, base_path) {
+    }
+
+    void set_api_doc(routes& r) {
+        new api_registry_20(r, _file_directory, _base_path);
+    }
+
+    /*!
+     * \brief register a doc_entry
+     * This doc_entry can be used to either take the definition from a file
+     * or generate them dynamically.
+     */
+    void register_function(routes& r, doc_entry&& f) {
+        auto h = get_register_base(r);
+        if (h) {
+            h->reg(std::move(f));
+        }
+    }
+    /*!
+     * \brief register an API
+     */
+    void register_api_file(routes& r, const sstring& api) {
+        register_function(r, get_file_reader(_file_directory + "/" + api + ".json"));
+    }
+
+
+    /*!
+     * Add a footer doc_entry
+     */
+    void add_definition(routes& r, doc_entry&& f) {
+        auto h = get_register_base(r);
+        if (h) {
+            h->add_definition(std::move(f));
+        }
+
+    }
+
+    /*!
+     * Add a definition file
+     */
+    void add_definitions_file(routes& r, const sstring& file) {
+        add_definition(r, get_file_reader(_file_directory + file + ".def.json" ));
+    }
+
 };
 
 }
