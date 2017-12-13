9192a0c1414b1a9c3961932d9ddd34d7a56a1d79
Avi Kivity <avi@scylladb.com>
2017-12-13 18:23:48 +0200

    systemwide_memory_barrier: implement using membarrier(MEMBARRIER_CMD_PRIVATE_EXPEDITED)
    Starting with Linux 4.14, the Linux membarrier() syscall has an expedited option
    that sends IPIs to all CPUs that host active threads from our process. This is
    faster than the current mprotect() method, works correctly on ARM, and most
    importantly does not trigger lock contention in the kernel.
    
    Detect this facility during compile-time and run-time and use it if
    available.
    
    strace shows the new syscall as:
    
      4029  18:17:22.113276 membarrier(MEMBARRIER_CMD_PRIVATE_EXPEDITED, 0) = 0 <0.000006>

--- core/systemwide_memory_barrier.cc
+++ core/systemwide_memory_barrier.cc
@@ -26,11 +26,54 @@
 #include <cassert>
 #include <atomic>
 
+#if __has_include("linux/membarrier.h")
+#define HAS_MEMBARRIER
+#include <linux/membarrier.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#endif
+
 namespace seastar {
+
+
+#ifdef HAS_MEMBARRIER
+
+static bool has_native_membarrier = [] {
+    auto r = syscall(SYS_membarrier, MEMBARRIER_CMD_QUERY, 0);
+    if (r == -1) {
+        return false;
+    }
+    int needed = MEMBARRIER_CMD_PRIVATE_EXPEDITED | MEMBARRIER_CMD_REGISTER_PRIVATE_EXPEDITED;
+    if ((r & needed) != needed) {
+        return false;
+    }
+    syscall(SYS_membarrier, MEMBARRIER_CMD_REGISTER_PRIVATE_EXPEDITED, 0);
+    return true;
+}();
+
+static bool try_native_membarrier() {
+    if (has_native_membarrier) {
+        syscall(SYS_membarrier, MEMBARRIER_CMD_PRIVATE_EXPEDITED, 0);
+        return true;
+    }
+    return false;
+}
+
+#else
+
+static bool try_native_membarrier() {
+    return false;
+}
+
+#endif
 
 // cause all threads to invoke a full memory barrier
 void
 systemwide_memory_barrier() {
+    if (try_native_membarrier()) {
+        return;
+    }
+
     // FIXME: use sys_membarrier() when available
     static thread_local char* mem = [] {
        void* mem = mmap(nullptr, getpagesize(),
@@ -64,6 +107,10 @@ struct alignas(cache_line_size) aligned_flag {
 static aligned_flag membarrier_lock;
 
 bool try_systemwide_memory_barrier() {
+    if (try_native_membarrier()) {
+        return true;
+    }
+
     if (!membarrier_lock.try_lock()) {
         return false;
     }
