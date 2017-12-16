27158cec33e0b388013088561d2a8107087ab6b2
Avi Kivity <avi@scylladb.com>
2017-12-16 14:33:47 +0200

    linux-aio: try to perform io_getevents in userspace
    If enough events are available (or if the timeout is zero), we
    can perform io_getevents entirely in userspace. This is possible
    because the completion event ring is mapped into userspace using
    a stable data structure.
    
    libaio implements this for the special case of zero available events.
    
    Credit to QEMU for pioneering this optimization.

--- core/linux-aio.cc
+++ core/linux-aio.cc
@@ -22,10 +22,32 @@
 #include "linux-aio.hh"
 #include <unistd.h>
 #include <sys/syscall.h>
+#include <atomic>
+#include <algorithm>
 
 namespace seastar {
 
 namespace internal {
+
+
+struct linux_aio_ring {
+    uint32_t id;
+    uint32_t nr;
+    std::atomic<uint32_t> head;
+    std::atomic<uint32_t> tail;
+    uint32_t magic;
+    uint32_t compat_features;
+    uint32_t incompat_features;
+    uint32_t header_length;
+};
+
+static linux_aio_ring* to_ring(::aio_context_t io_context) {
+    return reinterpret_cast<linux_aio_ring*>(uintptr_t(io_context));
+}
+
+static bool usable(const linux_aio_ring* ring) {
+    return ring->magic == 0xa10a10a1 && ring->incompat_features == 0;
+}
 
 int io_setup(int nr_events, ::aio_context_t* io_context) {
     return ::syscall(SYS_io_setup, nr_events, io_context);
@@ -44,6 +66,50 @@ int io_cancel(::aio_context_t io_context, ::iocb* iocb, ::io_event* result) {
 }
 
 int io_getevents(::aio_context_t io_context, long min_nr, long nr, ::io_event* events, const ::timespec* timeout) {
+    auto ring = to_ring(io_context);
+    if (usable(ring)) {
+        // Try to complete in userspace, if enough available events,
+        // or if the timeout is zero
+
+        // We're the only writer to ->head, so we can load with memory_order_relaxed (assuming
+        // only a single thread calls io_getevents()).
+        auto head = ring->head.load(std::memory_order_relaxed);
+        // The kernel will write to the ring from an interrupt and then release with a write
+        // to ring->tail, so we must memory_order_acquire here.
+        auto tail = ring->tail.load(std::memory_order_acquire); // kernel writes from interrupts
+        auto available = tail - head;
+        if (tail < head) {
+            available += ring->nr;
+        }
+        if (available >= uint32_t(min_nr)
+                || (timeout && timeout->tv_sec == 0 && timeout->tv_nsec == 0)) {
+            if (!available) {
+                return 0;
+            }
+            auto ring_events = reinterpret_cast<const ::io_event*>(uintptr_t(io_context) + ring->header_length);
+            auto now = std::min<uint32_t>(nr, available);
+            auto start = ring_events + head;
+            auto end = start + now;
+            if (head + now > ring->nr) {
+                end -= ring->nr;
+            }
+            if (end > start) {
+                std::copy(start, end, events);
+            } else {
+                auto p = std::copy(start, ring_events + ring->nr, events);
+                std::copy(ring_events, end, p);
+            }
+            head += now;
+            if (head >= ring->nr) {
+                head -= ring->nr;
+            }
+            // The kernel will read ring->head and update its view of how many entries
+            // in the ring are available, so memory_order_release to make sure any ring
+            // accesses are completed before the update to ring->head is visible.
+            ring->head.store(head, std::memory_order_release);
+            return now;
+        }
+    }
     return ::syscall(SYS_io_getevents, io_context, min_nr, nr, events, timeout);
 }
 
