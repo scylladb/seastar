0e52242c7998b7419205c935297c4b94d60c517d
Duarte Nunes <duarte@scylladb.com>
2018-02-09 14:42:35 +0000

    tests/test-utils: Add macro for running tests within a seastar thread
    This patch adds the SEASTAR_THREAD_TEST_CASE macro, which behaves like
    the SEASTAR_TEST_CASE one except it calls the test code in the context
    of a seastar thread. This allows most seastar tests to be written with
    one less indentation level.
    
    Signed-off-by: Duarte Nunes <duarte@scylladb.com>
    Message-Id: <20180209144235.32083-1-duarte@scylladb.com>

--- tests/test-utils.hh
+++ tests/test-utils.hh
@@ -49,5 +49,18 @@ public:
     static name name ## _instance; \
     future<> name::run_test_case()
 
+#define SEASTAR_THREAD_TEST_CASE(name) \
+    struct name : public seastar_test { \
+        const char* get_test_file() override { return __FILE__; } \
+        const char* get_name() override { return #name; } \
+        future<> run_test_case() override { \
+            return async([this] { \
+                do_run_test_case(); \
+            }); \
+        } \
+        void do_run_test_case(); \
+    }; \
+    static name name ## _instance; \
+    void name::do_run_test_case() \
 
 }
