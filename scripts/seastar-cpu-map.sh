fd507b70623f7775c03501b010334228c71b1276
Moreno Garcia <moreno@scylladb.com>
2018-03-05 19:30:21 -0300

    Simple shellscript to find out which logical CPU's shards are mapped to
    Usually a seastar application will export its metrics according to their seastar
    shards, and all performance analysis happen based on that. However, when we need
    to use external Linux tools like perf, that needs to happen in the CPU level,
    where the concept of seastar shard doesn't apply.
    This patch introduces a simple script (scripts/seastar_cpu_map.sh) that will
    read the task list of a seastar application and use the fact that threads are
    named with their shard names to produce a logical CPU mapping that is useful
    outside of seastar.
    
    Here is an example output from the script:
    shard: 0, cpu: 2
    shard: 1, cpu: 3
    shard: 0, cpu: 2
    shard: 1, cpu: 3
    shard: 0, cpu: 2
    shard: 1, cpu: 3
    shard: 0, cpu: 2
    shard: 1, cpu: 3
    
    Variable names were changed for better readability, shards are now correctly
    matched, code was shortened.
    
    Signed-off-by: Moreno Garcia <moreno@scylladb.com>
    Message-Id: <20180305223021.29036-1-moreno@scylladb.com>

---
+++ scripts/seastar-cpu-map.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# !
+# ! Usage: ./seastar-cpu-map.sh -p <process_PID> -n <process_Name> -s (optional) <shard>
+# !
+# ! List CPU affinity for a particular running process
+# !  providing a map of threads -> shard, for any seastar apps.
+# ! Ex.: ./seastar-cpu-map.sh -n scylla
+# !      ./seastar-cpu-map.sh -n scylla -s 0
+# !      ./seastar-cpu-map.sh -p 34
+# !      ./seastar-cpu-map.sh -p 32 -s 12
+
+usage() {
+    cat $0 | grep ^"# !" | cut -d"!" -f2-
+}
+
+while getopts 'p:n:s:' option; do
+  case "$option" in
+    p) PID=$OPTARG
+        ;;
+    n) PID=`pidof $OPTARG`
+        ;;
+    s) SHARD=$OPTARG
+        ;;
+    :) printf "missing argument for -%s\n" "$OPTARG" >&2
+       usage >&2
+       exit 1
+       ;;
+    \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+       usage >&2
+       exit 1
+       ;;
+  esac
+done
+
+if [ $# -eq 0 ]; then usage >&2; exit 1; fi
+
+if [ -e "/proc/$PID/task" ]; then
+  # get list of threads for given PID
+  THREADS=`ls /proc/$PID/task`
+  for i in $THREADS; do
+    # get shards from threads
+    # there were three options here to get the shard number:
+    # reactor-xx, syscall-xx and timer-xx
+    # syscall was preferred because reactor as a special case (reactor-0 is called scylla)
+    SYSCALL=`grep syscall /proc/$i/comm | cut -d"-" -f2`
+    if [ -n "$SYSCALL" ] && [ "$SYSCALL" = "$SHARD" ]; then
+      echo -e "shard: $SYSCALL, cpu:$(taskset -c -p $i | cut -d":" -f2)"
+    elif [ -n "$SYSCALL" ] && [ -z "$SHARD" ]; then
+      echo -e "shard: $SYSCALL, cpu:$(taskset -c -p $i | cut -d":" -f2)"
+    fi
+  done
+else
+  echo "Process does not exist"
+fi
