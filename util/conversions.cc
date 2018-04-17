fbf66b02f0e7a20208c90a5799115d32e8577043
Glauber Costa <glauber@scylladb.com>
2018-04-17 17:57:21 -0400

    utils: adjust identation
    last patch was idented for patch readability.
    
    Signed-off-by: Glauber Costa <glauber@scylladb.com>

--- util/conversions.cc
+++ util/conversions.cc
@@ -34,13 +34,13 @@ size_t parse_memory_size(std::string s) {
     if (s.size()) {
         auto c = s[s.size() - 1];
         if (!isdigit(c)) {
-        static std::string suffixes = "kMGT";
-        auto pos = suffixes.find(c);
-        if (pos == suffixes.npos) {
-            throw std::runtime_error(sprint("Cannot parse memory size '%s'", s));
-        }
-        factor <<= (pos + 1) * 10;
-        s = s.substr(0, s.size() - 1);
+            static std::string suffixes = "kMGT";
+            auto pos = suffixes.find(c);
+            if (pos == suffixes.npos) {
+                throw std::runtime_error(sprint("Cannot parse memory size '%s'", s));
+            }
+            factor <<= (pos + 1) * 10;
+            s = s.substr(0, s.size() - 1);
         }
     }
     return boost::lexical_cast<size_t>(s) * factor;
