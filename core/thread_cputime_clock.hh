d8610f0deef4c4ead8202b5df342587a7310e0af
Glauber Costa <glauber@scylladb.com>
2018-05-13 06:28:52 -0400

    thread_cputime_clock: add a clock interface for CLOCK_THREAD_CPUTIME_ID
    clock_gettime, if called with CLOCK_THREAD_CPUTIME_ID will give us an
    idea of how much time the current thread consumed. Using this can be
    useful for us to gain visibility of what's happening in the system.
    
    To make the integration with the rest of the code better, we can wrap
    it in an interface similar to std::chrono's clocks.
    
    Signed-off-by: Glauber Costa <glauber@scylladb.com>

---
+++ core/thread_cputime_clock.hh
@@ -0,0 +1,47 @@
+/*
+ * This file is open source software, licensed to you under the terms
+ * of the Apache License, Version 2.0 (the "License").  See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership.  You may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/*
+ * Copyright (C) 2018 ScyllaDB
+ */
+
+#pragma once
+
+#include <chrono>
+#include <time.h>
+
+namespace seastar {
+
+class thread_cputime_clock {
+public:
+    using rep = int64_t;
+    using period = std::chrono::nanoseconds::period;
+    using duration = std::chrono::duration<rep, period>;
+    using time_point = std::chrono::time_point<thread_cputime_clock, duration>;
+public:
+    static time_point now() {
+        using namespace std::chrono_literals;
+
+        struct timespec tp;
+        auto ret = clock_gettime(CLOCK_THREAD_CPUTIME_ID, &tp);
+        assert(ret == 0);
+        return time_point(tp.tv_nsec * 1ns + tp.tv_sec * 1s);
+    }
+};
+
+}
+
