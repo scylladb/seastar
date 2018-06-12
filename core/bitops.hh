ded9502c13f408abbdd96bd77758d5f68f8486b0
Botond Dénes <bdenes@scylladb.com>
2018-06-12 11:49:25 +0300

    bitops.hh/log2ceil(): add special case for n == 1
    The current implementation will lead to invalid input passed to the
    builtin `clz()` when n is 1. To avoid this add a special case for n == 1
    and return the precomputed value for it.
    
    Signed-off-by: Botond Dénes <bdenes@scylladb.com>
    Message-Id: <97a95fea2d44a63390e42878948e926e2a4d04fa.1528793345.git.bdenes@scylladb.com>

--- core/bitops.hh
+++ core/bitops.hh
@@ -59,6 +59,9 @@ constexpr unsigned count_trailing_zeros(unsigned long long x) {
 template<typename T>
 //requires stdx::is_integral_v<T>
 inline constexpr unsigned log2ceil(T n) {
+    if (n == 1) {
+        return 0;
+    }
     return std::numeric_limits<T>::digits - count_leading_zeros(n - 1);
 }
 
