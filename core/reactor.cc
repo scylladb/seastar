d5ad5f84dede3a5a8abdaba4184c191b947d91d0
Glauber Costa <glauber@scylladb.com>
2018-08-08 13:40:28 -0400

    properly configure I/O Scheduler when --max-io-requests is passed
    We now have scaling between reads and writes as part of the new I/O
    Scheduler. However, when a capacity is explicitly set, which means the
    old --max-io-requests is used, we end up miscalculating that, and as a
    result the I/O Scheduler fails to uphold the shares.
    
    If we are using the legacy config, which didn't differentiate reads from
    writes, then we should hard code the ratio to 1.
    
    Fixes #482
    
    Signed-off-by: Glauber Costa <glauber@scylladb.com>
    Message-Id: <20180808174029.12521-4-glauber@scylladb.com>

--- core/reactor.cc
+++ core/reactor.cc
@@ -4065,6 +4065,7 @@ public:
         uint64_t max_iops = std::max(p.read_req_rate, p.write_req_rate);
 
         cfg.capacity = per_io_queue(_capacity);
+        if (cfg.capacity == std::numeric_limits<unsigned>::max()) {
         cfg.disk_bytes_write_to_read_multiplier = (io_queue::read_request_base_count * p.read_bytes_rate) / p.write_bytes_rate;
         cfg.disk_req_write_to_read_multiplier = (io_queue::read_request_base_count * p.read_req_rate) / p.write_req_rate;
         cfg.max_req_count = max_bandwidth == std::numeric_limits<uint64_t>::max()
@@ -4074,6 +4075,10 @@ public:
             ? std::numeric_limits<unsigned>::max()
             : io_queue::read_request_base_count * per_io_queue(max_bandwidth * _latency_goal.count());
         cfg.mountpoint = p.mountpoint;
+        } else {
+            cfg.disk_bytes_write_to_read_multiplier = 1;
+            cfg.disk_req_write_to_read_multiplier = 1;
+        }
         return cfg;
     }
 
