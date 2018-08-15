733edaa411d9aa3679940991f60d710323822a09
Duarte Nunes <duarte@scylladb.com>
2018-08-15 10:51:08 +0100

    Revert "foreign_ptr: allow waiting for the destruction of the managed ptr"
    Will have no immediate users, so omit this for now to avoid
    complicating a complicated interface.
    
    This reverts commit 64188c51319179756bc99db4146092c267e61921.

--- core/sharded.hh
+++ core/sharded.hh
@@ -582,16 +582,12 @@ private:
     PtrType _value;
     unsigned _cpu;
 private:
-    future<> destroy(PtrType p, unsigned cpu) {
+    void destroy(PtrType p, unsigned cpu) {
         if (p && engine().cpu_id() != cpu) {
-            return smp::submit_to(cpu, [v = std::move(p)] () mutable {
+            smp::submit_to(cpu, [v = std::move(p)] () mutable {
                 auto local(std::move(v));
             });
         }
-        return make_ready_future<>();
-    }
-    void destroy_nowait(PtrType p, unsigned cpu) {
-        (void)destroy(std::move(p), cpu);
     }
 public:
     using element_type = typename std::pointer_traits<PtrType>::element_type;
@@ -625,12 +621,6 @@ public:
             return make_foreign(std::move(v));
         });
     }
-    /// Destroy the managed pointer on its owner shard.
-    ///
-    /// \returns a future that resolves when the pointer is destroyed.
-    future<> destroy() {
-        return destroy(std::exchange(_value, {}), _cpu);
-    }
     /// Accesses the wrapped object.
     element_type& operator*() const { return *_value; }
     /// Accesses the wrapped object.
@@ -656,8 +646,7 @@ public:
     }
     /// Replace the managed pointer with new_ptr.
     ///
-    /// The previous managed pointer is eventually destroyed on its
-    /// owner shard.
+    /// The previous managed pointer is destroyed on its owner shard.
     void reset(PtrType new_ptr) {
         auto old_ptr = std::move(_value);
         auto old_cpu = _cpu;
@@ -665,12 +654,11 @@ public:
         _value = std::move(new_ptr);
         _cpu = engine().cpu_id();
 
-        destroy_nowait(std::move(old_ptr), old_cpu);
+        destroy(std::move(old_ptr), old_cpu);
     }
     /// Replace the managed pointer with a null value.
     ///
-    /// The previous managed pointer is eventually destroyed on its
-    /// owner shard.
+    /// The previous managed pointer is destroyed on its owner shard.
     void reset(std::nullptr_t = nullptr) {
         reset(PtrType());
     }
