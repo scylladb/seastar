c2bd3f6bd06a935934ef326ad4be4cbdc068abc1
Jesse Haber-Kucharsky <jhaberku@scylladb.com>
2018-12-15 18:37:53 -0500

    build: Clean up the find-module for LinuxMembarrier

--- cmake/FindLinuxMembarrier.cmake
+++ cmake/FindLinuxMembarrier.cmake
@@ -27,10 +27,14 @@ include (CheckCXXSourceCompiles)
 file (READ ${CMAKE_CURRENT_LIST_DIR}/code_tests/LinuxMembarrier_test.cc _linuxmembarrier_test_code)
 check_cxx_source_compiles ("${_linuxmembarrier_test_code}" LinuxMembarrier_FOUND)
 
+if (LinuxMembarrier_FOUND)
+  set (LinuxMembarrier_INCLUDE_DIRS ${LinuxMembarrier_INCLUDE_DIR})
+endif ()
+
 if (LinuxMembarrier_FOUND AND NOT (TARGET LinuxMembarrier::membarrier))
   add_library (LinuxMembarrier::membarrier INTERFACE IMPORTED)
 
   set_target_properties (LinuxMembarrier::membarrier
     PROPERTIES
-      INTERFACE_INCLUDE_DIRECTORIES ${LinuxMembarrier_INCLUDE_DIR})
+      INTERFACE_INCLUDE_DIRECTORIES ${LinuxMembarrier_INCLUDE_DIRS})
 endif ()
