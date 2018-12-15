4302c370227bbaec37dd4097237453d6999223a5
Jesse Haber-Kucharsky <jhaberku@scylladb.com>
2018-12-15 18:49:16 -0500

    build: Clean up the find-module for numactl

--- cmake/Findnumactl.cmake
+++ cmake/Findnumactl.cmake
@@ -26,29 +26,25 @@ find_path (numactl_INCLUDE_DIR
 find_library (numactl_LIBRARY
   NAMES numa)
 
-if (numactl_INCLUDE_DIR AND numactl_LIBRARY)
-  set (numactl_FOUND ON)
-  set (numactl_INCLUDE_DIRS ${numactl_INCLUDE_DIR})
-  set (numactl_LIBRARIES ${numactl_LIBRARY})
-endif ()
+mark_as_advanced (
+  numactl_INCLUDE_DIR
+  numactl_LIBRARY)
 
 include (FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args (numactl
-  FOUND_VAR numactl_FOUND
   REQUIRED_VARS
-    numactl_INCLUDE_DIR
-    numactl_LIBRARY)
+    numactl_LIBRARY
+    numactl_INCLUDE_DIR)
+
+set (numactl_INCLUDE_DIRS ${numactl_INCLUDE_DIR})
+set (numactl_LIBRARIES ${numactl_LIBRARY})
 
 if (numactl_FOUND AND NOT (TARGET numactl::numactl))
   add_library (numactl::numactl UNKNOWN IMPORTED)
 
   set_target_properties (numactl::numactl
     PROPERTIES
-      IMPORTED_LOCATION ${numactl_LIBRARY}
+      IMPORTED_LOCATION ${numactl_LIBRARIES}
       INTERFACE_INCLUDE_DIRECTORIES ${numactl_INCLUDE_DIRS})
 endif ()
-
-mark_as_advanced (
-  numactl_INCLUDE_DIR
-  numactl_LIBRARY)
