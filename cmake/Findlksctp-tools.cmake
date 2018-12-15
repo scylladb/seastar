c9bc41aad4b45ca49850c37d640d6b2bc6dc49b6
Jesse Haber-Kucharsky <jhaberku@scylladb.com>
2018-12-15 18:43:10 -0500

    build: Clean up the find-module for lksctp-tools

--- cmake/Findlksctp-tools.cmake
+++ cmake/Findlksctp-tools.cmake
@@ -20,32 +20,32 @@
 # Copyright (C) 2018 Scylladb, Ltd.
 #
 
-find_path (lksctp-tools_INCLUDE_DIR
-  NAMES netinet/sctp.h)
-
 find_library (lksctp-tools_LIBRARY
   NAMES sctp)
 
-if (lksctp-tools_INCLUDE_DIR AND lksctp-tools_LIBRARY)
-  set (lksctp-tools_FOUND ON)
-  set (lksctp-tools_INCLUDE_DIRS ${lksctp-tools_INCLUDE_DIR})
-  set (lksctp-tools_LIBRARIES ${lksctp-tools_LIBRARY})
-endif ()
+find_path (lksctp-tools_INCLUDE_DIR
+  NAMES netinet/sctp.h)
+
+mark_as_advanced (
+  lksctp-tools_LIBRARY
+  lksctp-tools_INCLUDE_DIR)
 
 include (FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args (lksctp-tools
-  FOUND_VAR lksctp-tools_FOUND
   REQUIRED_VARS
-    lksctp-tools_INCLUDE_DIR
-    lksctp-tools_LIBRARY)
+    lksctp-tools_LIBRARY
+    lksctp-tools_INCLUDE_DIR)
+
+set (lksctp-tools_LIBRARIES ${lksctp-tools_LIBRARY})
+set (lksctp-tools_INCLUDE_DIRS ${lksctp-tools_INCLUDE_DIR})
 
 if (lksctp-tools_FOUND AND NOT (TARGET lksctp-tools::lksctp-tools))
   add_library (lksctp-tools::lksctp-tools UNKNOWN IMPORTED)
 
   set_target_properties (lksctp-tools::lksctp-tools
     PROPERTIES
-      IMPORTED_LOCATION ${lksctp-tools_LIBRARY}
+      IMPORTED_LOCATION ${lksctp-tools_LIBRARIES}
       INTERFACE_INCLUDE_DIRECTORIES ${lksctp-tools_INCLUDE_DIRS})
 endif ()
 
