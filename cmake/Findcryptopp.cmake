ebf17933a518c19acbac47cb13ac311186e50eeb
Jesse Haber-Kucharsky <jhaberku@scylladb.com>
2018-12-15 11:40:14 -0500

    build: Clean up the find-module for cryptopp

--- cmake/Findcryptopp.cmake
+++ cmake/Findcryptopp.cmake
@@ -20,36 +20,32 @@
 # Copyright (C) 2018 Scylladb, Ltd.
 #
 
-find_path (cryptopp_INCLUDE_DIR
-  NAMES cryptopp/aes.h)
-
 find_library (cryptopp_LIBRARY
   NAMES cryptopp)
 
-if (cryptopp_INCLUDE_DIR AND cryptopp_LIBRARY)
-  set (cryptopp_FOUND ON)
-  set (cryptopp_LIBRARIES ${cryptopp_LIBRARY})
-  set (cryptopp_INCLUDE_DIRS ${cryptopp_INCLUDE_DIR})
-endif ()
+find_path (cryptopp_INCLUDE_DIR
+  NAMES cryptopp/aes.h
+  PATH_SUFFIXES cryptopp)
+
+mark_as_advanced (
+  cryptopp_LIBRARY
+  cryptopp_INCLUDE_DIR)
 
 include (FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args (cryptopp
-  FOUND_VAR cryptopp_FOUND
   REQUIRED_VARS
-    cryptopp_INCLUDE_DIR
     cryptopp_LIBRARY
-  VERSION_VAR cryptopp_VERSION)
+    cryptopp_INCLUDE_DIR)
+
+set (cryptopp_LIBRARIES ${cryptopp_LIBRARY})
+set (cryptopp_INCLUDE_DIRS ${cryptopp_INCLUDE_DIR})
 
 if (cryptopp_FOUND AND NOT (TARGET cryptopp::cryptopp))
   add_library (cryptopp::cryptopp UNKNOWN IMPORTED)
 
   set_target_properties (cryptopp::cryptopp
     PROPERTIES
-      IMPORTED_LOCATION ${cryptopp_LIBRARY}
+      IMPORTED_LOCATION ${cryptopp_LIBRARIES}
       INTERFACE_INCLUDE_DIRECTORIES ${cryptopp_INCLUDE_DIRS})
 endif ()
-
-mark_as_advanced (
-  cryptopp_INCLUDE_DIR
-  cryptopp_LIBRARY)
