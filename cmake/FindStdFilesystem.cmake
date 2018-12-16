7587d30a8243858521ee907d7457bb4c6f29576b
Jesse Haber-Kucharsky <jhaberku@scylladb.com>
2018-12-15 19:20:21 -0500

    build: Clean up the find-module for StdFilesystem
    The `check_cxx_source_compiles` function automatically sets cache
    variables, which is more useful for our purposes.

--- cmake/FindStdFilesystem.cmake
+++ cmake/FindStdFilesystem.cmake
@@ -20,37 +20,46 @@
 # Copyright (C) 2018 Scylladb, Ltd.
 #
 
-set (_stdfilesystem_test_source ${CMAKE_CURRENT_LIST_DIR}/code_tests/StdFilesystem_test.cc)
+include (CheckCXXSourceCompiles)
+file (READ ${CMAKE_CURRENT_LIST_DIR}/code_tests/StdFilesystem_test.cc _stdfilesystem_test_code)
 
-try_compile (StdFilesystem_test_stdc++fs
-  ${CMAKE_CURRENT_BINARY_DIR}
-  SOURCES ${_stdfilesystem_test_source}
-  LINK_LIBRARIES stdc++fs)
+macro (_stdfilesystem_check_compiles var)
+  set (libraries ${ARGN})
+  set (CMAKE_REQUIRED_LIBRARIES ${libraries})
+  set (CMAKE_REQUIRED_FLAGS -std=gnu++14)
+  check_cxx_source_compiles ("${_stdfilesystem_test_code}" ${var})
+endmacro ()
 
-if (StdFilesystem_test_stdc++fs)
-  set (StdFilesystem_LIBRARY_NAME stdc++fs)
+# Try to compile without the library first.
+_stdfilesystem_check_compiles (StdFilesystem_NO_EXPLICIT_LINK)
+
+if (StdFilesystem_NO_EXPLICIT_LINK)
+  set (StdFilesystem_FOUND yes)
 else ()
-  # Try libc++.
-  try_compile (StdFilesystem_test_c++experimental
-    ${CMAKE_CURRENT_BINARY_DIR}
-    SOURCES ${_stdfilesystem_test_source}
-    LINK_LIBRARIES libc++experimental)
+  _stdfilesystem_check_compiles (StdFilesystem_STDCXXFS_LIBRARY
+    stdc++fs)
 
-  if (StdFilesystem_test_c++experimental)
-    set (StdFilesystem_LIBRARY_NAME c++experimental)
+  if (StdFilesystem_STDCXXFS_LIBRARY)
+    set (StdFilesystem_LIBRARY_NAME stdc++fs)
+  else ()
+    # Try libc++.
+    _stdfilesystem_check_compiles (StdFilesystem_CXXEXPERIMENTAL_LIBRARY
+      libc++experimental)
+
+    if (StdFilesystem_CXXEXPERIMENTAL_LIBRARY)
+      set (StdFilesystem_LIBRARY_NAME c++experimental)
+    endif ()
   endif ()
+
+  if (StdFilesystem_LIBRARY_NAME)
+    set (StdFilesystem_LIBRARIES -l${StdFilesystem_LIBRARY_NAME})
+  endif ()
+
+  include (FindPackageHandleStandardArgs)
+
+  find_package_handle_standard_args (StdFilesystem
+    REQUIRED_VARS StdFilesystem_LIBRARY_NAME)
 endif ()
-
-if (StdFilesystem_LIBRARY_NAME)
-  set (StdFilesystem_FOUND ON)
-  set (StdFilesystem_LIBRARIES -l${StdFilesystem_LIBRARY_NAME})
-endif ()
-
-include (FindPackageHandleStandardArgs)
-
-find_package_handle_standard_args (StdFilesystem
-  FOUND_VAR StdFilesystem_FOUND
-  REQUIRED_VARS StdFilesystem_LIBRARY_NAME)
 
 if (StdFilesystem_FOUND AND NOT (TARGET StdFilesystem::filesystem))
   add_library (StdFilesystem::filesystem INTERFACE IMPORTED)
@@ -59,5 +68,3 @@ if (StdFilesystem_FOUND AND NOT (TARGET StdFilesystem::filesystem))
     PROPERTIES
       INTERFACE_LINK_LIBRARIES ${StdFilesystem_LIBRARIES})
 endif ()
-
-mark_as_advanced (StdFilesystem_LIBRARY_NAME)
