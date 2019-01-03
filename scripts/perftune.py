0919b380b9526fb92ef9352c82795cbd21ba1ec1
Vlad Zolotarov <vladz@scylladb.com>
2019-01-03 17:50:21 -0500

    perftune.py: tune only active NVMe HW queues on i3 AWS instances
    Tune only NVMe HW queues with indexes between 1 and <number of CPUs>
    if running on i3.xxx AWS instance.
    
    NVMe cards use queue[0] for management and next queues for handling
    data - up to <number of CPUs> HW queues for data.
    
    Usually, nvme kernel module is not going to allocate more than
    <number of CPUs> + 1 HW queues. However, due to a problem in the AWS
    hypervisor nvme is going to allocate more HW queues (and thereby IRQ
    vectors) on AWS instances.
    
    However these "additional" HW queues are still not going to be active,
    therefore we should ignore them - otherwise active queues (and thereby
    I/O load) are not going to be distributed anong CPUs as expected.
    
    Signed-off-by: Vlad Zolotarov <vladz@scylladb.com>
    Message-Id: <20190103225021.23191-1-vladz@scylladb.com>

--- scripts/perftune.py
+++ scripts/perftune.py
@@ -6,6 +6,7 @@ import enum
 import functools
 import glob
 import itertools
+import multiprocessing
 import os
 import pathlib
 import pyudev
@@ -13,6 +14,7 @@ import re
 import shutil
 import subprocess
 import sys
+import urllib.request
 import yaml
 
 def run_one_command(prog_args, my_stderr=None, check=True):
@@ -688,6 +690,7 @@ class DiskPerfTuner(PerfTunerBase):
 
         self.__pyudev_ctx = pyudev.Context()
         self.__dir2disks = self.__learn_directories()
+        self.__irqs2procline = get_irqs2procline_map()
         self.__disk2irqs = self.__learn_irqs()
         self.__type2diskinfo = self.__group_disks_info_by_type()
 
@@ -763,6 +766,30 @@ class DiskPerfTuner(PerfTunerBase):
         """
         return self.__type2diskinfo[DiskPerfTuner.SupportedDiskTypes(disks_type)]
 
+    def __nvme_fast_path_irq_filter(self, irq):
+        """
+        Return True for fast path NVMe IRQs.
+        For NVMe device only queues 1-<number of CPUs> are going to do fast path work.
+
+        NVMe IRQs have the following name convention:
+             nvme<device index>q<queue index>, e.g. nvme0q7
+
+        :param irq: IRQ number
+        :return: True if this IRQ is an IRQ of a FP NVMe queue.
+        """
+        nvme_irq_re = re.compile(r'(\s|^)nvme\d+q(\d+)(\s|$)')
+
+        # There may be more than an single HW queue bound to the same IRQ. In this case queue names are going to be
+        # coma separated
+        split_line = self.__irqs2procline[irq].split(",")
+
+        for line in split_line:
+            m = nvme_irq_re.search(line)
+            if m and 0 < int(m.group(2)) <= multiprocessing.cpu_count():
+                return True
+
+        return False
+
     def __group_disks_info_by_type(self):
         """
         Return a map of tuples ( [<disks>], [<irqs>] ), where "disks" are all disks of the specific type
@@ -792,7 +819,23 @@ class DiskPerfTuner(PerfTunerBase):
         if not (nvme_disks or non_nvme_disks):
             raise Exception("'disks' tuning was requested but no disks were found")
 
-        disks_info_by_type[DiskPerfTuner.SupportedDiskTypes.nvme] = ( list(nvme_disks), list(nvme_irqs) )
+        nvme_irqs = list(nvme_irqs)
+
+        # There is a known issue with Xen hypervisor that exposes itself on AWS i3 instances where nvme module
+        # over-allocates HW queues and uses only queues 1,2,3,..., <up to number of CPUs> for data transfer.
+        # On these instances we will distribute only these queues.
+        try:
+            aws_instance_type = urllib.request.urlopen("http://169.254.169.254/latest/meta-data/instance-type", timeout=0.1).read().decode()
+            if re.match(r'i3\.\w+', aws_instance_type):
+                nvme_irqs = list(filter(self.__nvme_fast_path_irq_filter, nvme_irqs))
+        except urllib.error.URLError:
+            # Non-AWS case
+            pass
+
+        # Sort IRQs for easier verification
+        nvme_irqs.sort(key=lambda irq_num_str: int(irq_num_str))
+
+        disks_info_by_type[DiskPerfTuner.SupportedDiskTypes.nvme] = (list(nvme_disks), nvme_irqs)
         disks_info_by_type[DiskPerfTuner.SupportedDiskTypes.non_nvme] = ( list(non_nvme_disks), list(non_nvme_irqs) )
 
         return disks_info_by_type
@@ -838,7 +881,6 @@ class DiskPerfTuner(PerfTunerBase):
 
     def __learn_irqs(self):
         disk2irqs = {}
-        irqs2procline = get_irqs2procline_map()
 
         for devices in list(self.__dir2disks.values()) + [ self.args.devs ]:
             for device in devices:
@@ -867,7 +909,7 @@ class DiskPerfTuner(PerfTunerBase):
                         break
 
                 controler_path_str = functools.reduce(lambda x, y : os.path.join(x, y), controller_path_parts)
-                disk2irqs[device] = learn_all_irqs_one(controler_path_str, irqs2procline, 'blkif')
+                disk2irqs[device] = learn_all_irqs_one(controler_path_str, self.__irqs2procline, 'blkif')
 
         return disk2irqs
 
