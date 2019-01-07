2f512b7dbb7c03a9a202869ea1f117a39148a4a5
Juliana Oliveira <juliana@scylladb.com>
2019-01-07 17:55:02 -0200

    cgroup: create cpu_set and memory_limit functions
    Moved old get_cgroup_memory_limit() to cgroup::memory_limit() and
    created cgroup::cpu_set()
    
    Signed-off-by: Juliana Oliveira <juliana@scylladb.com>

--- src/core/resource.cc
+++ src/core/resource.cc
@@ -29,11 +29,15 @@
 #include <seastar/util/read_first_line.hh>
 #include <stdlib.h>
 #include <limits>
+#include "cgroup.hh"
+#include <seastar/util/log.hh>
 
 #include <boost/range/adaptor/map.hpp>
 #include <boost/range/algorithm/copy.hpp>
 
 namespace seastar {
+
+extern logger seastar_logger;
 
 // This function was made optional because of validate. It needs to
 // throw an error when a non parseable input is given.
@@ -88,6 +92,39 @@ void validate(boost::any& v,
     } else {
         throw validation_error(validation_error::invalid_option_value);
     }
+}
+
+namespace cgroup {
+
+
+optional<cpuset> cpu_set() {
+    auto cpuset = read_setting_as<std::string>("/sys/fs/cgroup/cpuset/cpuset.cpus");
+    if (cpuset) {
+        return seastar::parse_cpuset(*cpuset);
+    }
+
+    seastar_logger.warn("Unable to parse cgroup's cpuset. Ignoring.");
+    return seastar::compat::nullopt;
+}
+
+size_t memory_limit() {
+    return read_setting_as<size_t>("/sys/fs/cgroup/memory/memory.limit_in_bytes")
+        .value_or(std::numeric_limits<size_t>::max());
+}
+
+
+template <typename T>
+optional<T> read_setting_as(std::string path) {
+    try {
+        auto line = read_first_line(path);
+        return boost::lexical_cast<T>(line);
+    } catch (...) {
+        seastar_logger.warn("Couldn't read cgroup file {}.", path);
+    }
+
+    return seastar::compat::nullopt;
+}
+
 }
 
 namespace resource {
@@ -280,16 +317,6 @@ allocate_io_queues(hwloc_topology_t& topology, std::vector<cpu> cpus, unsigned n
 }
 
 
-size_t get_cgroup_memory_limit() {
-    compat::filesystem::path cgroup_memory = "/sys/fs/cgroup/memory/memory.limit_in_bytes";
-
-    try {
-        return boost::lexical_cast<size_t>(read_first_line(cgroup_memory));
-    } catch (...) {
-        return std::numeric_limits<size_t>::max();
-    }
-}
-
 resources allocate(configuration c) {
     hwloc_topology_t topology;
     hwloc_topology_init(&topology);
@@ -320,7 +347,7 @@ resources allocate(configuration c) {
     auto machine = hwloc_get_obj_by_depth(topology, machine_depth, 0);
     auto available_memory = machine->memory.total_memory;
     size_t mem = calculate_memory(c, std::min(available_memory,
-                                              get_cgroup_memory_limit()));
+                                              cgroup::memory_limit()));
     unsigned available_procs = hwloc_get_nbobjs_by_type(topology, HWLOC_OBJ_PU);
     unsigned procs = c.cpus.value_or(available_procs);
     if (procs > available_procs) {
