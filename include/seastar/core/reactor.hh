e82851b525b2c982f3a130a0e4f2b813bafd42bb
Avi Kivity <avi@scylladb.com>
2019-01-10 16:25:51 +0200

    reactor: move pollable_fd and related classes to an internal header files
    pollable_fd and friends are internal data structures and should not be exposed.
    This is still too difficult at the moment because public classes depend on them,
    but this patch starts the work and moves them to a separate header file. It also
    makes it possible to make syscall_work_queue, which depends on pollable_fd,
    private.

--- include/seastar/core/reactor.hh
+++ include/seastar/core/reactor.hh
@@ -78,6 +78,7 @@
 #include <seastar/core/manual_clock.hh>
 #include <seastar/core/metrics_registration.hh>
 #include <seastar/core/scheduling.hh>
+#include "internal/pollable_fd.hh"
 
 #ifdef HAVE_OSV
 #include <osv/sched.hh>
@@ -97,34 +98,6 @@ namespace alien {
 class message_queue;
 }
 class reactor;
-class pollable_fd;
-class pollable_fd_state;
-
-class pollable_fd_state {
-public:
-    struct speculation {
-        int events = 0;
-        explicit speculation(int epoll_events_guessed = 0) : events(epoll_events_guessed) {}
-    };
-    ~pollable_fd_state();
-    explicit pollable_fd_state(file_desc fd, speculation speculate = speculation())
-        : fd(std::move(fd)), events_known(speculate.events) {}
-    pollable_fd_state(const pollable_fd_state&) = delete;
-    void operator=(const pollable_fd_state&) = delete;
-    void speculate_epoll(int events) { events_known |= events; }
-    file_desc fd;
-    bool events_rw = false;   // single consumer for both read and write (accept())
-    bool no_more_recv = false; // For udp, there is no shutdown indication from the kernel
-    bool no_more_send = false; // For udp, there is no shutdown indication from the kernel
-    int events_requested = 0; // wanted by pollin/pollout promises
-    int events_epoll = 0;     // installed in epoll
-    int events_known = 0;     // returned from epoll
-    promise<> pollin;
-    promise<> pollout;
-    friend class reactor;
-    friend class pollable_fd;
-};
-
 inline
 size_t iovec_len(const std::vector<iovec>& iov)
 {
@@ -134,44 +107,6 @@ size_t iovec_len(const std::vector<iovec>& iov)
     }
     return ret;
 }
-
-class pollable_fd {
-public:
-    using speculation = pollable_fd_state::speculation;
-    pollable_fd(file_desc fd, speculation speculate = speculation())
-        : _s(std::make_unique<pollable_fd_state>(std::move(fd), speculate)) {}
-public:
-    pollable_fd(pollable_fd&&) = default;
-    pollable_fd& operator=(pollable_fd&&) = default;
-    future<size_t> read_some(char* buffer, size_t size);
-    future<size_t> read_some(uint8_t* buffer, size_t size);
-    future<size_t> read_some(const std::vector<iovec>& iov);
-    future<> write_all(const char* buffer, size_t size);
-    future<> write_all(const uint8_t* buffer, size_t size);
-    future<size_t> write_some(net::packet& p);
-    future<> write_all(net::packet& p);
-    future<> readable();
-    future<> writeable();
-    future<> readable_or_writeable();
-    void abort_reader();
-    void abort_writer();
-    future<pollable_fd, socket_address> accept();
-    future<size_t> sendmsg(struct msghdr *msg);
-    future<size_t> recvmsg(struct msghdr *msg);
-    future<size_t> sendto(socket_address addr, const void* buf, size_t len);
-    file_desc& get_file_desc() const { return _s->fd; }
-    void shutdown(int how) { _s->fd.shutdown(how); }
-    void close() { _s.reset(); }
-protected:
-    int get_fd() const { return _s->fd.get(); }
-    void maybe_no_more_recv();
-    void maybe_no_more_send();
-    friend class reactor;
-    friend class readable_eventfd;
-    friend class writeable_eventfd;
-private:
-    std::unique_ptr<pollable_fd_state> _s;
-};
 
 }
 
@@ -193,38 +128,6 @@ namespace seastar {
 void register_network_stack(sstring name, boost::program_options::options_description opts,
     std::function<future<std::unique_ptr<network_stack>>(boost::program_options::variables_map opts)> create,
     bool make_default = false);
-
-class writeable_eventfd;
-
-class readable_eventfd {
-    pollable_fd _fd;
-public:
-    explicit readable_eventfd(size_t initial = 0) : _fd(try_create_eventfd(initial)) {}
-    readable_eventfd(readable_eventfd&&) = default;
-    writeable_eventfd write_side();
-    future<size_t> wait();
-    int get_write_fd() { return _fd.get_fd(); }
-private:
-    explicit readable_eventfd(file_desc&& fd) : _fd(std::move(fd)) {}
-    static file_desc try_create_eventfd(size_t initial);
-
-    friend class writeable_eventfd;
-};
-
-class writeable_eventfd {
-    file_desc _fd;
-public:
-    explicit writeable_eventfd(size_t initial = 0) : _fd(try_create_eventfd(initial)) {}
-    writeable_eventfd(writeable_eventfd&&) = default;
-    readable_eventfd read_side();
-    void signal(size_t nr);
-    int get_read_fd() { return _fd.get(); }
-private:
-    explicit writeable_eventfd(file_desc&& fd) : _fd(std::move(fd)) {}
-    static file_desc try_create_eventfd(size_t initial);
-
-    friend class readable_eventfd;
-};
 
 class thread_pool;
 class smp;
