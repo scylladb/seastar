scylladb/seastar

commit 5aea50a8a4f46316bb5015a79989c3281d4110e7

reactor: virtualize high resolution timer processing
The epoll backend uses signals for high resolution timers, so that a timer
expiration will set need_preempt(). But since we'll use a different method for
preemption, we need to move away from signals with the aio backend.

To prepare for that, move high resolution timer processing to reactor_backend.

src/core/reactor.cc
@@ -294,6 +294,12 @@ wrap_syscall(int result, const Extra& extra) {
     return syscall_result_extra<Extra>{result, errno, extra};
 }
 
+inline int alarm_signal() {
+    // We don't want to use SIGALRM, because the boost unit test library
+    // also plays with it.
+    return SIGRTMIN;
+}
+
 reactor_backend_epoll::reactor_backend_epoll(reactor* r)
         : _r(r), _epollfd(file_desc::epoll_create(EPOLL_CLOEXEC)) {
     ::epoll_event event;
@@ -301,6 +307,17 @@ reactor_backend_epoll::reactor_backend_epoll(reactor* r)
     event.data.ptr = nullptr;
     auto ret = ::epoll_ctl(_epollfd.get(), EPOLL_CTL_ADD, _r->_notify_eventfd.get(), &event);
     throw_system_error_on(ret == -1);
+
+    struct sigevent sev;
+    sev.sigev_notify = SIGEV_THREAD_ID;
+    sev._sigev_un._tid = syscall(SYS_gettid);
+    sev.sigev_signo = alarm_signal();
+    ret = timer_create(CLOCK_MONOTONIC, &sev, &_steady_clock_timer);
+    assert(ret >= 0);
+}
+
+reactor_backend_epoll::~reactor_backend_epoll() {
+    timer_delete(_steady_clock_timer);
 }
 
 void reactor_backend_epoll::start_tick() {
@@ -318,6 +335,17 @@ void reactor_backend_epoll::stop_tick() {
     _r->_dying.store(true, std::memory_order_relaxed);
     _r->_task_quota_timer.timerfd_settime(0, seastar::posix::to_relative_itimerspec(1ns, 1ms)); // Make the timer fire soon
     _task_quota_timer_thread.join();
+}
+
+void reactor_backend_epoll::arm_highres_timer(const ::itimerspec& its) {
+    auto ret = timer_settime(_steady_clock_timer, TIMER_ABSTIME, &its, NULL);
+    throw_system_error_on(ret == -1);
+    if (!_timer_enabled) {
+        _timer_enabled = true;
+        _r->_signals.handle_signal(alarm_signal(), [r = _r] {
+            r->service_highres_timer();
+        });
+    }
 }
 
 reactor::signals::signals() : _pending_signals(0) {
@@ -471,12 +499,6 @@ static void print_with_backtrace(const char* cause) noexcept {
     print_with_backtrace(buf);
 }
 
-inline int alarm_signal() {
-    // We don't want to use SIGALRM, because the boost unit test library
-    // also plays with it.
-    return SIGRTMIN;
-}
-
 // Installs signal handler stack for current thread.
 // The stack remains installed as long as the returned object is kept alive.
 // When it goes out of scope the previous handler is restored.
@@ -599,12 +621,6 @@ reactor::reactor(unsigned id)
     sigaddset(&mask, alarm_signal());
     r = ::pthread_sigmask(SIG_BLOCK, &mask, NULL);
     assert(r == 0);
-    struct sigevent sev;
-    sev.sigev_notify = SIGEV_THREAD_ID;
-    sev._sigev_un._tid = syscall(SYS_gettid);
-    sev.sigev_signo = alarm_signal();
-    r = timer_create(CLOCK_MONOTONIC, &sev, &_steady_clock_timer);
-    assert(r >= 0);
     sigemptyset(&mask);
     sigaddset(&mask, cpu_stall_detector::signal_number());
     r = ::pthread_sigmask(SIG_UNBLOCK, &mask, NULL);
@@ -625,7 +641,6 @@ reactor::~reactor() {
     assert(r == 0);
 
     _backend.stop_tick();
-    timer_delete(_steady_clock_timer);
     auto eraser = [](auto& list) {
         while (!list.empty()) {
             auto& timer = *list.begin();
@@ -2572,8 +2587,7 @@ void reactor::enable_timer(steady_clock_type::time_point when)
     itimerspec its;
     its.it_interval = {};
     its.it_value = to_timespec(when);
-    auto ret = timer_settime(_steady_clock_timer, TIMER_ABSTIME, &its, NULL);
-    throw_system_error_on(ret == -1);
+    _backend.arm_highres_timer(its);
 #else
     using ns = std::chrono::nanoseconds;
     WITH_LOCK(_timer_mutex) {
@@ -3257,6 +3271,14 @@ reactor::activate(task_queue& tq) {
     _activating_task_queues.push_back(&tq);
 }
 
+void reactor::service_highres_timer() {
+    complete_timers(_timers, _expired_timers, [this] {
+        if (!_timers.empty()) {
+            enable_timer(_timers.get_next_timeout());
+        }
+    });
+}
+
 int reactor::run() {
     auto signal_stack = install_signal_handler_stack();
 
@@ -3307,15 +3329,6 @@ int reactor::run() {
     });
 
     poller syscall_poller(std::make_unique<syscall_pollfn>(*this));
-#ifndef HAVE_OSV
-    _signals.handle_signal(alarm_signal(), [this] {
-        complete_timers(_timers, _expired_timers, [this] {
-            if (!_timers.empty()) {
-                enable_timer(_timers.get_next_timeout());
-            }
-        });
-    });
-#endif
 
     poller drain_cross_cpu_freelist(std::make_unique<drain_cross_cpu_freelist_pollfn>());
 

include/seastar/core/reactor.hh
@@ -462,6 +462,7 @@ public:
     virtual void handle_signal(int signo) = 0;
     virtual void start_tick() = 0;
     virtual void stop_tick() = 0;
+    virtual void arm_highres_timer(const ::itimerspec& ts) = 0;
 };
 
 // reactor backend using file-descriptor & epoll, suitable for running on
@@ -471,6 +472,8 @@ public:
 class reactor_backend_epoll : public reactor_backend {
     reactor* _r;
     std::thread _task_quota_timer_thread;
+    timer_t _steady_clock_timer = {};
+    bool _timer_enabled = false;
 private:
     file_desc _epollfd;
     future<> get_epoll_future(pollable_fd_state& fd,
@@ -480,7 +483,7 @@ private:
     static void signal_received(int signo, siginfo_t* siginfo, void* ignore);
 public:
     explicit reactor_backend_epoll(reactor* r);
-    virtual ~reactor_backend_epoll() override { }
+    virtual ~reactor_backend_epoll() override;
     virtual bool wait_and_process(int timeout, const sigset_t* active_sigmask) override;
     virtual future<> readable(pollable_fd_state& fd) override;
     virtual future<> writeable(pollable_fd_state& fd) override;
@@ -489,6 +492,7 @@ public:
     virtual void handle_signal(int signo) override;
     virtual void start_tick() override;
     virtual void stop_tick() override;
+    virtual void arm_highres_timer(const ::itimerspec& ts) override;
 };
 
 #ifdef HAVE_OSV
@@ -760,7 +764,6 @@ private:
     bool _handle_sigint = true;
     promise<std::unique_ptr<network_stack>> _network_stack_ready_promise;
     int _return = 0;
-    timer_t _steady_clock_timer = {};
     file_desc _task_quota_timer;
     promise<> _start_promise;
     semaphore _cpu_started;
@@ -913,6 +916,7 @@ private:
     uint64_t min_vruntime() const;
     void request_preemption();
     void reset_preemption_monitor();
+    void service_highres_timer();
 public:
     static boost::program_options::options_description get_options_description(std::chrono::duration<double> default_task_quota);
     explicit reactor(unsigned id);

commit c2762e949d584ae4da452fb9ce4f449dcd700f85

reactor: fix loss of highres timers with the epoll backend
The lowres_timer_pollfn and signal_pollfn pollers have a hidden
dependency: lowres_timer_pollfn::try_enter_interrupt_mode() may arm a
timer, which (if it happens to be the first highres timer to be armed)
register a signal handler. But signal_pollfn assumes there are no changes
to signal registration between signal_pollfn::try_enter_interrupt_mode()
and signal_pollfn::exit_interrupt_mode(); if there are, the newly registered
signal becomes blocked.

This was broken in 5aea50a8a4f46316 ("reactor: virtualize high resolution
timer processing") and is seen in memcached tests with the epoll backend.

Fix by reversing the order of poller registration so that the problem is
never triggered. The bug deserves a better fix, but let's start with
something simple.
Message-Id: <20190112202337.22987-1-avi@scylladb.com>

src/core/reactor.cc
@@ -3763,7 +3763,6 @@ int reactor::run() {
         aio_poller = poller(std::make_unique<aio_batch_submit_pollfn>(*this));
     }
 
-    poller sig_poller(std::make_unique<signal_pollfn>(*this));
     poller batch_flush_poller(std::make_unique<batch_flush_pollfn>(*this));
     poller execution_stage_poller(std::make_unique<execution_stage_pollfn>());
 
@@ -3794,7 +3793,11 @@ int reactor::run() {
 
     poller drain_cross_cpu_freelist(std::make_unique<drain_cross_cpu_freelist_pollfn>());
 
+    // expire_lowres_timers must be before sig_poller, because lowres_timer_pollfn
+    // may arm the first highres timer, which can add a new signal to be registerd. If the order
+    // is reversed, then signal_pollfn::exit_interrupt_mode() can re-block the timer signal.
     poller expire_lowres_timers(std::make_unique<lowres_timer_pollfn>(*this));
+    poller sig_poller(std::make_unique<signal_pollfn>(*this));
 
     using namespace std::chrono_literals;
     timer<lowres_clock> load_timer;