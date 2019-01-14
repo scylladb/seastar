65e40b8a4f53e034b10b61522c0ce167e99b128e
Paweł Dziepak <pdziepak@scylladb.com>
2019-01-14 18:00:10 +0000

    apps/io_tester: add virtual dtor for class_data

--- apps/io_tester/io_tester.cc
+++ apps/io_tester/io_tester.cc
@@ -144,6 +144,8 @@ public:
         , _pos_distribution(0,  file_data_size / _config.shard_info.request_size)
     {}
 
+    virtual ~class_data() = default;
+
     future<> issue_requests(std::chrono::steady_clock::time_point stop) {
         _start = std::chrono::steady_clock::now();
         return with_scheduling_group(_sg, [this, stop] {
