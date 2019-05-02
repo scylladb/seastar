bfbd41560476b752abe51b0f2d13fada56ea2a78
Rafael Ávila de Espíndola <espindola@scylladb.com>
2019-05-02 12:01:54 -0700

    future: Fix build with libc++'s non-trivially-constructible  std::tuple<>
    This adds a hack for std::tuple<> not being trivially constructible in
    libc++.
    
    Signed-off-by: Rafael Ávila de Espíndola <espindola@scylladb.com>
    Message-Id: <20190502190154.44254-1-espindola@scylladb.com>

--- include/seastar/core/future.hh
+++ include/seastar/core/future.hh
@@ -171,13 +171,25 @@ template <typename T> struct uninitialized_wrapper_base<T, true> : private T {
     }
 };
 
+template <typename T>
+constexpr bool can_inherit =
+#ifdef _LIBCPP_VERSION
+// We expect std::tuple<> to be trivially constructible and
+// destructible. That is not the case with libc++
+// (https://bugs.llvm.org/show_bug.cgi?id=41714).  We could avoid this
+// optimization when using libc++ and relax the asserts, but
+// inspection suggests that std::tuple<> is trivial, it is just not
+// marked as such.
+        std::is_same<std::tuple<>, T>::value ||
+#endif
+        (std::is_trivially_destructible<T>::value && std::is_trivially_constructible<T>::value &&
+                std::is_class<T>::value && !std::is_final<T>::value);
+
 // The objective is to avoid extra space for empty types like std::tuple<>. We could use std::is_empty_v, but it is
 // better to check that both the constructor and destructor can be skipped.
 template <typename T>
 struct uninitialized_wrapper
-    : public uninitialized_wrapper_base<T, std::is_trivially_destructible<T>::value &&
-                                                   std::is_trivially_constructible<T>::value &&
-                                                   std::is_class<T>::value && !std::is_final<T>::value> {};
+    : public uninitialized_wrapper_base<T, can_inherit<T>> {};
 
 static_assert(std::is_empty<uninitialized_wrapper<std::tuple<>>>::value, "This should still be empty");
 }
