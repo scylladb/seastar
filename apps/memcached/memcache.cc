2ea17a071c1f532969337f13922138bfc998b51e
Avi Kivity <avi@scylladb.com>
2019-07-27 17:39:11 +0300

    memcache: switch to api_v2::server_socket
    Allows building it if the user selects either api_v1 or api_v2 as
    the public API.

--- apps/memcached/memcache.cc
+++ apps/memcached/memcache.cc
@@ -1319,7 +1319,7 @@ public:
 
 class tcp_server {
 private:
-    lw_shared_ptr<server_socket> _listener;
+    lw_shared_ptr<seastar::api_v2::server_socket> _listener;
     sharded_cache& _cache;
     distributed<system_stats>& _system_stats;
     uint16_t _port;
@@ -1355,9 +1355,11 @@ public:
     void start() {
         listen_options lo;
         lo.reuse_address = true;
-        _listener = engine().listen(make_ipv4_address({_port}), lo);
+        _listener = seastar::api_v2::server_socket(engine().listen(make_ipv4_address({_port}), lo));
         keep_doing([this] {
-            return _listener->accept().then([this] (connected_socket fd, socket_address addr) mutable {
+            return _listener->accept().then([this] (accept_result ar) mutable {
+                connected_socket fd = std::move(ar.connection);
+                socket_address addr = std::move(ar.remote_address);
                 auto conn = make_lw_shared<connection>(std::move(fd), addr, _cache, _system_stats);
                 do_until([conn] { return conn->_in.eof(); }, [conn] {
                     return conn->_proto.handle(conn->_in, conn->_out).then([conn] {
