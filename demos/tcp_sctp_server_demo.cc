0c1f9ef4872b7a26e220ced71aa44bef6fc45b21
Benny Halevy <bhalevy@scylladb.com>
2019-08-01 14:06:03 +0300

    demos: suppress discarded future warnings
    Cast to (void) to suppress the warning.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- demos/tcp_sctp_server_demo.cc
+++ demos/tcp_sctp_server_demo.cc
@@ -72,11 +72,12 @@ public:
 
     void do_accepts(std::vector<api_v2::server_socket>& listeners) {
         int which = listeners.size() - 1;
-        listeners[which].accept().then([this, &listeners] (accept_result ar) mutable {
+        // Accept in the background.
+        (void)listeners[which].accept().then([this, &listeners] (accept_result ar) mutable {
             connected_socket fd = std::move(ar.connection);
             socket_address addr = std::move(ar.remote_address);
             auto conn = new connection(*this, std::move(fd), addr);
-            conn->process().then_wrapped([conn] (auto&& f) {
+            (void)conn->process().then_wrapped([conn] (auto&& f) {
                 delete conn;
                 try {
                     f.get();
@@ -191,11 +192,12 @@ int main(int ac, char** av) {
             return engine().exit(1);
         }
         auto server = new distributed<tcp_server>;
-        server->start().then([server = std::move(server), port] () mutable {
+        (void)server->start().then([server = std::move(server), port] () mutable {
             engine().at_exit([server] {
                 return server->stop();
             });
-            server->invoke_on_all(&tcp_server::listen, ipv4_addr{port});
+            // Start listening in the background.
+            (void)server->invoke_on_all(&tcp_server::listen, ipv4_addr{port});
         }).then([port] {
             std::cout << "Seastar TCP server listening on port " << port << " ...\n";
         });
