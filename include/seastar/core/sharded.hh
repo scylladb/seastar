d199d27681cd0572fd135c00328db33337b7b191
Avi Kivity <avi@scylladb.com>
2019-08-04 21:53:03 +0300

    sharded: support for non-cooperative service types
    The sharded<Service> template was designed with cooperating service classes
    in mind. This cooperation is in the form of providing a stop() method for
    orderly shutdown.
    
    The sharded<> template is also useful for non-cooperating services: for example,
    std::unordered_map<std::string can be broadcast to all shards with
    
        sharded<std::unordered_map<sstring, sstring>> my_map;
        my_map.start(initializer).get();
        ...
        my_map.invoke_on_all([] (my_map_type& mm) { mm["x"] = "y"; }).get();
        ...
        my_map.stop().get();
    
    All is required is that the "Service" doesn't perform asynchronous operations (as
    is usual for plain data types) or that the user stops them in some other way. But
    this currently doesn't compile because the Service::stop() method is required.
    
    To allow that, this patch only calls stop() if it is detected.
    Message-Id: <20190804185303.4966-1-avi@scylladb.com>

--- include/seastar/core/sharded.hh
+++ include/seastar/core/sharded.hh
@@ -570,6 +570,55 @@ sharded<Service>::start_single(Args&&... args) {
     });
 }
 
+namespace internal {
+
+// Helper check if Service::stop exists
+struct sharded_has_stop {
+    // If a member names "stop" exists, try to call it, even if it doesn't
+    // have the correct signature. This is so that we don't ignore a function
+    // named stop() just because the signature is incorrect, and instead
+    // force the user to resolve the ambiguity.
+    template <typename Service>
+    constexpr static auto check(Service*) -> std::enable_if_t<(sizeof(&Service::stop) >= 0), bool> {
+        return true;
+    }
+
+    // Fallback in case Service::stop doesn't exist.
+    static constexpr auto check(...) -> bool {
+        return false;
+    }
+};
+
+template <bool stop_exists>
+struct sharded_call_stop {
+    template <typename Service>
+    static future<> call(Service& instance);
+};
+
+template <>
+template <typename Service>
+inline
+future<> sharded_call_stop<true>::call(Service& instance) {
+    return instance.stop();
+}
+
+template <>
+template <typename Service>
+inline
+future<> sharded_call_stop<false>::call(Service& instance) {
+    return make_ready_future<>();
+}
+
+template <typename Service>
+inline
+future<>
+stop_sharded_instance(Service& instance) {
+    constexpr bool has_stop = internal::sharded_has_stop::check(&instance);
+    return internal::sharded_call_stop<has_stop>::call(instance);
+}
+
+}
+
 template <typename Service>
 future<>
 sharded<Service>::stop() {
@@ -579,7 +628,7 @@ sharded<Service>::stop() {
             if (!inst) {
                 return make_ready_future<>();
             }
-            return inst->stop();
+            return internal::stop_sharded_instance(*inst);
         });
     }).then([this] {
         return internal::sharded_parallel_for_each(_instances.size(), [this] (unsigned c) {
