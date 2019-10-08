5d20255b438f604746680542754e1a43a2929d28
Asias He <asias@scylladb.com>
2019-10-08 20:27:02 +0800

    reactor: Move posix_file_impl to file.cc
    Signed-off-by: Asias He <asias@scylladb.com>

--- src/core/file.cc
+++ src/core/file.cc
@@ -19,9 +19,28 @@
  * Copyright 2019 ScyllaDB
  */
 
+#define __user /* empty */  // for xfs includes, below
+
+#include <sys/syscall.h>
+#include <dirent.h>
+#include <linux/types.h> // for xfs, below
+#include <sys/ioctl.h>
+#include <xfs/linux.h>
+#define min min    /* prevent xfs.h from defining min() as a macro */
+#include <xfs/xfs.h>
+#undef min
+#include <seastar/core/reactor.hh>
 #include <seastar/core/file.hh>
+#include <seastar/core/report_exception.hh>
+#include <seastar/core/linux-aio.hh>
+#include "core/file-impl.hh"
+#include "core/syscall_result.hh"
+#include "core/thread_pool.hh"
 
 namespace seastar {
+
+using namespace internal;
+using namespace internal::linux_abi;
 
 file_handle::file_handle(const file_handle& x)
         : _impl(x._impl ? x._impl->clone() : std::unique_ptr<file_handle_impl>()) {
@@ -45,6 +64,401 @@ file_handle::to_file() const & {
 file
 file_handle::to_file() && {
     return file(std::move(*_impl).to_file());
+}
+
+posix_file_impl::posix_file_impl(int fd, open_flags f, file_open_options options, io_queue* ioq)
+        : _io_queue(ioq)
+        , _open_flags(f)
+        , _fd(fd)
+{
+    query_dma_alignment();
+}
+
+posix_file_impl::~posix_file_impl() {
+    if (_refcount && _refcount->fetch_add(-1, std::memory_order_relaxed) != 1) {
+        return;
+    }
+    delete _refcount;
+    if (_fd != -1) {
+        // Note: close() can be a blocking operation on NFS
+        ::close(_fd);
+    }
+}
+
+void
+posix_file_impl::query_dma_alignment() {
+    dioattr da;
+    auto r = ioctl(_fd, XFS_IOC_DIOINFO, &da);
+    if (r == 0) {
+        _memory_dma_alignment = da.d_mem;
+        _disk_read_dma_alignment = da.d_miniosz;
+        // xfs wants at least the block size for writes
+        // FIXME: really read the block size
+        _disk_write_dma_alignment = std::max<unsigned>(da.d_miniosz, 4096);
+    }
+}
+
+std::unique_ptr<seastar::file_handle_impl>
+posix_file_impl::dup() {
+    if (!_refcount) {
+        _refcount = new std::atomic<unsigned>(1u);
+    }
+    auto ret = std::make_unique<posix_file_handle_impl>(_fd, _open_flags, _refcount, _io_queue);
+    _refcount->fetch_add(1, std::memory_order_relaxed);
+    return ret;
+}
+
+posix_file_impl::posix_file_impl(int fd, open_flags f, std::atomic<unsigned>* refcount, io_queue *ioq)
+        : _refcount(refcount), _io_queue(ioq), _open_flags(f), _fd(fd) {
+}
+
+future<>
+posix_file_impl::flush(void) {
+    if ((_open_flags & open_flags::dsync) != open_flags{}) {
+        return make_ready_future<>();
+    }
+    return engine().fdatasync(_fd);
+}
+
+future<struct stat>
+posix_file_impl::stat(void) {
+    return engine()._thread_pool->submit<syscall_result_extra<struct stat>>([this] {
+        struct stat st;
+        auto ret = ::fstat(_fd, &st);
+        return wrap_syscall(ret, st);
+    }).then([] (syscall_result_extra<struct stat> ret) {
+        ret.throw_if_error();
+        return make_ready_future<struct stat>(ret.extra);
+    });
+}
+
+future<>
+posix_file_impl::truncate(uint64_t length) {
+    return engine()._thread_pool->submit<syscall_result<int>>([this, length] {
+        return wrap_syscall<int>(::ftruncate(_fd, length));
+    }).then([] (syscall_result<int> sr) {
+        sr.throw_if_error();
+        return make_ready_future<>();
+    });
+}
+
+future<>
+posix_file_impl::discard(uint64_t offset, uint64_t length) {
+    return engine()._thread_pool->submit<syscall_result<int>>([this, offset, length] () mutable {
+        return wrap_syscall<int>(::fallocate(_fd, FALLOC_FL_PUNCH_HOLE|FALLOC_FL_KEEP_SIZE,
+            offset, length));
+    }).then([] (syscall_result<int> sr) {
+        sr.throw_if_error();
+        return make_ready_future<>();
+    });
+}
+
+future<>
+posix_file_impl::allocate(uint64_t position, uint64_t length) {
+#ifdef FALLOC_FL_ZERO_RANGE
+    // FALLOC_FL_ZERO_RANGE is fairly new, so don't fail if it's not supported.
+    static bool supported = true;
+    if (!supported) {
+        return make_ready_future<>();
+    }
+    return engine()._thread_pool->submit<syscall_result<int>>([this, position, length] () mutable {
+        auto ret = ::fallocate(_fd, FALLOC_FL_ZERO_RANGE|FALLOC_FL_KEEP_SIZE, position, length);
+        if (ret == -1 && errno == EOPNOTSUPP) {
+            ret = 0;
+            supported = false; // Racy, but harmless.  At most we issue an extra call or two.
+        }
+        return wrap_syscall<int>(ret);
+    }).then([] (syscall_result<int> sr) {
+        sr.throw_if_error();
+        return make_ready_future<>();
+    });
+#else
+    return make_ready_future<>();
+#endif
+}
+
+future<uint64_t>
+posix_file_impl::size() {
+    auto r = ::lseek(_fd, 0, SEEK_END);
+    if (r == -1) {
+        return make_exception_future<uint64_t>(std::system_error(errno, std::system_category()));
+    }
+    return make_ready_future<uint64_t>(r);
+}
+
+future<>
+posix_file_impl::close() noexcept {
+    if (_fd == -1) {
+        seastar_logger.warn("double close() detected, contact support");
+        return make_ready_future<>();
+    }
+    auto fd = _fd;
+    _fd = -1;  // Prevent a concurrent close (which is illegal) from closing another file's fd
+    if (_refcount && _refcount->fetch_add(-1, std::memory_order_relaxed) != 1) {
+        _refcount = nullptr;
+        return make_ready_future<>();
+    }
+    delete _refcount;
+    _refcount = nullptr;
+    auto closed = [fd] () noexcept {
+        try {
+            return engine()._thread_pool->submit<syscall_result<int>>([fd] {
+                return wrap_syscall<int>(::close(fd));
+            });
+        } catch (...) {
+            report_exception("Running ::close() in reactor thread, submission failed with exception", std::current_exception());
+            return make_ready_future<syscall_result<int>>(wrap_syscall<int>(::close(fd)));
+        }
+    }();
+    return closed.then([] (syscall_result<int> sr) {
+        sr.throw_if_error();
+    });
+}
+
+future<uint64_t>
+blockdev_file_impl::size(void) {
+    return engine()._thread_pool->submit<syscall_result_extra<size_t>>([this] {
+        uint64_t size;
+        int ret = ::ioctl(_fd, BLKGETSIZE64, &size);
+        return wrap_syscall(ret, size);
+    }).then([] (syscall_result_extra<uint64_t> ret) {
+        ret.throw_if_error();
+        return make_ready_future<uint64_t>(ret.extra);
+    });
+}
+
+subscription<directory_entry>
+posix_file_impl::list_directory(std::function<future<> (directory_entry de)> next) {
+    static constexpr size_t buffer_size = 8192;
+    struct work {
+        stream<directory_entry> s;
+        unsigned current = 0;
+        unsigned total = 0;
+        bool eof = false;
+        int error = 0;
+        char buffer[buffer_size];
+    };
+
+    // While it would be natural to use fdopendir()/readdir(),
+    // our syscall thread pool doesn't support malloc(), which is
+    // required for this to work.  So resort to using getdents()
+    // instead.
+
+    // From getdents(2):
+    struct linux_dirent64 {
+        ino64_t        d_ino;    /* 64-bit inode number */
+        off64_t        d_off;    /* 64-bit offset to next structure */
+        unsigned short d_reclen; /* Size of this dirent */
+        unsigned char  d_type;   /* File type */
+        char           d_name[]; /* Filename (null-terminated) */
+    };
+
+    auto w = make_lw_shared<work>();
+    auto ret = w->s.listen(std::move(next));
+    // List the directory asynchronously in the background.
+    // Caller synchronizes using the returned subscription.
+    (void)w->s.started().then([w, this] {
+        auto eofcond = [w] { return w->eof; };
+        return do_until(eofcond, [w, this] {
+            if (w->current == w->total) {
+                return engine()._thread_pool->submit<syscall_result<long>>([w , this] () {
+                    auto ret = ::syscall(__NR_getdents64, _fd, reinterpret_cast<linux_dirent64*>(w->buffer), buffer_size);
+                    return wrap_syscall(ret);
+                }).then([w] (syscall_result<long> ret) {
+                    ret.throw_if_error();
+                    if (ret.result == 0) {
+                        w->eof = true;
+                    } else {
+                        w->current = 0;
+                        w->total = ret.result;
+                    }
+                });
+            }
+            auto start = w->buffer + w->current;
+            auto de = reinterpret_cast<linux_dirent64*>(start);
+            compat::optional<directory_entry_type> type;
+            switch (de->d_type) {
+            case DT_BLK:
+                type = directory_entry_type::block_device;
+                break;
+            case DT_CHR:
+                type = directory_entry_type::char_device;
+                break;
+            case DT_DIR:
+                type = directory_entry_type::directory;
+                break;
+            case DT_FIFO:
+                type = directory_entry_type::fifo;
+                break;
+            case DT_REG:
+                type = directory_entry_type::regular;
+                break;
+            case DT_LNK:
+                type = directory_entry_type::link;
+                break;
+            case DT_SOCK:
+                type = directory_entry_type::socket;
+                break;
+            default:
+                // unknown, ignore
+                ;
+            }
+            w->current += de->d_reclen;
+            sstring name = de->d_name;
+            if (name == "." || name == "..") {
+                return make_ready_future<>();
+            }
+            return w->s.produce({std::move(name), type});
+        });
+    }).then([w] {
+        w->s.close();
+    }).handle_exception([] (std::exception_ptr ignored) {});
+    return ret;
+}
+
+future<size_t>
+posix_file_impl::write_dma(uint64_t pos, const void* buffer, size_t len, const io_priority_class& io_priority_class) {
+    return engine().submit_io_write(_io_queue, io_priority_class, len, [fd = _fd, pos, buffer, len] (iocb& io) {
+        io = make_write_iocb(fd, pos, const_cast<void*>(buffer), len);
+    }).then([] (io_event ev) {
+        engine().handle_io_result(ev);
+        return make_ready_future<size_t>(size_t(ev.res));
+    });
+}
+
+future<size_t>
+posix_file_impl::write_dma(uint64_t pos, std::vector<iovec> iov, const io_priority_class& io_priority_class) {
+    auto len = internal::sanitize_iovecs(iov, _disk_write_dma_alignment);
+    auto size = iov.size();
+    auto data = iov.data();
+    return engine().submit_io_write(_io_queue, io_priority_class, len, [fd = _fd, pos, data, size] (iocb& io) {
+        io = make_writev_iocb(fd, pos, data, size);
+    }).then([iov = std::move(iov)] (io_event ev) {
+        engine().handle_io_result(ev);
+        return make_ready_future<size_t>(size_t(ev.res));
+    });
+}
+
+future<size_t>
+posix_file_impl::read_dma(uint64_t pos, void* buffer, size_t len, const io_priority_class& io_priority_class) {
+    return engine().submit_io_read(_io_queue, io_priority_class, len, [fd = _fd, pos, buffer, len] (iocb& io) {
+        io = make_read_iocb(fd, pos, buffer, len);
+    }).then([] (io_event ev) {
+        engine().handle_io_result(ev);
+        return make_ready_future<size_t>(size_t(ev.res));
+    });
+}
+
+future<size_t>
+posix_file_impl::read_dma(uint64_t pos, std::vector<iovec> iov, const io_priority_class& io_priority_class) {
+    auto len = internal::sanitize_iovecs(iov, _disk_read_dma_alignment);
+    auto size = iov.size();
+    auto data = iov.data();
+    return engine().submit_io_read(_io_queue, io_priority_class, len, [fd = _fd, pos, data, size] (iocb& io) {
+        io = make_readv_iocb(fd, pos, data, size);
+    }).then([iov = std::move(iov)] (io_event ev) {
+        engine().handle_io_result(ev);
+        return make_ready_future<size_t>(size_t(ev.res));
+    });
+}
+
+future<temporary_buffer<uint8_t>>
+posix_file_impl::dma_read_bulk(uint64_t offset, size_t range_size, const io_priority_class& pc) {
+    using tmp_buf_type = typename file::read_state<uint8_t>::tmp_buf_type;
+
+    auto front = offset & (_disk_read_dma_alignment - 1);
+    offset -= front;
+    range_size += front;
+
+    auto rstate = make_lw_shared<file::read_state<uint8_t>>(offset, front,
+                                                       range_size,
+                                                       _memory_dma_alignment,
+                                                       _disk_read_dma_alignment);
+
+    //
+    // First, try to read directly into the buffer. Most of the reads will
+    // end here.
+    //
+    auto read = read_dma(offset, rstate->buf.get_write(),
+                         rstate->buf.size(), pc);
+
+    return read.then([rstate, this, &pc] (size_t size) mutable {
+        rstate->pos = size;
+
+        //
+        // If we haven't read all required data at once -
+        // start read-copy sequence. We can't continue with direct reads
+        // into the previously allocated buffer here since we have to ensure
+        // the aligned read length and thus the aligned destination buffer
+        // size.
+        //
+        // The copying will actually take place only if there was a HW glitch.
+        // In EOF case or in case of a persistent I/O error the only overhead is
+        // an extra allocation.
+        //
+        return do_until(
+            [rstate] { return rstate->done(); },
+            [rstate, this, &pc] () mutable {
+            return read_maybe_eof(
+                rstate->cur_offset(), rstate->left_to_read(), pc).then(
+                    [rstate] (auto buf1) mutable {
+                if (buf1.size()) {
+                    rstate->append_new_data(buf1);
+                } else {
+                    rstate->eof = true;
+                }
+
+                return make_ready_future<>();
+            });
+        }).then([rstate] () mutable {
+            //
+            // If we are here we are promised to have read some bytes beyond
+            // "front" so we may trim straight away.
+            //
+            rstate->trim_buf_before_ret();
+            return make_ready_future<tmp_buf_type>(std::move(rstate->buf));
+        });
+    });
+}
+
+future<temporary_buffer<uint8_t>>
+posix_file_impl::read_maybe_eof(uint64_t pos, size_t len, const io_priority_class& pc) {
+    //
+    // We have to allocate a new aligned buffer to make sure we don't get
+    // an EINVAL error due to unaligned destination buffer.
+    //
+    temporary_buffer<uint8_t> buf = temporary_buffer<uint8_t>::aligned(
+               _memory_dma_alignment, align_up(len, size_t(_disk_read_dma_alignment)));
+
+    // try to read a single bulk from the given position
+    auto dst = buf.get_write();
+    auto buf_size = buf.size();
+    return read_dma(pos, dst, buf_size, pc).then_wrapped(
+            [buf = std::move(buf)](future<size_t> f) mutable {
+        try {
+            size_t size = std::get<0>(f.get());
+
+            buf.trim(size);
+
+            return std::move(buf);
+        } catch (std::system_error& e) {
+            //
+            // TODO: implement a non-trowing file_impl::dma_read() interface to
+            //       avoid the exceptions throwing in a good flow completely.
+            //       Otherwise for users that don't want to care about the
+            //       underlying file size and preventing the attempts to read
+            //       bytes beyond EOF there will always be at least one
+            //       exception throwing at the file end for files with unaligned
+            //       length.
+            //
+            if (e.code().value() == EINVAL) {
+                buf.trim(0);
+                return std::move(buf);
+            } else {
+                throw;
+            }
+        }
+    });
 }
 
 }
