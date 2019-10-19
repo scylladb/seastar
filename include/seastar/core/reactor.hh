3652e4e658c7878e2fe09fe6dedad0c7343a3aff
Asias He <asias@scylladb.com>
2019-10-19 23:12:54 +0800

    reactor: Add reactor_config.hh
    Signed-off-by: Asias He <asias@scylladb.com>

--- include/seastar/core/reactor.hh
+++ include/seastar/core/reactor.hh
@@ -54,6 +54,7 @@
 #include <boost/thread/barrier.hpp>
 #include <boost/container/static_vector.hpp>
 #include <set>
+#include <seastar/core/reactor_config.hh>
 #include <seastar/core/linux-aio.hh>
 #include <seastar/util/eclipse.hh>
 #include <seastar/core/future.hh>
@@ -95,25 +96,6 @@ extern "C" int _Unwind_RaiseException(struct _Unwind_Exception *h);
 namespace seastar {
 
 using shard_id = unsigned;
-
-/// Configuration structure for reactor
-///
-/// This structure provides configuration items for the reactor. It is typically
-/// provided by \ref app_template, not the user.
-struct reactor_config {
-    std::chrono::duration<double> task_quota{0.5e-3}; ///< default time between polls
-    /// \brief Handle SIGINT/SIGTERM by calling reactor::stop()
-    ///
-    /// When true, Seastar will set up signal handlers for SIGINT/SIGTERM that call
-    /// reactor::stop(). The reactor will then execute callbacks installed by
-    /// reactor::at_exit().
-    ///
-    /// When false, Seastar will not set up signal handlers for SIGINT/SIGTERM
-    /// automatically. The default behavior (terminate the program) will be kept.
-    /// You can adjust the behavior of SIGINT/SIGTERM by installing signal handlers
-    /// via reactor::handle_signal().
-    bool auto_handle_sigint_sigterm = true;  ///< automatically terminate on SIGINT/SIGTERM
-};
 
 namespace alien {
 class message_queue;
