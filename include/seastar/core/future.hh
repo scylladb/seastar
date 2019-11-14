2888b5d8bdff85f9766e522ee1032fd8d8888ed8
Radoslaw Zarzynski <rzarzyns@redhat.com>
2019-11-15 00:58:48 +0100

    future: don't create temporaries on get_value().
    `seastar::future_state<T>::get_value()`, used on the hot path of
    `::then()`, returns `std::tuple<T>` by value. This brings to live
    a temporary object which can be seen on following dump obtained
    from `crimson-osd` compiled with GCC 8.1.0:
    
    ```
        [[gnu::always_inline]]
        future_state<T...> get_available_state() noexcept {
            if (_promise) {
        f160:	48 85 d2             	test   %rdx,%rdx
        f163:	74 21                	je     f186 <ceph::osd::OpsExecuter::execute_osd_op(OSDOp&)+0x3c6>
                detach_promise();
            }
        }
    ...
        f186:	48 89 85 e0 fe ff ff 	mov    %rax,-0x120(%rbp)
        static_assert(std::is_nothrow_destructible<std::tuple<T...>>::value,
                      "Types must be no-throw destructible");
        future_state() noexcept {}
        [[gnu::always_inline]]
        future_state(future_state&& x) noexcept : future_state_base(std::move(x)) {
            if (_u.st == state::result) {
        f18d:	48 83 bd e0 fe ff ff 	cmpq   $0x2,-0x120(%rbp)
        f194:	02
                if (x.st < state::exception_min) {
                    st = x.st;
                } else {
                    new (&ex) std::exception_ptr(x.take_exception());
                }
                x.st = state::invalid;
        f195:	48 c7 85 38 ff ff ff 	movq   $0x0,-0xc8(%rbp)
        f19c:	00 00 00 00
        static_assert(std::is_nothrow_destructible<std::tuple<T...>>::value,
                      "Types must be no-throw destructible");
        future_state() noexcept {}
        [[gnu::always_inline]]
        future_state(future_state&& x) noexcept : future_state_base(std::move(x)) {
            if (_u.st == state::result) {
        f1a0:	0f 84 9f 03 00 00    	je     f545 <ceph::osd::OpsExecuter::execute_osd_op(OSDOp&)+0x785>
    ...
        f545:	4c 8d b5 90 fe ff ff 	lea    -0x170(%rbp),%r14
        f54c:	49 8d 75 10          	lea    0x10(%r13),%rsi
        f550:	4c 89 f7             	mov    %r14,%rdi
        f553:	4c 8d bd e0 fe ff ff 	lea    -0x120(%rbp),%r15
        f55a:	e8 00 00 00 00       	callq  f55f <ceph::osd::OpsExecuter::execute_osd_op(OSDOp&)+0x79f>
        f55f:	49 8d 7f 08          	lea    0x8(%r15),%rdi
        f563:	4c 89 f6             	mov    %r14,%rsi
        f566:	e8 00 00 00 00       	callq  f56b <ceph::osd::OpsExecuter::execute_osd_op(OSDOp&)+0x7ab>
        f56b:	48 8b bd 90 fe ff ff 	mov    -0x170(%rbp),%rdi
        f572:	4c 39 f7             	cmp    %r14,%rdi
        f575:	74 24                	je     f59b <ceph::osd::OpsExecuter::execute_osd_op(OSDOp&)+0x7db>
        f577:	66 0f 1f 84 00 00 00 	nopw   0x0(%rax,%rax,1)
    ```
    
    The fragment starting at `f545` after relocations and demangling:
    ```
      8587b5:       4c 8d b5 90 fe ff ff    lea    -0x170(%rbp),%r14
      8587bc:       49 8d 75 10             lea    0x10(%r13),%rsi
      8587c0:       4c 89 f7                mov    %r14,%rdi
      8587c3:       4c 8d bd e0 fe ff ff    lea    -0x120(%rbp),%r15
      8587ca:       e8 51 6a 33 00          callq  b8f220 <ceph::buffer::v14_2_0::list::list(ceph::buffer::v14_2_0::list&&)>
      8587cf:       49 8d 7f 08             lea    0x8(%r15),%rdi
      8587d3:       4c 89 f6                mov    %r14,%rsi
      8587d6:       e8 45 6a 33 00          callq  b8f220 <ceph::buffer::v14_2_0::list::list(ceph::buffer::v14_2_0::list&&)>
    ```
    
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>
    Message-Id: <1573775931-12826-1-git-send-email-rzarzyns@redhat.com>

--- include/seastar/core/future.hh
+++ include/seastar/core/future.hh
@@ -351,12 +351,12 @@ struct future_state :  public future_state_base, private internal::uninitialized
         new (this) future_state(ready_future_marker(), std::forward<A>(a)...);
     }
     future_state(exception_future_marker m, std::exception_ptr&& ex) : future_state_base(std::move(ex)) { }
-    std::tuple<T...> get_value() && noexcept {
+    std::tuple<T...>&& get_value() && noexcept {
         assert(_u.st == state::result);
         return std::move(this->uninitialized_get());
     }
     template<typename U = std::tuple<T...>>
-    std::enable_if_t<std::is_copy_constructible<U>::value, U> get_value() const& noexcept(copy_noexcept) {
+    const std::enable_if_t<std::is_copy_constructible<U>::value, U>& get_value() const& noexcept(copy_noexcept) {
         assert(_u.st == state::result);
         return this->uninitialized_get();
     }
