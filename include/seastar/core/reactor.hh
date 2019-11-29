scylladb/seastar

commit 5b89e8fefa625ffcc4ba2a1e737539ab9ea064c0

reactor: Simplify network initialization
Currently the network initialization is a bit convoluted. We create a
future just to call set_value on a promise.

Avoid that by replacing _network_stack_ready_promise with an optional
future.

Signed-off-by: Rafael Ávila de Espíndola <espindola@scylladb.com>
Message-Id: <20191122185632.13405-1-espindola@scylladb.com>

include/seastar/core/reactor.hh
@@ -258,7 +258,7 @@ private:
     bool _stopped = false;
     condition_variable _stop_requested;
     bool _handle_sigint = true;
-    promise<std::unique_ptr<network_stack>> _network_stack_ready_promise;
+    std::optional<future<std::unique_ptr<network_stack>>> _network_stack_ready;
     int _return = 0;
     promise<> _start_promise;
     semaphore _cpu_started;

src/core/reactor.cc
@@ -1242,13 +1242,9 @@ public:
 };
 
 void reactor::configure(boost::program_options::variables_map vm) {
-    auto network_stack_ready = vm.count("network-stack")
+    _network_stack_ready = vm.count("network-stack")
         ? network_stack_registry::create(sstring(vm["network-stack"].as<std::string>()), vm)
         : network_stack_registry::create(vm);
-    // FIXME: future is discarded
-    (void)network_stack_ready.then([this] (std::unique_ptr<network_stack> stack) {
-        _network_stack_ready_promise.set_value(std::move(stack));
-    });
 
     _handle_sigint = !vm.count("no-handle-interrupt");
     auto task_quota = vm["task-quota-ms"].as<double>() * 1ms;
@@ -2585,7 +2581,7 @@ int reactor::run() {
         });
     });
     // Wait for network stack in the background and then signal all cpus.
-    (void)_network_stack_ready_promise.get_future().then([this] (std::unique_ptr<network_stack> stack) {
+    (void)_network_stack_ready->then([this] (std::unique_ptr<network_stack> stack) {
         _network_stack = std::move(stack);
         return smp::invoke_on_all([] {
             engine()._cpu_started.signal();

commit 93845eb289ea666c29e8146b700b7c4063b1151b

reactor: replace std::optional (in _network_stack_ready) with compat::optional
std::optional isn't available in C++14, so use compat::optional instead (which
dispatches to either std::optional, or std::experimental::optional, depending
on what is available).

Broken since 5b89e8fefa625ff.
Message-Id: <20191128140922.468328-1-avi@scylladb.com>

include/seastar/core/reactor.hh
@@ -258,7 +258,7 @@ private:
     bool _stopped = false;
     condition_variable _stop_requested;
     bool _handle_sigint = true;
-    std::optional<future<std::unique_ptr<network_stack>>> _network_stack_ready;
+    compat::optional<future<std::unique_ptr<network_stack>>> _network_stack_ready;
     int _return = 0;
     promise<> _start_promise;
     semaphore _cpu_started;