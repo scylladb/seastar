dfbcceae1b0af4bdc451623d1da0be5774f9b5de
Rafael Ávila de Espíndola <espindola@scylladb.com>
2020-01-07 08:55:13 -0800

    future-util: Move parallel_for_each_state code to a .cc
    The fast path of parallel_for_each doesn't use parallel_for_each_state
    at all, so it makes sense to move the related code to a .cc file.
    
    This patch also moves failed futures to the slow path and refactors
    the code a bit so that more logic is with parallel_for_each_state.
    
    This is a 0.42% improvement in .text size (from 41735428 to 41560324
    bytes) and a 2.2% improvement in
    "perf_simple_query-patch --duration 16 --smp 1 -m4G":
    
    master
    median 145075.59
    median absolute deviation: 194.48
    maximum: 145812.19
    minimum: 144769.14
    
    patch
    median 148295.81
    median absolute deviation: 263.70
    maximum: 148639.72
    minimum: 146785.09
    
    Signed-off-by: Rafael Ávila de Espíndola <espindola@scylladb.com>
    Message-Id: <20200107165513.9982-1-espindola@scylladb.com>

--- src/core/reactor.cc
+++ src/core/reactor.cc
@@ -3925,6 +3925,59 @@ void report_failed_future(const future_state_base& state) noexcept {
     report_failed_future(state._u.ex);
 }
 
+parallel_for_each_state::parallel_for_each_state(size_t n) {
+    _incomplete.reserve(n);
+}
+
+future<> parallel_for_each_state::get_future() {
+    auto ret = _result.get_future();
+    wait_for_one();
+    return ret;
+}
+
+void parallel_for_each_state::add_future(future<>&& f) {
+    _incomplete.push_back(std::move(f));
+}
+
+void parallel_for_each_state::wait_for_one() {
+    // Process from back to front, on the assumption that the front
+    // futures are likely to complete earlier than the back futures.
+    // If that's indeed the case, then the front futures will be
+    // available and we won't have to wait for them.
+
+    // Skip over futures that happen to be complete already.
+    while (!_incomplete.empty() && _incomplete.back().available()) {
+        if (_incomplete.back().failed()) {
+            _ex = _incomplete.back().get_exception();
+        }
+        _incomplete.pop_back();
+    }
+
+    // If there's an incompelete future, wait for it.
+    if (!_incomplete.empty()) {
+        internal::set_callback(_incomplete.back(), std::unique_ptr<continuation_base<>>(this));
+        // This future's state will be collected in run_and_dispose(), so we can drop it.
+        _incomplete.pop_back();
+        return;
+    }
+
+    // Everything completed, report a result.
+    if (__builtin_expect(bool(_ex), false)) {
+        _result.set_exception(std::move(_ex));
+    } else {
+        _result.set_value();
+    }
+    delete this;
+}
+
+void parallel_for_each_state::run_and_dispose() noexcept {
+    if (_state.failed()) {
+        _ex = std::move(_state).get_exception();
+    }
+    _state = {};
+    wait_for_one();
+}
+
 broken_promise::broken_promise() : logic_error("broken promise") { }
 
 promise_base::promise_base(promise_base&& x) noexcept
