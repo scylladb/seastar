6b9a7598197ce9694effa72000d0a2e864176a86
Kefu Chai <kchai@redhat.com>
2020-02-20 15:34:52 +0800

    build: check for pthread_setname_np()
    strictly speaking pthread_setname_np() is not portable. we
    need to at least check for its existence before using it, and fail
    when checking for dependencies when running "cmake", instead of
    failing when building seastar.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>
    Message-Id: <20200220073452.482910-3-tchaikov@gmail.com>

---
+++ cmake/FindPthreadSetName.cmake
@@ -0,0 +1,12 @@
+include (CMakePushCheckState)
+
+cmake_push_check_state (RESET)
+set (CMAKE_REQUIRED_FLAGS "-pthread")
+set (CMAKE_REQUIRED_DEFINITIONS "-D_GNU_SOURCE")
+check_symbol_exists (pthread_setname_np pthread.h HAVE_PTHREAD_SETNAME_NP)
+cmake_pop_check_state ()
+
+find_package_handle_standard_args (PthreadSetName
+  FOUND_VAR PthreadSetName_FOUND
+  REQUIRED_VARS
+    HAVE_PTHREAD_SETNAME_NP)
