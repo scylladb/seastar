521009c7959d3dea94bc4277df79e3fe78e42e44
Rafael Ávila de Espíndola <espindola@scylladb.com>
2020-02-21 13:34:13 -0800

    core: Add a on_internal_error helper
    In scylla there are places where we want to check an invariant, but
    not bring down the database if it fails.
    
    Like the invariants checked with assert, these are things that should
    never happen and indicate bugs, not just unusual issues like a
    corrupted file.
    
    Unlike the invariants checked with assert, we are reasonably confident
    that it is possible to abort a particular task without bringing down
    the entire server.
    
    To handle that, scylla has a on_internal_error function that will
    either throw or abort depending on a runtime flag.
    
    This patch moves on_internal_error to seastar. It seems general enough
    and we want this flag to be always set to abort during tests, which is
    more naturally done in seastar.
    
    Signed-off-by: Rafael Ávila de Espíndola <espindola@scylladb.com>
    Message-Id: <20200221213414.506150-2-espindola@scylladb.com>

---
+++ include/seastar/core/on_internal_error.hh
@@ -0,0 +1,39 @@
+/*
+ * This file is open source software, licensed to you under the terms
+ * of the Apache License, Version 2.0 (the "License").  See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership.  You may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/*
+ * Copyright 2020 ScyllaDB
+ */
+
+#pragma once
+
+#include <seastar/util/std-compat.hh>
+
+namespace seastar {
+
+class logger;
+
+/// Controls whether on_internal_error() aborts or throws. The default
+/// is to throw.
+void set_abort_on_internal_error(bool do_abort);
+
+/// Report an internal error
+///
+/// Depending on the value passed to set_abort_on_internal_error, this
+/// will either log to \p logger and abort or throw a std::runtime_error.
+[[noreturn]] void on_internal_error(logger& logger, compat::string_view reason);
+}
