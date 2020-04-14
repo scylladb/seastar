f846a348b3f2c9a6bd71f1fc087b9e5c40717b28
Gleb Natapov <gleb@scylladb.com>
2020-04-14 11:02:57 +0300

    rpc: always shutdown socket when stopping a client
    When stopping a client socket::shutdown() needs to be called to abort
    send and receive loop, but it is skipped if _error flag is already set.
    Set _error flag does not mean though that both loops already aware
    that they need to shutdown.
    
    The path calls the shutdown() unconditionally. It is safe to call
    socket::shutdown() if the error left an underlying socket disconnected
    since shutdown() does not throw in this case.
    
    Refs scylladb/scylla#6060
    
    Message-Id: <20200414080257.GA27043@scylladb.com>

--- src/rpc/rpc.cc
+++ src/rpc/rpc.cc
@@ -613,9 +613,11 @@ namespace rpc {
   }
 
   future<> client::stop() {
-      if (!_error) {
-          _error = true;
+      _error = true;
+      try {
           _socket.shutdown();
+      } catch(...) {
+          log_exception(*this, log_level::error, "fail to shutdown connection while stopping", std::current_exception());
       }
       return _stopped.get_future();
   }
