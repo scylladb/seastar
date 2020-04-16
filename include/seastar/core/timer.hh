339f34fef43207c50bb5dc5a317671d9257046e8
Avi Kivity <avi@scylladb.com>
2020-04-16 15:47:18 +0300

    timer: document
    Document the timer class. It is placed into a new "timers"
    module - I could not find a better place for it.
    
    The private type callback_t in method signatures was expanded
    to noncopyable_function<void ()> - it's not reasonable to use
    a private type in a signature, and even making it public requires
    extra effort to figure out what it is. I left internal uses as is.
    Message-Id: <20200416124718.95132-1-avi@scylladb.com>

--- include/seastar/core/timer.hh
+++ include/seastar/core/timer.hh
@@ -29,10 +29,51 @@
 #include <seastar/core/timer-set.hh>
 #include <seastar/core/scheduling.hh>
 
+/// \file
+
+/// \defgroup timers Timers
+///
+/// Seastar provides timers that can be defined to run a callback at a certain
+/// time point in the future; timers are provided for \ref lowres_clock (10ms
+/// resolution, efficient), for std::chrono::steady_clock (accurate but less
+/// efficient) and for \ref manual_clock_type (for testing purposes).
+///
+/// Timers are optimized for cancellation; that is, adding a timer and cancelling
+/// it is very efficient. This means that attaching a timer per object for
+/// a timeout that rarely happens is reasonable; one does not have to maintain
+/// a single timer and a sorted list for this use case.
+///
+/// Timer callbacks should be short and execute quickly. If involved processing
+/// is required, a timer can launch a continuation.
+
 namespace seastar {
 
 using steady_clock_type = std::chrono::steady_clock;
 
+
+/// \addtogroup timers
+/// @{
+
+/// Timer - run a callback at a certain time point in the future.
+///
+/// Timer callbacks should execute quickly. If more involved computation
+/// is required, the timer should launch it as a fiber (or signal an
+/// existing fiber to continue execution). Fibers launched from a timer
+/// callback are executed under the scheduling group that was current
+/// when the timer was created (see current_scheduling_group()), or the
+/// scheduling that was given explicitly by the caller when the callback
+/// was specified.
+///
+/// Expiration of a `timer<std::chrono::steady_clock>` is independent of
+/// task_quota, so it has relatively high accuracy, but as a result this
+/// is a relatively expensive timer. It is recommended to use `timer<lowres_clock>`
+/// instead, which has very coarse resolution (~10ms) but is quite efficient.
+/// It is suitable for most user timeouts.
+///
+/// \tparam Clock type of clock used to denote time points; can be
+///        std::chrono::steady_clock_type (default), lowres_clock (more efficient
+///        but with less resolution) and manual_clock_type (fine-grained control
+///        for testing.
 template <typename Clock = steady_clock_type>
 class timer {
 public:
@@ -59,40 +100,103 @@ private:
         _queued = true;
     }
 public:
+    /// Constructs a timer with no callback set and no expiration time.
     timer() = default;
+    /// Constructs a timer from another timer that is moved from.
+    ///
+    /// \note care should be taken when moving a timer whose callback captures `this`,
+    ///       since the object pointed to by `this` may have been moved as well.
     timer(timer&& t) noexcept : _sg(t._sg), _callback(std::move(t._callback)), _expiry(std::move(t._expiry)), _period(std::move(t._period)),
             _armed(t._armed), _queued(t._queued), _expired(t._expired) {
         _link.swap_nodes(t._link);
         t._queued = false;
         t._armed = false;
     }
-    timer(scheduling_group sg, callback_t&& callback) : _sg(sg), _callback{std::move(callback)} {
+    /// Constructs a timer with a callback. The timer is not armed.
+    ///
+    /// \param sg Scheduling group to run the callback under.
+    /// \param callback function (with signature `void ()`) to execute after the timer is armed and expired.
+    timer(scheduling_group sg, noncopyable_function<void ()>&& callback) : _sg(sg), _callback{std::move(callback)} {
     }
-    explicit timer(callback_t&& callback) : timer(current_scheduling_group(), std::move(callback)) {
+    /// Constructs a timer with a callback. The timer is not armed.
+    ///
+    /// \param callback function (with signature `void ()`) to execute after the timer is armed and expired.
+    explicit timer(noncopyable_function<void ()>&& callback) : timer(current_scheduling_group(), std::move(callback)) {
     }
+    /// Destroys the timer. The timer is cancelled if armed.
     ~timer();
-    void set_callback(scheduling_group sg, callback_t&& callback) {
+    /// Sets the callback function to be called when the timer expires.
+    ///
+    /// \param sg the scheduling group under which the callback will be executed.
+    /// \param callback the callback to be executed when the timer expires.
+    void set_callback(scheduling_group sg, noncopyable_function<void ()>&& callback) {
         _sg = sg;
         _callback = std::move(callback);
     }
-    void set_callback(callback_t&& callback) {
+    /// Sets the callback function to be called when the timer expires.
+    ///
+    /// \param callback the callback to be executed when the timer expires.
+    void set_callback(noncopyable_function<void ()>&& callback) {
         set_callback(current_scheduling_group(), std::move(callback));
     }
+    /// Sets the timer expiration time.
+    ///
+    /// It is illegal to arm a timer that has already been armed (and
+    /// not disarmed by expiration or cancel()). In the current
+    /// implementation, this will result in an assertion failure. See
+    /// rearm().
+    ///
+    /// \param until the time when the timer expires
+    /// \param period optional automatic rearm duration; if given the timer
+    ///        will automatically rearm itself when it expires, using the period
+    ///        to calculate the next expiration time.
     void arm(time_point until, compat::optional<duration> period = {});
+    /// Sets the timer expiration time. If the timer was already armed, it is
+    /// canceled first.
+    ///
+    /// \param until the time when the timer expires
+    /// \param period optional automatic rearm duration; if given the timer
+    ///        will automatically rearm itself when it expires, using the period
+    ///        to calculate the next expiration time.
     void rearm(time_point until, compat::optional<duration> period = {}) {
         if (_armed) {
             cancel();
         }
         arm(until, period);
     }
+    /// Sets the timer expiration time.
+    ///
+    /// It is illegal to arm a timer that has already been armed (and
+    /// not disarmed by expiration or cancel()). In the current
+    /// implementation, this will result in an assertion failure. See
+    /// rearm().
+    ///
+    /// \param delta the time when the timer expires, relative to now
     void arm(duration delta) {
         return arm(Clock::now() + delta);
     }
+    /// Sets the timer expiration time, with automatic rearming
+    ///
+    /// \param delta the time when the timer expires, relative to now. The timer
+    ///        will also rearm automatically using the same delta time.
     void arm_periodic(duration delta) {
         arm(Clock::now() + delta, {delta});
     }
+    /// Returns whether the timer is armed
+    ///
+    /// \return `true` if the timer is armed and has not expired yet.
     bool armed() const { return _armed; }
+    /// Cancels an armed timer.
+    ///
+    /// If the timer was armed, it is disarmed. If the timer was not
+    /// armed, does nothing.
+    ///
+    /// \return `true` if the timer was armed before the call.
     bool cancel();
+    /// Gets the expiration time of an armed timer.
+    ///
+    /// \return the time at which the timer is scheduled to expire (undefined if the
+    ///       timer is not armed).
     time_point get_timeout() {
         return _expiry;
     }
@@ -101,6 +205,9 @@ public:
 };
 
 extern template class timer<steady_clock_type>;
+
+
+/// @}
 
 }
 
