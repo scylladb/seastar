89894b9adc8d784648c1abe4e0cb6d21e438df8d
Rafael Ávila de Espíndola <espindola@scylladb.com>
2020-06-08 16:36:50 -0700

    byteorder: Mark functions as noexcept
    These functions operate on trivial types and can be marked noexcept.
    
    Signed-off-by: Rafael Ávila de Espíndola <espindola@scylladb.com>
    Message-Id: <20200608233650.152370-1-espindola@scylladb.com>

--- include/seastar/core/byteorder.hh
+++ include/seastar/core/byteorder.hh
@@ -27,56 +27,56 @@
 
 namespace seastar {
 
-inline uint8_t cpu_to_le(uint8_t x) { return x; }
-inline uint8_t le_to_cpu(uint8_t x) { return x; }
-inline uint16_t cpu_to_le(uint16_t x) { return htole16(x); }
-inline uint16_t le_to_cpu(uint16_t x) { return le16toh(x); }
-inline uint32_t cpu_to_le(uint32_t x) { return htole32(x); }
-inline uint32_t le_to_cpu(uint32_t x) { return le32toh(x); }
-inline uint64_t cpu_to_le(uint64_t x) { return htole64(x); }
-inline uint64_t le_to_cpu(uint64_t x) { return le64toh(x); }
+inline uint8_t cpu_to_le(uint8_t x) noexcept { return x; }
+inline uint8_t le_to_cpu(uint8_t x) noexcept { return x; }
+inline uint16_t cpu_to_le(uint16_t x) noexcept { return htole16(x); }
+inline uint16_t le_to_cpu(uint16_t x) noexcept { return le16toh(x); }
+inline uint32_t cpu_to_le(uint32_t x) noexcept { return htole32(x); }
+inline uint32_t le_to_cpu(uint32_t x) noexcept { return le32toh(x); }
+inline uint64_t cpu_to_le(uint64_t x) noexcept { return htole64(x); }
+inline uint64_t le_to_cpu(uint64_t x) noexcept { return le64toh(x); }
 
-inline int8_t cpu_to_le(int8_t x) { return x; }
-inline int8_t le_to_cpu(int8_t x) { return x; }
-inline int16_t cpu_to_le(int16_t x) { return htole16(x); }
-inline int16_t le_to_cpu(int16_t x) { return le16toh(x); }
-inline int32_t cpu_to_le(int32_t x) { return htole32(x); }
-inline int32_t le_to_cpu(int32_t x) { return le32toh(x); }
-inline int64_t cpu_to_le(int64_t x) { return htole64(x); }
-inline int64_t le_to_cpu(int64_t x) { return le64toh(x); }
+inline int8_t cpu_to_le(int8_t x) noexcept { return x; }
+inline int8_t le_to_cpu(int8_t x) noexcept { return x; }
+inline int16_t cpu_to_le(int16_t x) noexcept { return htole16(x); }
+inline int16_t le_to_cpu(int16_t x) noexcept { return le16toh(x); }
+inline int32_t cpu_to_le(int32_t x) noexcept { return htole32(x); }
+inline int32_t le_to_cpu(int32_t x) noexcept { return le32toh(x); }
+inline int64_t cpu_to_le(int64_t x) noexcept { return htole64(x); }
+inline int64_t le_to_cpu(int64_t x) noexcept { return le64toh(x); }
 
-inline uint8_t cpu_to_be(uint8_t x) { return x; }
-inline uint8_t be_to_cpu(uint8_t x) { return x; }
-inline uint16_t cpu_to_be(uint16_t x) { return htobe16(x); }
-inline uint16_t be_to_cpu(uint16_t x) { return be16toh(x); }
-inline uint32_t cpu_to_be(uint32_t x) { return htobe32(x); }
-inline uint32_t be_to_cpu(uint32_t x) { return be32toh(x); }
-inline uint64_t cpu_to_be(uint64_t x) { return htobe64(x); }
-inline uint64_t be_to_cpu(uint64_t x) { return be64toh(x); }
+inline uint8_t cpu_to_be(uint8_t x) noexcept { return x; }
+inline uint8_t be_to_cpu(uint8_t x) noexcept { return x; }
+inline uint16_t cpu_to_be(uint16_t x) noexcept { return htobe16(x); }
+inline uint16_t be_to_cpu(uint16_t x) noexcept { return be16toh(x); }
+inline uint32_t cpu_to_be(uint32_t x) noexcept { return htobe32(x); }
+inline uint32_t be_to_cpu(uint32_t x) noexcept { return be32toh(x); }
+inline uint64_t cpu_to_be(uint64_t x) noexcept { return htobe64(x); }
+inline uint64_t be_to_cpu(uint64_t x) noexcept { return be64toh(x); }
 
-inline int8_t cpu_to_be(int8_t x) { return x; }
-inline int8_t be_to_cpu(int8_t x) { return x; }
-inline int16_t cpu_to_be(int16_t x) { return htobe16(x); }
-inline int16_t be_to_cpu(int16_t x) { return be16toh(x); }
-inline int32_t cpu_to_be(int32_t x) { return htobe32(x); }
-inline int32_t be_to_cpu(int32_t x) { return be32toh(x); }
-inline int64_t cpu_to_be(int64_t x) { return htobe64(x); }
-inline int64_t be_to_cpu(int64_t x) { return be64toh(x); }
+inline int8_t cpu_to_be(int8_t x) noexcept { return x; }
+inline int8_t be_to_cpu(int8_t x) noexcept { return x; }
+inline int16_t cpu_to_be(int16_t x) noexcept { return htobe16(x); }
+inline int16_t be_to_cpu(int16_t x) noexcept { return be16toh(x); }
+inline int32_t cpu_to_be(int32_t x) noexcept { return htobe32(x); }
+inline int32_t be_to_cpu(int32_t x) noexcept { return be32toh(x); }
+inline int64_t cpu_to_be(int64_t x) noexcept { return htobe64(x); }
+inline int64_t be_to_cpu(int64_t x) noexcept { return be64toh(x); }
 
 template <typename T>
-inline T cpu_to_le(const unaligned<T>& v) {
+inline T cpu_to_le(const unaligned<T>& v) noexcept {
     return cpu_to_le(T(v));
 }
 
 template <typename T>
-inline T le_to_cpu(const unaligned<T>& v) {
+inline T le_to_cpu(const unaligned<T>& v) noexcept {
     return le_to_cpu(T(v));
 }
 
 template <typename T>
 inline
 T
-read_le(const char* p) {
+read_le(const char* p) noexcept {
     T datum;
     std::copy_n(p, sizeof(T), reinterpret_cast<char*>(&datum));
     return le_to_cpu(datum);
@@ -85,7 +85,7 @@ read_le(const char* p) {
 template <typename T>
 inline
 void
-write_le(char* p, T datum) {
+write_le(char* p, T datum) noexcept {
     datum = cpu_to_le(datum);
     std::copy_n(reinterpret_cast<const char*>(&datum), sizeof(T), p);
 }
@@ -93,7 +93,7 @@ write_le(char* p, T datum) {
 template <typename T>
 inline
 T
-read_be(const char* p) {
+read_be(const char* p) noexcept {
     T datum;
     std::copy_n(p, sizeof(T), reinterpret_cast<char*>(&datum));
     return be_to_cpu(datum);
@@ -102,7 +102,7 @@ read_be(const char* p) {
 template <typename T>
 inline
 void
-write_be(char* p, T datum) {
+write_be(char* p, T datum) noexcept {
     datum = cpu_to_be(datum);
     std::copy_n(reinterpret_cast<const char*>(&datum), sizeof(T), p);
 }
@@ -110,7 +110,7 @@ write_be(char* p, T datum) {
 template <typename T>
 inline
 T
-consume_be(const char*& p) {
+consume_be(const char*& p) noexcept {
     auto ret = read_be<T>(p);
     p += sizeof(T);
     return ret;
@@ -119,7 +119,7 @@ consume_be(const char*& p) {
 template <typename T>
 inline
 void
-produce_be(char*& p, T datum) {
+produce_be(char*& p, T datum) noexcept {
     write_be<T>(p, datum);
     p += sizeof(T);
 }
