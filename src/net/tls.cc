f7a5c3530c8de958d7cbc6f4814b95f010d2cb7d
Radoslaw Zarzynski <rzarzyns@redhat.com>
2020-08-10 22:38:41 +0200

    net/tls: fix compilation guards around sec_param().
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>
    Message-Id: <1597091921-26171-2-git-send-email-rzarzyns@redhat.com>

--- src/net/tls.cc
+++ src/net/tls.cc
@@ -203,12 +203,14 @@ public:
     operator gnutls_dh_params_t() const {
         return _params.get();
     }
+#if GNUTLS_VERSION_NUMBER >= 0x030506
     std::optional<gnutls_sec_param_t> sec_param() const {
         return _sec_param;
     }
+#endif
 private:
     dh_ptr _params;
-#if GNUTLS_VERSION_NUMBER >= 0x030600
+#if GNUTLS_VERSION_NUMBER >= 0x030506
     std::optional<gnutls_sec_param_t> _sec_param;
 #endif
 };
