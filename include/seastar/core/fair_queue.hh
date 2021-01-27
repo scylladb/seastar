scylladb/seastar

commit 98366fd5142d7c64c81e05b3319b65fe7b8b0e15

fair_queue: Improve requests preemption while in pending state
The nowadays fair queue enters pending state when it tries to submit
a request and the associated group's remaining capacity is not enough
for the request's ticket. When in this state the queue waits for the
group's head rover to move forward ahead of a certain value, which
should denote that there's a room for the request in question. While
the queue is waiting one more request from another priority class may
pop up and get dispatched, thus trying to "preempt" the original
request.

To handle this preemption and to dispatch the new request, the queue
should somehow re-order the group accounting. Currently it's solved
by de-accounting the pending request from the group and accounting
the new one into it. This keeps the counters intact, but effectively
puts the new request into the group's queue tail and penalizes the
shard's queue.

The better solution is to take into account the fact that the
original request actually "charged" the group for its capacity and
the new request may try to reuse it, or some part of it if it's
smaller. Or, if it's larger, account for the needed delta and to
_continue_ waiting in the "same place" of the group queue. In the
worst case -- do both, as request tickets are 2-dimentional.

While accounting for fractions of 2d requests tickets is tricky, it's
not really needed -- once (if) the new request will be dispatched
right at once, the old request will be attempted to get dispatched
instantly within the same poll -- right on the next dispatching loop
iteration. So to keep the tickets computation simpler, if the new
request _successfully_ tries to preemt the pending one, both requests
are accounted into the group right at once, so at the next loop
iteration the queue happens in the same state as it would be if the
fractions accounting took place.

As a side effect this simplifies the code that accounts a ticket into
a group (tail-rover moving method).

include/seastar/core/fair_queue.hh
@@ -188,7 +188,7 @@ public:
     explicit fair_group(config cfg) noexcept;
 
     fair_queue_ticket maximum_capacity() const noexcept { return _maximum_capacity; }
-    std::pair<fair_queue_ticket, fair_group_rover> grab_capacity(fair_queue_ticket cap) noexcept;
+    fair_group_rover grab_capacity(fair_queue_ticket cap) noexcept;
     void release_capacity(fair_queue_ticket cap) noexcept;
 
     fair_group_rover head() const noexcept {
@@ -252,18 +252,18 @@ private:
      * When the shared capacity os over the local queue delays
      * further dispatching untill better times
      *
-     * \head  -- the value group head rover should pass by
-     * \cap   -- the capacity that's waited for
+     * \orig_tail  -- the value group tail rover had when it happened
+     * \cap   -- the capacity that's accounted on the group
      *
      * The last field is needed to "rearm" the wait in case
      * queue decides that it wants to dispatch another capacity
      * in the middle of the waiting
      */
     struct pending {
-        fair_group_rover head;
+        fair_group_rover orig_tail;
         fair_queue_ticket cap;
 
-        pending(fair_group_rover h, fair_queue_ticket c) noexcept : head(h), cap(c) {}
+        pending(fair_group_rover t, fair_queue_ticket c) noexcept : orig_tail(t), cap(c) {}
     };
 
     std::optional<pending> _pending;
@@ -341,7 +341,8 @@ public:
              * which's sub-optimal. The expectation is that we think disk
              * works faster, than it really does.
              */
-            fair_queue_ticket over = _pending->head.maybe_ahead_of(_group.head());
+            fair_group_rover pending_head = _pending->orig_tail + _pending->cap;
+            fair_queue_ticket over = pending_head.maybe_ahead_of(_group.head());
             return std::chrono::steady_clock::now() + duration(over);
         }
 

src/core/fair_queue.cc
@@ -110,12 +110,10 @@ fair_group::fair_group(config cfg) noexcept
     seastar_logger.debug("Created fair group, capacity {}:{}", cfg.max_req_count, cfg.max_bytes_count);
 }
 
-std::pair<fair_queue_ticket, fair_group_rover> fair_group::grab_capacity(fair_queue_ticket cap) noexcept {
+fair_group_rover fair_group::grab_capacity(fair_queue_ticket cap) noexcept {
     fair_group_rover cur = _capacity_tail.load(std::memory_order_relaxed);
     while (!_capacity_tail.compare_exchange_weak(cur, cur + cap)) ;
-    fair_group_rover head = _capacity_head.load(std::memory_order_relaxed);
-    fair_group_rover want_head = cur + cap;
-    return std::make_pair(want_head.maybe_ahead_of(head), want_head);
+    return cur;
 }
 
 void fair_group::release_capacity(fair_queue_ticket cap) noexcept {
@@ -168,33 +166,23 @@ std::chrono::microseconds fair_queue_ticket::duration_at_pace(float weight_pace,
 }
 
 bool fair_queue::grab_pending_capacity(fair_queue_ticket cap) noexcept {
-    if (cap == _pending->cap) {
-        if (_pending->head.maybe_ahead_of(_group.head())) {
-            return false;
-        }
-    } else {
-        /*
-         * A new request with different capacity preempted
-         * the previous. The simplest solution here is to
-         * forget about the previous one and start the new
-         * pending wait, but this is sub-optimal wrt latency.
-         *
-         * The better solution should check if the new cap
-         * is bigger or smaller than the preious value and
-         * (de)account the difference into group rovers, thus
-         * effectively consuming the fraction of the capacity
-         * that might had been released by that time.
-         */
-        _group.release_capacity(_pending->cap);
-
-        auto over = _group.grab_capacity(cap);
-        if (over.first) {
-            _pending.emplace(over.second, cap);
-            return false;
-        }
+    fair_group_rover pending_head = _pending->orig_tail + cap;
+    if (pending_head.maybe_ahead_of(_group.head())) {
+        return false;
     }
 
-    _pending.reset();
+    if (cap == _pending->cap) {
+        _pending.reset();
+    } else {
+        /*
+         * This branch is called when the fair queue decides to
+         * submit not the same request that entered it into the
+         * pending state and this new request crawls through the
+         * expected head value.
+         */
+        _pending->orig_tail = _group.grab_capacity(cap);
+    }
+
     return true;
 }
 
@@ -203,9 +191,9 @@ bool fair_queue::grab_capacity(fair_queue_ticket cap) noexcept {
         return grab_pending_capacity(cap);
     }
 
-    auto over = _group.grab_capacity(cap);
-    if (over.first) {
-        _pending.emplace(over.second, cap);
+    fair_group_rover orig_tail = _group.grab_capacity(cap);
+    if ((orig_tail + cap).maybe_ahead_of(_group.head())) {
+        _pending.emplace(orig_tail, cap);
         return false;
     }
 

commit 52d41277a8a58ab68ebc3d629985bfe7acefe7d9

fair_queue: Preempted requests got re-queued too far
When a request is preempted, it's put at the group's queue
tail by setting the queue to wait for the group's head rover
to become larger than the group's current tail. In turn,
when the preempting request completes it forwards the group
head rover by _its_ size.

If the preempting request is smaller than the preempted one,
and there are more than one queue doing this, the resulting
head value may not become large enough to pass by the expected
thresholds and the whole group may get stuck.

The problem was introduced by 98366fd5 and it reveals itself
when queue is fed with over-sized requests and they all are
preempted with tiny ones.

Before the mentioned patch this didn't happen because preempted
request pushed the groups head rover forward as if they had
completed, thus letting the queues dispatch their requests (not
in order, but still).

The fix is in re-queueing preempted requests right after the
preempted one.

tests: unit(dev), dtest.resharding(dev)

Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>
Message-Id: <20210126144325.14086-1-xemul@scylladb.com>

include/seastar/core/fair_queue.hh
@@ -112,6 +112,7 @@ public:
      */
     fair_queue_ticket maybe_ahead_of(const fair_group_rover& other) const noexcept;
     fair_group_rover operator+(fair_queue_ticket t) const noexcept;
+    fair_group_rover& operator+=(fair_queue_ticket t) noexcept;
 
     friend std::ostream& operator<<(std::ostream& os, fair_group_rover r);
 };

src/core/fair_queue.cc
@@ -99,6 +99,12 @@ fair_group_rover fair_group_rover::operator+(fair_queue_ticket t) const noexcept
     return fair_group_rover(_weight + t._weight, _size + t._size);
 }
 
+fair_group_rover& fair_group_rover::operator+=(fair_queue_ticket t) noexcept {
+    _weight += t._weight;
+    _size += t._size;
+    return *this;
+}
+
 std::ostream& operator<<(std::ostream& os, fair_group_rover r) {
     return os << r._weight << ":" << r._size;
 }
@@ -183,7 +189,8 @@ bool fair_queue::grab_pending_capacity(fair_queue_ticket cap) noexcept {
          * pending state and this new request crawls through the
          * expected head value.
          */
-        _pending->orig_tail = _group.grab_capacity(cap);
+        _group.grab_capacity(cap);
+        _pending->orig_tail += cap;
     }
 
     return true;