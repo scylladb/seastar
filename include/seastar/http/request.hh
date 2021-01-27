a639b10cb5b5b448ae8c879d1698785edd5778db
Wojciech Mitros <wojciech.mitros@scylladb.com>
2021-01-28 00:10:35 +0100

    http: add reading/skipping all bytes in an input_stream
    If an application knows that it will need all bytes from a buffer,
    it can now use short_stream_reader methods:
    the read_entire_stream() method saves all buffers from a stream to
    a std::vector.
    Some applications may know that they're going to receive small amounts
    of data in a stream. For them it may be more convenient to work on a
    single string instead of a vector. The read_entire_stream_contiguous()
    method provides such a string.
    An app (e.g. an http handler) may need to exhaust an entire input_stream
    (e.g. to enable processing the next http request on the same connection).
    This can now be achieved by using the skip_entire_stream() method.
    
    Refs: scylladb/scylla#7213
    
    Signed-off-by: Wojciech Mitros <wojciech.mitros@scylladb.com>

--- include/seastar/http/request.hh
+++ include/seastar/http/request.hh
@@ -35,6 +35,7 @@
 #include <vector>
 #include <strings.h>
 #include <seastar/http/common.hh>
+#include <seastar/core/iostream.hh>
 
 namespace seastar {
 
@@ -128,6 +129,55 @@ struct request {
     }
 
 };
+
+/**
+ * Helper class for reading entire streams, for example streams containing content of a HTTP request
+ */
+class short_stream_reader {
+public:
+    /// Returns all bytes from the stream until eof, accessible in chunks
+    static future<std::vector<temporary_buffer<char>>> read_entire_stream(input_stream<char>& inp) {
+        using tmp_buf = temporary_buffer<char>;
+        using consumption_result_type = consumption_result<char>;
+        return do_with(std::vector<tmp_buf>(), [&inp] (std::vector<tmp_buf>& bufs) {
+            return inp.consume([&bufs] (tmp_buf buf) {
+                if (buf.empty()) {
+                    return make_ready_future<consumption_result_type>(stop_consuming(std::move(buf)));
+                }
+                bufs.push_back(std::move(buf));
+                return make_ready_future<consumption_result_type>(continue_consuming());
+            }).then([&bufs] {
+                return std::move(bufs);
+            });
+        });
+    }
+
+    /// Returns all bytes from the stream until eof as a single buffer, use only on short streams
+    static future<sstring> read_entire_stream_contiguous(input_stream<char>& inp) {
+        return read_entire_stream(inp).then([&inp] (std::vector<temporary_buffer<char>> bufs) {
+            size_t total_size = 0;
+            for (auto&& buf : bufs) {
+                total_size += buf.size();
+            }
+            sstring ret(sstring::initialized_later(), total_size);
+            size_t pos = 0;
+            for (auto&& buf : bufs) {
+                std::copy(buf.begin(), buf.end(), ret.data() + pos);
+                pos += buf.size();
+            }
+            return ret;
+        });
+    };
+
+    /// Ignores all bytes until eof
+    static future<> skip_entire_stream(input_stream<char>& inp) {
+        return inp.consume([] (temporary_buffer<char> tmp) {
+            return tmp.empty() ? make_ready_future<consumption_result<char>>(stop_consuming(temporary_buffer<char>()))
+                            : make_ready_future<consumption_result<char>>(continue_consuming());
+        });
+    }
+};
+
 
 } // namespace httpd
 
