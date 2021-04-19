b4d40ecdcf8c87cb3bc7b52c275ba9652fe07b24
Pavel Emelyanov <xemul@scylladb.com>
2021-04-19 09:15:55 +0000

    io_queue: Sanitize local variables in config creation
    This just cleans stuff after previous patch, not functional changes.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- src/core/reactor.cc
+++ src/core/reactor.cc
@@ -3658,17 +3658,15 @@ public:
         seastar_logger.debug("generate_group_config dev_id: {}", devid);
         const mountpoint_params& p = _mountpoints.at(devid);
         struct io_group::config cfg;
-        uint64_t max_bandwidth = p.read_bytes_rate;
-        uint64_t max_iops = p.read_req_rate;
 
         cfg.disk_req_write_to_read_multiplier = io_queue::read_request_base_count;
 
         if (!_capacity) {
-            if (max_bandwidth != std::numeric_limits<uint64_t>::max()) {
-                cfg.max_bytes_count = io_queue::read_request_base_count * per_io_group(max_bandwidth * latency_goal().count(), nr_groups);
+            if (p.read_bytes_rate != std::numeric_limits<uint64_t>::max()) {
+                cfg.max_bytes_count = io_queue::read_request_base_count * per_io_group(p.read_bytes_rate * latency_goal().count(), nr_groups);
             }
-            if (max_iops != std::numeric_limits<uint64_t>::max()) {
-                cfg.max_req_count = io_queue::read_request_base_count * per_io_group(max_iops * latency_goal().count(), nr_groups);
+            if (p.read_req_rate != std::numeric_limits<uint64_t>::max()) {
+                cfg.max_req_count = io_queue::read_request_base_count * per_io_group(p.read_req_rate * latency_goal().count(), nr_groups);
                 cfg.disk_req_write_to_read_multiplier = (io_queue::read_request_base_count * p.read_req_rate) / p.write_req_rate;
             }
         } else {
@@ -3684,21 +3682,19 @@ public:
         seastar_logger.debug("generate_config dev_id: {}", devid);
         const mountpoint_params& p = _mountpoints.at(devid);
         struct io_queue::config cfg;
-        uint64_t max_bandwidth = p.read_bytes_rate;
-        uint64_t max_iops = p.read_req_rate;
 
         cfg.devid = devid;
         cfg.disk_bytes_write_to_read_multiplier = io_queue::read_request_base_count;
         cfg.disk_req_write_to_read_multiplier = io_queue::read_request_base_count;
 
         if (!_capacity) {
-            if (max_bandwidth != std::numeric_limits<uint64_t>::max()) {
+            if (p.read_bytes_rate != std::numeric_limits<uint64_t>::max()) {
                 cfg.disk_bytes_write_to_read_multiplier = (io_queue::read_request_base_count * p.read_bytes_rate) / p.write_bytes_rate;
-                cfg.disk_us_per_byte = 1000000. / max_bandwidth;
+                cfg.disk_us_per_byte = 1000000. / p.read_bytes_rate;
             }
-            if (max_iops != std::numeric_limits<uint64_t>::max()) {
+            if (p.read_req_rate != std::numeric_limits<uint64_t>::max()) {
                 cfg.disk_req_write_to_read_multiplier = (io_queue::read_request_base_count * p.read_req_rate) / p.write_req_rate;
-                cfg.disk_us_per_request = 1000000. / max_iops;
+                cfg.disk_us_per_request = 1000000. / p.read_req_rate;
             }
             cfg.mountpoint = p.mountpoint;
         } else {
