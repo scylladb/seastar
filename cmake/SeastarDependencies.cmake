4b01fc5ec6fd261010dd76ea22e9d23e74e0316d
Kefu Chai <tchaikov@gmail.com>
2021-10-03 01:46:21 +0800

    cmake: check for DPDK only if enabled
    so the configure.py is less chatty. and it's less confusing if
    SPDK is enabled when configuration Seastar. because SPDK comes
    with a bundled DPDK, whose version is not necessarily identical to
    the one in the top-level dpdk submodule. so the output of cmake
    when checking DPDK does not imply the existence of the DPDK used
    by SPDK. since the DPDK and SPDK supports cannot be enabled at
    the same time, it'd be less confusing if DPDK is checked only if
    it is enabled.
    
    Signed-off-by: Kefu Chai <tchaikov@gmail.com>

--- cmake/SeastarDependencies.cmake
+++ cmake/SeastarDependencies.cmake
@@ -52,7 +52,6 @@ macro (seastar_find_dependencies)
     Boost
     c-ares
     cryptopp
-    dpdk # No version information published.
     fmt
     lz4
     # Private and private/public dependencies.
@@ -66,6 +65,11 @@ macro (seastar_find_dependencies)
     numactl # No version information published.
     rt
     yaml-cpp)
+
+  if (Seastar_DPDK)
+    list (APPEND _seastar_all_dependencies
+      dpdk)
+  endif ()
 
   # Arguments to `find_package` for each 3rd-party dependency.
   # Note that the version specification is a "minimal" version requirement.
