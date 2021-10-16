a9cbb643db4c5b9829570ad2266560eda4eb41c0
Kefu Chai <tchaikov@gmail.com>
2021-10-16 13:34:00 +0800

    reactor: call memory::configure() before initialize dpdk
    dpdk calls realloc() when registering telemetry commands. this happens
    in seastar::smp::configure(), but the original memory buffer being reallocated
    could be allocated before the seastar::smp::configure() call. in other
    words, we could realloc() a memory buffer allocated previously in a non-reactor
    thread in a reactor thread, given that is_reactor_thread is set by
    memory::configure().
    
    to prevent this from happening, in this change, the dpdk::eal::init() is
    moved before memory::configure(), so DPDK calls realloc() before
    memory::configure() marks the current thread a reactor thread.
    
    this should address abort() failure in realloc() when running seastar
    application with --dpdk-pmd option built with non-debug settings.
    
    Signed-off-by: Kefu Chai <tchaikov@gmail.com>
    Message-Id: <20211016053400.756909-1-tchaikov@gmail.com>

--- src/core/reactor.cc
+++ src/core/reactor.cc
@@ -4046,6 +4046,17 @@ void smp::configure(boost::program_options::variables_map configuration, reactor
     if (thread_affinity) {
         smp::pin(allocations[0].cpu_id);
     }
+
+#ifdef SEASTAR_HAVE_DPDK
+    if (_using_dpdk) {
+        dpdk::eal::cpuset cpus;
+        for (auto&& a : allocations) {
+            cpus[a.cpu_id] = true;
+        }
+        dpdk::eal::init(cpus, configuration);
+    }
+#endif
+
     memory::configure(allocations[0].mem, mbind, hugepages_path);
 
     if (configuration.count("abort-on-seastar-bad-alloc")) {
@@ -4062,16 +4073,6 @@ void smp::configure(boost::program_options::variables_map configuration, reactor
     if (heapprof_enabled) {
         memory::set_heap_profiling_enabled(heapprof_enabled);
     }
-
-#ifdef SEASTAR_HAVE_DPDK
-    if (_using_dpdk) {
-        dpdk::eal::cpuset cpus;
-        for (auto&& a : allocations) {
-            cpus[a.cpu_id] = true;
-        }
-        dpdk::eal::init(cpus, configuration);
-    }
-#endif
 
     // Better to put it into the smp class, but at smp construction time
     // correct smp::count is not known.
