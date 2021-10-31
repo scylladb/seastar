a189cdc45ddb1b79e1fba52763618856d45520d4
Avi Kivity <avi@scylladb.com>
2021-10-31 22:46:37 +0200

    print: deprecate print() family
    The print()/sprint() functions are outdated as they use C-style format
    strings rather than C++20 std::format() style format strings. In any
    case Seastar should not be in the business of string formatting.
    
    Deprecate the functions and point the user to better alternatives.
    Message-Id: <20211031204637.4177124-2-avi@scylladb.com>

--- include/seastar/core/print.hh
+++ include/seastar/core/print.hh
@@ -49,6 +49,7 @@ operator<<(std::ostream&& os, const void* ptr) {
 namespace seastar {
 
 template <typename... A>
+[[deprecated("use std::format_to() or fmt::print()")]]
 std::ostream&
 fprint(std::ostream& os, const char* fmt, A&&... a) {
     ::fmt::fprintf(os, fmt, std::forward<A>(a)...);
@@ -56,12 +57,14 @@ fprint(std::ostream& os, const char* fmt, A&&... a) {
 }
 
 template <typename... A>
+[[deprecated("use std::format_to() or fmt::print()")]]
 void
 print(const char* fmt, A&&... a) {
     ::fmt::printf(fmt, std::forward<A>(a)...);
 }
 
 template <typename... A>
+[[deprecated("use std::format() or fmt::format()")]]
 std::string
 sprint(const char* fmt, A&&... a) {
     std::ostringstream os;
@@ -70,6 +73,7 @@ sprint(const char* fmt, A&&... a) {
 }
 
 template <typename... A>
+[[deprecated("use std::format() or fmt::format()")]]
 std::string
 sprint(const sstring& fmt, A&&... a) {
     std::ostringstream os;
@@ -143,6 +147,7 @@ format(const char* fmt, A&&... a) {
 
 // temporary, use fmt::print() instead
 template <typename... A>
+[[deprecated("use std::format() or fmt::print()")]]
 std::ostream&
 fmt_print(std::ostream& os, const char* format, A&&... a) {
     fmt::print(os, format, std::forward<A>(a)...);
