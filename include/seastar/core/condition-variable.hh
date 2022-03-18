scylladb/seastar

commit e74a93c3934b2c8d360468bcdc03e2651dc1020d

condition_variable: Remove semaphore usage, keep internal wait queue
Uses boost::intrusive::list for a simple wait queue, with individual
timers for any timed waiters.
Wait objects are heap-allocated to ensure we only generate a single
continuation waiter (promise+future), no finally etc. The waiters
are prepared for coroutine enhancements.

v4:
* Make signal bool instead of semaphore counter
v5:
* Add missing "override"
Message-Id: <20220307114157.28288-2-calle@scylladb.com>

include/seastar/core/condition-variable.hh
@@ -21,7 +21,9 @@
 
 #pragma once
 
-#include <seastar/core/semaphore.hh>
+#include <boost/intrusive/list.hpp>
+
+#include <seastar/core/timer.hh>
 #include <seastar/core/loop.hh>
 
 namespace seastar {
@@ -59,19 +61,50 @@ public:
 /// acting as a cancellation point.
 
 class condition_variable {
-    using duration = semaphore::duration;
-    using clock = semaphore::clock;
-    using time_point = semaphore::time_point;
-    struct condition_variable_exception_factory {
-        static condition_variable_timed_out timeout() noexcept;
-        static broken_condition_variable broken() noexcept;
+private:
+    using clock = typename timer<>::clock;
+    using duration = typename clock::duration;
+    using time_point = typename clock::time_point;
+
+    // the base for queue waiters. looks complicated, but this is
+    // to make it transparent once we add non-promise based nodes
+    struct waiter : public boost::intrusive::list_base_hook<boost::intrusive::link_mode<boost::intrusive::auto_unlink>> {
+        virtual ~waiter() = default;
+        void timeout() noexcept;
+
+        virtual void signal() noexcept = 0;
+        virtual void set_exception(std::exception_ptr) noexcept = 0;
     };
-    basic_semaphore<condition_variable_exception_factory> _sem;
+
+    struct promise_waiter : public waiter, public promise<> {
+        void signal() noexcept override {
+            set_value();
+            // note: we self-delete in either case we are woken
+            // up. See usage below: only the resulting future
+            // state is required once we've left the wait queue
+            delete this;
+        }
+        void set_exception(std::exception_ptr ep) noexcept override {
+            promise<>::set_exception(std::move(ep));
+            // see comment above
+            delete this;
+        }
+    };
+
+    boost::intrusive::list<waiter, boost::intrusive::constant_time_size<false>> _waiters;
+    std::exception_ptr _ex; //"broken" exception
+    bool _signalled = false; // set to true if signalled while no waiters
+
+    void add_waiter(waiter&) noexcept;
+    void timeout(waiter&) noexcept;
+    bool wakeup_first() noexcept;
+    bool check_and_consume_signal() noexcept;
 public:
     /// Constructs a condition_variable object.
     /// Initialzie the semaphore with a default value of 0 to enusre
     /// the first call to wait() before signal() won't be waken up immediately.
-    condition_variable() noexcept : _sem(0) {}
+    condition_variable() noexcept = default;
+    ~condition_variable();
 
     /// Waits until condition variable is signaled, may wake up without condition been met
     ///
@@ -79,7 +112,13 @@ public:
     ///         If the condition variable was \ref broken() will return \ref broken_condition_variable
     ///         exception.
     future<> wait() noexcept {
-        return _sem.wait();
+        if (check_and_consume_signal()) {
+            return make_ready_future();
+        }
+        auto* w = new promise_waiter;
+        auto f = w->get_future();
+        add_waiter(*w);
+        return f;
     }
 
     /// Waits until condition variable is signaled or timeout is reached
@@ -90,7 +129,18 @@ public:
     ///         If the condition variable was \ref broken() will return \ref broken_condition_variable
     ///         exception. If timepoint is reached will return \ref condition_variable_timed_out exception.
     future<> wait(time_point timeout) noexcept {
-        return _sem.wait(timeout);
+        if (check_and_consume_signal()) {
+            return make_ready_future();
+        }
+        struct timeout_waiter : public promise_waiter, public timer<> {};
+        // use a unique_ptr here, because bind/set_callback can throw
+        auto w = std::make_unique<timeout_waiter>();
+        auto f = w->get_future();
+
+        w->set_callback(std::bind(&waiter::timeout, w.get()));
+        w->arm(timeout);
+        add_waiter(*w.release());
+        return f;
     }
 
     /// Waits until condition variable is signaled or timeout is reached
@@ -101,7 +151,7 @@ public:
     ///         If the condition variable was \ref broken() will return \ref broken_condition_variable
     ///         exception. If timepoint is passed will return \ref condition_variable_timed_out exception.
     future<> wait(duration timeout) noexcept {
-        return _sem.wait(timeout);
+        return wait(timer<>::clock::now() + timeout);
     }
 
     /// Waits until condition variable is notified and pred() == true, otherwise
@@ -131,7 +181,7 @@ public:
     template<typename Pred>
     SEASTAR_CONCEPT( requires seastar::InvokeReturns<Pred, bool> )
     future<> wait(time_point timeout, Pred&& pred) noexcept {
-        return do_until(std::forward<Pred>(pred), [this, timeout] () mutable {
+        return do_until(std::forward<Pred>(pred), [this, timeout] {
             return wait(timeout);
         });
     }
@@ -148,25 +198,20 @@ public:
     template<typename Pred>
     SEASTAR_CONCEPT( requires seastar::InvokeReturns<Pred, bool> )
     future<> wait(duration timeout, Pred&& pred) noexcept {
-        return wait(clock::now() + timeout, std::forward<Pred>(pred));
+        return wait(timer<>::clock::now() + timeout, std::forward<Pred>(pred));
     }
     /// Notify variable and wake up a waiter if there is one
-    void signal() noexcept {
-        if (_sem.waiters()) {
-            _sem.signal();
-        }
-    }
+    void signal() noexcept;
+
     /// Notify variable and wake up all waiter
-    void broadcast() noexcept {
-        _sem.signal(_sem.waiters());
-    }
+    void broadcast() noexcept;
 
     /// Signal to waiters that an error occurred.  \ref wait() will see
     /// an exceptional future<> containing the provided exception parameter.
     /// The future is made available immediately.
-    void broken() noexcept {
-        _sem.broken();
-    }
+    void broken() noexcept;
+
+    void broken(std::exception_ptr) noexcept;
 };
 
 /// @}

src/core/condition-variable.cc
@@ -31,14 +31,64 @@ const char* condition_variable_timed_out::what() const noexcept {
     return "Condition variable timed out";
 }
 
-condition_variable_timed_out condition_variable::condition_variable_exception_factory::timeout() noexcept {
-    static_assert(std::is_nothrow_default_constructible_v<condition_variable_timed_out>);
-    return condition_variable_timed_out();
+condition_variable::~condition_variable() {
+    broken();
 }
 
-broken_condition_variable condition_variable::condition_variable_exception_factory::broken() noexcept {
-    static_assert(std::is_nothrow_default_constructible_v<broken_condition_variable>);
-    return broken_condition_variable();
+void condition_variable::add_waiter(waiter& w) noexcept {
+    assert(!_signalled); // should not have snuck between
+    if (_ex) {
+        w.set_exception(_ex);
+        return;
+    }
+    _waiters.push_back(w);
+}
+
+void condition_variable::waiter::timeout() noexcept {
+    this->unlink();
+    this->set_exception(std::make_exception_ptr(condition_variable_timed_out()));
+}
+
+bool condition_variable::wakeup_first() noexcept {
+    if (_waiters.empty()) {
+        return false;
+    }
+    auto& w = _waiters.front();
+    _waiters.pop_front();
+    if (_ex) {
+        w.set_exception(_ex);
+    } else {
+        w.signal();
+    }
+    return true;
+}
+
+bool condition_variable::check_and_consume_signal() noexcept {
+    return std::exchange(_signalled, false);
+}
+
+void condition_variable::signal() noexcept {
+    if (!wakeup_first()) {
+        _signalled = true;
+    }
+}
+
+/// Notify variable and wake up all waiter
+void condition_variable::broadcast() noexcept {
+    while (wakeup_first())
+    {}
+}
+
+/// Signal to waiters that an error occurred.  \ref wait() will see
+/// an exceptional future<> containing the provided exception parameter.
+/// The future is made available immediately.
+void condition_variable::broken() noexcept {
+    broken(std::make_exception_ptr(broken_condition_variable()));
+}
+
+void condition_variable::broken(std::exception_ptr ep) noexcept {
+    _ex = ep;
+    broadcast();
 }
 
 } // namespace seastar

commit 79dfb83cc93f147eb34f4ca4aa5519a3a55e309d

condition_variable: restore move-constructability.
This patch restores the move-constructability of
the `seastar::condition_variable` which is crucial
when e.g. a user of it needs to be passed to
`seastar::do_with()`. Lost of this ability was
the root cause of the following FTBFS in crimson-osd
(moving ctor has been manually added to `Throttle`
to illustrate the issue in a more concise way):

```
In file included from ../src/crimson/net/SocketConnection.h:20,
                 from ../src/msg/Message.h:39,
                 from ../src/auth/cephx/CephxSessionHandler.cc:23:
../src/crimson/common/throttle.h: In constructor ‘crimson::common::Throttle::Throttle(crimson::common::Throttle&&)’:
../src/crimson/common/throttle.h:26:49: error: use of deleted function ‘seastar::condition_variable::condition_variable(const seastar::condition_variable&)’
   26 |     : on_free_slots(std::move(rhs.on_free_slots)) {
      |                                                 ^
```

The problem has been introduced in e74a93c3934b2c8d360468bcdc03e2651dc1020d.

Signed-off-by: Radosław Zarzyński <rzarzyns@redhat.com>
Message-Id: <20220318132552.1087723-1-rzarzyns@redhat.com>

include/seastar/core/condition-variable.hh
@@ -155,6 +155,7 @@ public:
     /// Initialzie the semaphore with a default value of 0 to enusre
     /// the first call to wait() before signal() won't be waken up immediately.
     condition_variable() noexcept = default;
+    condition_variable(condition_variable&& rhs) noexcept = default;
     ~condition_variable();
 
     /// Waits until condition variable is signaled, may wake up without condition been met