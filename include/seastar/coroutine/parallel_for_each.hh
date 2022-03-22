scylladb/seastar

commit 710a1bfdc02202fe9e59df8ea31de5b82b893fb4

future: set_callback: consume future input
As it is unusable after set_callback returns.

Fixes #1027

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>
Message-Id: <20220317153256.76658-1-bhalevy@scylladb.com>

include/seastar/coroutine/parallel_for_each.hh
@@ -85,13 +85,13 @@ class [[nodiscard("must co_await an parallel_for_each() object")]] parallel_for_
     }
 
     void set_callback() noexcept {
-        auto fut = std::move(_futures.back());
+        auto&& fut = std::move(_futures.back());
         _futures.pop_back();
         // To reuse `this` as continuation_base<>
         // we must reset _state, to allow setting
         // it again.
         this->_state = {};
-        seastar::internal::set_callback(fut, reinterpret_cast<continuation_base<>*>(this));
+        seastar::internal::set_callback(std::move(fut), reinterpret_cast<continuation_base<>*>(this));
     }
 
     void resume_or_set_callback() noexcept {

include/seastar/core/do_with.hh
@@ -98,7 +98,7 @@ do_with_impl(T1&& rv1, T2&& rv2, More&&... more) {
         return fut;
     }
     auto ret = task->get_future();
-    internal::set_callback(fut, task.release());
+    internal::set_callback(std::move(fut), task.release());
     return ret;
 }
 }

include/seastar/core/future.hh
@@ -799,7 +799,7 @@ template <typename... T>
 future<T...> make_exception_future(future_state_base&& state) noexcept;
 
 template <typename... T, typename U>
-void set_callback(future<T...>& fut, U* callback) noexcept;
+void set_callback(future<T...>&& fut, U* callback) noexcept;
 
 class future_base;
 
@@ -1907,7 +1907,7 @@ private:
     template <typename... U>
     friend future<U...> current_exception_as_future() noexcept;
     template <typename... U, typename V>
-    friend void internal::set_callback(future<U...>&, V*) noexcept;
+    friend void internal::set_callback(future<U...>&&, V*) noexcept;
     template <typename Future>
     friend struct internal::call_then_impl;
     /// \endcond
@@ -2182,10 +2182,10 @@ namespace internal {
 
 template <typename... T, typename U>
 inline
-void set_callback(future<T...>& fut, U* callback) noexcept {
+void set_callback(future<T...>&& fut, U* callback) noexcept {
     // It would be better to use continuation_base<T...> for U, but
     // then a derived class of continuation_base<T...> won't be matched
-    return fut.set_callback(callback);
+    return std::move(fut).set_callback(callback);
 }
 
 }

include/seastar/core/loop.hh
@@ -72,7 +72,7 @@ public:
             do {
                 auto f = futurize_invoke(_action);
                 if (!f.available()) {
-                    internal::set_callback(f, this);
+                    internal::set_callback(std::move(f), this);
                     return;
                 }
                 if (f.get0() == stop_iteration::yes) {
@@ -124,7 +124,7 @@ future<> repeat(AsyncAction&& action) noexcept {
                 memory::scoped_critical_alloc_section _;
                 auto repeater = new internal::repeater<AsyncAction>(std::move(action));
                 auto ret = repeater->get_future();
-                internal::set_callback(f, repeater);
+                internal::set_callback(std::move(f), repeater);
                 return ret;
             }();
         }
@@ -189,7 +189,7 @@ public:
             do {
                 auto f = futurize_invoke(_action);
                 if (!f.available()) {
-                    internal::set_callback(f, this);
+                    internal::set_callback(std::move(f), this);
                     return;
                 }
                 auto ret = f.get0();
@@ -242,7 +242,7 @@ repeat_until_value(AsyncAction action) noexcept {
             memory::scoped_critical_alloc_section _;
             auto state = new internal::repeat_until_value_state<AsyncAction, value_type>(std::move(action));
             auto ret = state->get_future();
-            internal::set_callback(f, state);
+            internal::set_callback(std::move(f), state);
             return ret;
           }();
         }
@@ -296,7 +296,7 @@ public:
                 }
                 auto f = _action();
                 if (!f.available()) {
-                    internal::set_callback(f, this);
+                    internal::set_callback(std::move(f), this);
                     return;
                 }
                 if (f.failed()) {
@@ -349,7 +349,7 @@ future<> do_until(StopCondition stop_cond, AsyncAction action) noexcept {
                 memory::scoped_critical_alloc_section _;
                 auto task = new do_until_state<StopCondition, AsyncAction>(std::move(stop_cond), std::move(action));
                 auto ret = task->get_future();
-                internal::set_callback(f, task);
+                internal::set_callback(std::move(f), task);
                 return ret;
             }();
         }
@@ -383,9 +383,9 @@ class do_for_each_state final : public continuation_base<> {
     promise<> _pr;
 
 public:
-    do_for_each_state(Iterator begin, Iterator end, AsyncAction action, future<> first_unavailable)
+    do_for_each_state(Iterator begin, Iterator end, AsyncAction action, future<>&& first_unavailable)
         : _begin(std::move(begin)), _end(std::move(end)), _action(std::move(action)) {
-        internal::set_callback(first_unavailable, this);
+        internal::set_callback(std::move(first_unavailable), this);
     }
     virtual void run_and_dispose() noexcept override {
         std::unique_ptr<do_for_each_state> zis(this);
@@ -401,7 +401,7 @@ public:
             }
             if (!f.available() || need_preempt()) {
                 _state = {};
-                internal::set_callback(f, this);
+                internal::set_callback(std::move(f), this);
                 zis.release();
                 return;
             }

include/seastar/core/when_all.hh
@@ -113,7 +113,7 @@ public:
             return true;
         } else {
             auto c = new (continuation) when_all_state_component(wasb, f);
-            set_callback(*f, c);
+            set_callback(std::move(*f), c);
             return false;
         }
     }

include/seastar/coroutine/all.hh
@@ -183,7 +183,7 @@ private:
             } else {
                 if (!std::get<idx>(state._futures).available()) {
                     auto task = new (&_continuation_storage) intermediate_task<idx>(*this);
-                    seastar::internal::set_callback(std::get<idx>(state._futures), task);
+                    seastar::internal::set_callback(std::move(std::get<idx>(state._futures)), task);
                 } else {
                     process<idx + 1>();
                 }

src/core/future-util.cc
@@ -57,7 +57,7 @@ void parallel_for_each_state::wait_for_one() noexcept {
 
     // If there's an incompelete future, wait for it.
     if (!_incomplete.empty()) {
-        internal::set_callback(_incomplete.back(), static_cast<continuation_base<>*>(this));
+        internal::set_callback(std::move(_incomplete.back()), static_cast<continuation_base<>*>(this));
         // This future's state will be collected in run_and_dispose(), so we can drop it.
         _incomplete.pop_back();
         return;

commit 5773f962dc283aefdd4adcf4b0b0c2aa502f2187

coroutine: parallel_for_each: set_callback: destroy future after move
Calling _futures.pop_back() before std::move(fut)
destroys it in place, before it's been moved, causing
set_callback to fail assert(_promise);

This was caused by 710a1bfdc02202fe9e59df8ea31de5b82b893fb4
future: set_callback: consume future input

Test: coroutines_test.test_parallel_for_each_exception(debug)

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>
Message-Id: <20220322102149.1612698-1-bhalevy@scylladb.com>

include/seastar/coroutine/parallel_for_each.hh
@@ -85,13 +85,12 @@ class [[nodiscard("must co_await an parallel_for_each() object")]] parallel_for_
     }
 
     void set_callback() noexcept {
-        auto&& fut = std::move(_futures.back());
-        _futures.pop_back();
         // To reuse `this` as continuation_base<>
         // we must reset _state, to allow setting
         // it again.
         this->_state = {};
-        seastar::internal::set_callback(std::move(fut), reinterpret_cast<continuation_base<>*>(this));
+        seastar::internal::set_callback(std::move(_futures.back()), reinterpret_cast<continuation_base<>*>(this));
+        _futures.pop_back();
     }
 
     void resume_or_set_callback() noexcept {