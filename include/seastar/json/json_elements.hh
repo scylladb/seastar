3b267834d8d3b45ca0eab462cd5e02268873b008
Benny Halevy <bhalevy@scylladb.com>
2022-05-25 08:25:39 +0300

    json_elements: stream_range_as_array, stream_object: mark lambda mutable
    Otherwise the captured objects will get copied when
    moved later when lambda is called.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- include/seastar/json/json_elements.hh
+++ include/seastar/json/json_elements.hh
@@ -317,7 +317,7 @@ struct json_return_type {
 template<typename Container, typename Func>
 SEASTAR_CONCEPT( requires requires (Container c, Func aa, output_stream<char> s) { { formatter::write(s, aa(*c.begin())) } -> std::same_as<future<>>; } )
 std::function<future<>(output_stream<char>&&)> stream_range_as_array(Container val, Func fun) {
-    return [val = std::move(val), fun = std::move(fun)](output_stream<char>&& s) {
+    return [val = std::move(val), fun = std::move(fun)](output_stream<char>&& s) mutable {
         return do_with(output_stream<char>(std::move(s)), Container(std::move(val)), Func(std::move(fun)), true, [](output_stream<char>& s, const Container& val, const Func& f, bool& first){
             return s.write("[").then([&val, &s, &first, &f] () {
                 return do_for_each(val, [&s, &first, &f](const typename Container::value_type& v){
@@ -344,7 +344,7 @@ std::function<future<>(output_stream<char>&&)> stream_range_as_array(Container v
  */
 template<class T>
 std::function<future<>(output_stream<char>&&)> stream_object(T val) {
-    return [val = std::move(val)](output_stream<char>&& s) {
+    return [val = std::move(val)](output_stream<char>&& s) mutable {
         return do_with(output_stream<char>(std::move(s)), T(std::move(val)), [](output_stream<char>& s, const T& val){
             return formatter::write(s, val).then([&s] {
                 return s.close();
