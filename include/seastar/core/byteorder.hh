f407c5969516f7c45fcb165c084420ab8a1860a9
Kefu Chai <tchaikov@gmail.com>
2022-06-14 21:21:37 +0800

    byteorder: use boost::endian to do the conversion.
    instead of using ADL to dispatch the functions manually, use
    boost::endian conversion to do this job. this change not only
    reduces the code base to maintain. it also helps to avoid relying
    on some non-standard-compliant header and function.
    
    Signed-off-by: Kefu Chai <tchaikov@gmail.com>
    Message-Id: <20220614132138.3489754-1-tchaikov@gmail.com>

--- include/seastar/core/byteorder.hh
+++ include/seastar/core/byteorder.hh
@@ -22,46 +22,28 @@
 #pragma once
 
 #include <algorithm>
-#include <endian.h>
+#include <boost/endian/conversion.hpp>
 #include <seastar/core/unaligned.hh>
 
 namespace seastar {
 
-inline uint8_t cpu_to_le(uint8_t x) noexcept { return x; }
-inline uint8_t le_to_cpu(uint8_t x) noexcept { return x; }
-inline uint16_t cpu_to_le(uint16_t x) noexcept { return htole16(x); }
-inline uint16_t le_to_cpu(uint16_t x) noexcept { return le16toh(x); }
-inline uint32_t cpu_to_le(uint32_t x) noexcept { return htole32(x); }
-inline uint32_t le_to_cpu(uint32_t x) noexcept { return le32toh(x); }
-inline uint64_t cpu_to_le(uint64_t x) noexcept { return htole64(x); }
-inline uint64_t le_to_cpu(uint64_t x) noexcept { return le64toh(x); }
+template <typename T>
+inline T cpu_to_le(T x) noexcept {
+  return boost::endian::native_to_little(x);
+}
+template <typename T>
+inline T le_to_cpu(T x) noexcept {
+  return boost::endian::little_to_native(x);
+}
 
-inline int8_t cpu_to_le(int8_t x) noexcept { return x; }
-inline int8_t le_to_cpu(int8_t x) noexcept { return x; }
-inline int16_t cpu_to_le(int16_t x) noexcept { return htole16(x); }
-inline int16_t le_to_cpu(int16_t x) noexcept { return le16toh(x); }
-inline int32_t cpu_to_le(int32_t x) noexcept { return htole32(x); }
-inline int32_t le_to_cpu(int32_t x) noexcept { return le32toh(x); }
-inline int64_t cpu_to_le(int64_t x) noexcept { return htole64(x); }
-inline int64_t le_to_cpu(int64_t x) noexcept { return le64toh(x); }
-
-inline uint8_t cpu_to_be(uint8_t x) noexcept { return x; }
-inline uint8_t be_to_cpu(uint8_t x) noexcept { return x; }
-inline uint16_t cpu_to_be(uint16_t x) noexcept { return htobe16(x); }
-inline uint16_t be_to_cpu(uint16_t x) noexcept { return be16toh(x); }
-inline uint32_t cpu_to_be(uint32_t x) noexcept { return htobe32(x); }
-inline uint32_t be_to_cpu(uint32_t x) noexcept { return be32toh(x); }
-inline uint64_t cpu_to_be(uint64_t x) noexcept { return htobe64(x); }
-inline uint64_t be_to_cpu(uint64_t x) noexcept { return be64toh(x); }
-
-inline int8_t cpu_to_be(int8_t x) noexcept { return x; }
-inline int8_t be_to_cpu(int8_t x) noexcept { return x; }
-inline int16_t cpu_to_be(int16_t x) noexcept { return htobe16(x); }
-inline int16_t be_to_cpu(int16_t x) noexcept { return be16toh(x); }
-inline int32_t cpu_to_be(int32_t x) noexcept { return htobe32(x); }
-inline int32_t be_to_cpu(int32_t x) noexcept { return be32toh(x); }
-inline int64_t cpu_to_be(int64_t x) noexcept { return htobe64(x); }
-inline int64_t be_to_cpu(int64_t x) noexcept { return be64toh(x); }
+template <typename T>
+inline T cpu_to_be(T x) noexcept {
+  return boost::endian::native_to_big(x);
+}
+template <typename T>
+inline T be_to_cpu(T x) noexcept {
+  return boost::endian::big_to_native(x);
+}
 
 template <typename T>
 inline T cpu_to_le(const unaligned<T>& v) noexcept {
