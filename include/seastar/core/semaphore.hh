scylladb/seastar

commit 13e168bf12d66a89d46a127ac78f7adaae74e303

semaphore: expiry_handler: distinguish between broken_semaphore and abort_requested_exception
Distiguish between an exception generated via semaphore::broken()
vs. a particular wait() being abort via its abort_source.

In the first case, the exception is kept in semaphore::_ex
so just get it from there (by default it will use
the ExceptionFactory broken() excception, if available, or
semaphore::broken_semaphore otherwise.

If there's no semaphore-global exception set, it means
that expiry_handler was called on a particular entry.
Generate abort_requested_exception so that the caller of wait()
or get_units() would be able to distinguish between the cases.

Extend the existing exception factories with an aborted() exception
for representing this case.

Test: semaphore_test

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

include/seastar/core/semaphore.hh
@@ -43,6 +43,15 @@ class has_broken {
 public:
     constexpr static bool value = check<T>(nullptr);
 };
+// Test if a class T has member function aborted()
+template <typename T>
+class has_aborted {
+    template <typename U> constexpr static bool check(decltype(&U::aborted)) { return true; }
+    template <typename U> constexpr static bool check(...) { return false; }
+
+public:
+    constexpr static bool value = check<T>(nullptr);
+};
 }
 
 /// \addtogroup fiber-module
@@ -66,6 +75,16 @@ public:
     virtual const char* what() const noexcept;
 };
 
+/// Exception thrown when a semaphore wait operation
+/// was aborted.
+///
+/// \see semaphore::wait(abort_source&, size_t nr)
+class semaphore_aborted : public abort_requested_exception {
+public:
+    /// Reports the exception reason.
+    virtual const char* what() const noexcept;
+};
+
 /// Exception Factory for standard semaphore
 ///
 /// constructs standard semaphore exceptions
@@ -73,6 +92,7 @@ public:
 struct semaphore_default_exception_factory {
     static semaphore_timed_out timeout() noexcept;
     static broken_semaphore broken() noexcept;
+    static semaphore_aborted aborted() noexcept;
 };
 
 class named_semaphore_timed_out : public semaphore_timed_out {
@@ -89,12 +109,20 @@ public:
     virtual const char* what() const noexcept;
 };
 
+class named_semaphore_aborted : public abort_requested_exception {
+    sstring _msg;
+public:
+    named_semaphore_aborted(std::string_view msg) noexcept;
+    virtual const char* what() const noexcept;
+};
+
 // A factory of semaphore exceptions that contain additional context: the semaphore name
 // auto sem = named_semaphore(0, named_semaphore_exception_factory{"file_opening_limit_semaphore"});
 struct named_semaphore_exception_factory {
     sstring name;
     named_semaphore_timed_out timeout() const noexcept;
     broken_named_semaphore broken() const noexcept;
+    named_semaphore_aborted aborted() const noexcept;
 };
 
 /// \brief Counted resource guard.
@@ -140,15 +168,17 @@ private:
                 } catch (...) {
                     e.pr.set_exception(semaphore_timed_out());
                 }
+            } else if (sem._ex) {
+                e.pr.set_exception(sem._ex);
             } else {
-                if constexpr (internal::has_broken<exception_factory>::value) {
+                if constexpr (internal::has_aborted<exception_factory>::value) {
                     try {
-                        e.pr.set_exception(static_cast<exception_factory>(sem).broken());
+                        e.pr.set_exception(static_cast<exception_factory>(sem).aborted());
                     } catch (...) {
-                        e.pr.set_exception(broken_semaphore());
+                        e.pr.set_exception(semaphore_aborted());
                     }
                 } else {
-                    e.pr.set_exception(broken_semaphore());
+                    e.pr.set_exception(semaphore_aborted());
                 }
             }
         }

src/core/semaphore.cc
@@ -45,6 +45,10 @@ const char* semaphore_timed_out::what() const noexcept {
     return "Semaphore timedout";
 }
 
+const char* semaphore_aborted::what() const noexcept {
+    return "Semaphore aborted";
+}
+
 semaphore_timed_out semaphore_default_exception_factory::timeout() noexcept {
     static_assert(std::is_nothrow_default_constructible_v<semaphore_timed_out>);
     return semaphore_timed_out();
@@ -53,6 +57,11 @@ semaphore_timed_out semaphore_default_exception_factory::timeout() noexcept {
 broken_semaphore semaphore_default_exception_factory::broken() noexcept {
     static_assert(std::is_nothrow_default_constructible_v<broken_semaphore>);
     return broken_semaphore();
+}
+
+semaphore_aborted semaphore_default_exception_factory::aborted() noexcept {
+    static_assert(std::is_nothrow_default_constructible_v<semaphore_aborted>);
+    return semaphore_aborted();
 }
 
 // A factory of semaphore exceptions that contain additional context: the semaphore name
@@ -81,6 +90,14 @@ broken_named_semaphore::broken_named_semaphore(std::string_view msg) noexcept : 
     }
 }
 
+named_semaphore_aborted::named_semaphore_aborted(std::string_view msg) noexcept : _msg() {
+    try {
+        _msg = format("Semaphore aborted: {}", msg);
+    } catch (...) {
+        // ignore, empty _msg will generate a static message in what().
+    }
+}
+
 const char* named_semaphore_timed_out::what() const noexcept {
     // return a static message if generating the dynamic message failed.
     return _msg.empty() ? "Named semaphore timed out" : _msg.c_str();
@@ -91,12 +108,21 @@ const char* broken_named_semaphore::what() const noexcept {
     return _msg.empty() ? "Broken named semaphore" : _msg.c_str();
 }
 
+const char* named_semaphore_aborted::what() const noexcept {
+    // return a static message if generating the dynamic message failed.
+    return _msg.empty() ? "Named semaphore aborted" : _msg.c_str();
+}
+
 named_semaphore_timed_out named_semaphore_exception_factory::timeout() const noexcept {
     return named_semaphore_timed_out(name);
 }
 
 broken_named_semaphore named_semaphore_exception_factory::broken() const noexcept {
     return broken_named_semaphore(name);
+}
+
+named_semaphore_aborted named_semaphore_exception_factory::aborted() const noexcept {
+    return named_semaphore_aborted(name);
 }
 
 } // namespace seastar

tests/unit/semaphore_test.cc
@@ -334,7 +334,7 @@ SEASTAR_THREAD_TEST_CASE(test_semaphore_units_abort) {
         as.request_abort();
     });
 
-    BOOST_REQUIRE_THROW(f.get(), broken_semaphore);
+    BOOST_REQUIRE_THROW(f.get(), semaphore_aborted);
 }
 
 SEASTAR_THREAD_TEST_CASE(test_named_semaphore_error) {
@@ -380,7 +380,7 @@ SEASTAR_THREAD_TEST_CASE(test_semaphore_abort_after_wait) {
     });
     as.request_abort();
     sem.signal();
-    BOOST_CHECK_THROW(fut1.get(), broken_semaphore);
+    BOOST_CHECK_THROW(fut1.get(), semaphore_aborted);
     BOOST_REQUIRE_EQUAL(x, 0);
 }
 
@@ -393,6 +393,6 @@ SEASTAR_THREAD_TEST_CASE(test_semaphore_abort_before_wait) {
         x++;
     });
     sem.signal();
-    BOOST_CHECK_THROW(fut1.get(), broken_semaphore);
+    BOOST_CHECK_THROW(fut1.get(), semaphore_aborted);
     BOOST_REQUIRE_EQUAL(x, 0);
 }

commit 43fce0379347f92ccaa0c4836f2c66d3c0861ade

semaphore: derive named_semaphore_aborted exception from semaphore_aborted
Change 13e168bf12d66a89d46a127ac78f7adaae74e303 introduced
both semaphore_aborted and named_semaphore_aborted,
but defined both as derived from abort_requested_exception.

While correct by itself, the convention for semaphore
exception factories is to derive the exceptions from the
basic semaphore exceptions so they can be handled
as a class of errors regardless of the particular type
of semaphore.

This is causing scylladb/scylla#10666
after scylladb/scylla@8f39547d89991dc251256fc9a57b3eeea5175e2b

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

include/seastar/core/semaphore.hh
@@ -109,7 +109,7 @@ public:
     virtual const char* what() const noexcept;
 };
 
-class named_semaphore_aborted : public abort_requested_exception {
+class named_semaphore_aborted : public semaphore_aborted {
     sstring _msg;
 public:
     named_semaphore_aborted(std::string_view msg) noexcept;