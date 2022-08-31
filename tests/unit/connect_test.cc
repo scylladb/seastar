b2e445015bd737ddf8115f13c02e67c34cd0b791
Benny Halevy <bhalevy@scylladb.com>
2022-08-31 14:56:13 +0300

    connect_test: convert test cases to seastar thread
    To make it easier to close net resources in the following patches.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- tests/unit/connect_test.cc
+++ tests/unit/connect_test.cc
@@ -1,59 +1,43 @@
 #include <seastar/core/reactor.hh>
 #include <seastar/testing/test_case.hh>
+#include <seastar/testing/thread_test_case.hh>
 #include <seastar/testing/test_runner.hh>
 #include <seastar/net/ip.hh>
 
 using namespace seastar;
 using namespace net;
 
-SEASTAR_TEST_CASE(test_connection_attempt_is_shutdown) {
+SEASTAR_THREAD_TEST_CASE(test_connection_attempt_is_shutdown) {
     ipv4_addr server_addr("172.16.0.1");
     auto unconn = make_socket();
-    auto f = unconn
-        .connect(make_ipv4_address(server_addr))
-        .then_wrapped([] (auto&& f) {
-            try {
-                f.get();
-                BOOST_REQUIRE(false);
-            } catch (...) {}
-        });
+    auto f = unconn.connect(make_ipv4_address(server_addr));
     unconn.shutdown();
-    return f.finally([unconn = std::move(unconn)] {});
+    BOOST_REQUIRE_THROW(f.get(), std::exception);
 }
 
-SEASTAR_TEST_CASE(test_unconnected_socket_shutsdown_established_connection) {
+SEASTAR_THREAD_TEST_CASE(test_unconnected_socket_shutsdown_established_connection) {
     // Use a random port to reduce chance of conflict.
     // TODO: retry a few times on failure.
     std::default_random_engine& rnd = testing::local_random_engine;
     auto distr = std::uniform_int_distribution<uint16_t>(12000, 65000);
     auto sa = make_ipv4_address({"127.0.0.1", distr(rnd)});
-    return do_with(engine().net().listen(sa, listen_options()), [sa] (auto& listener) {
+    auto listener = engine().net().listen(sa, listen_options());
+    // FIXME: indentation
         auto f = listener.accept();
         auto unconn = make_socket();
-        auto connf = unconn.connect(sa);
-        return connf.then([unconn = std::move(unconn)] (auto&& conn) mutable {
+        auto conn = unconn.connect(sa).get();
             unconn.shutdown();
-            return do_with(std::move(conn), [] (auto& conn) {
-                return do_with(conn.output(1), [] (auto& out) {
-                    return out.write("ping").then_wrapped([] (auto&& f) {
-                        try {
-                            f.get();
-                            BOOST_REQUIRE(false);
-                        } catch (...) {}
-                    });
-                });
-            });
-        }).finally([f = std::move(f)] () mutable {
-            return std::move(f);
-        });
-    });
+            auto out = conn.output(1);
+            BOOST_REQUIRE_THROW(out.write("ping").get(), std::exception);
+    f.get();
 }
 
-SEASTAR_TEST_CASE(test_accept_after_abort) {
+SEASTAR_THREAD_TEST_CASE(test_accept_after_abort) {
     std::default_random_engine& rnd = testing::local_random_engine;
     auto distr = std::uniform_int_distribution<uint16_t>(12000, 65000);
     auto sa = make_ipv4_address({"127.0.0.1", distr(rnd)});
-    return do_with(seastar::server_socket(engine().net().listen(sa, listen_options())), [] (auto& listener) {
+    auto listener = engine().net().listen(sa, listen_options());
+    // FIXME: indentation
         using ftype = future<accept_result>;
         promise<ftype> p;
         future<ftype> done = p.get_future();
@@ -62,13 +46,7 @@ SEASTAR_TEST_CASE(test_accept_after_abort) {
             p.set_value(listener.accept());
         });
         listener.abort_accept();
-        return done.then([] (ftype f) {
-          return f.then_wrapped([] (ftype f) {
-            BOOST_REQUIRE(f.failed());
-            if (f.available()) {
-                f.ignore_ready_future();
-            }
-          });
-        });
-    });
+        ftype done_fut = done.get();
+        BOOST_REQUIRE_THROW(done_fut.get(), std::exception);
+    f.get();
 }
