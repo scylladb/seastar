5cc4acbdd862b9f7d1f70823ccb8a8694de265a8
Kefu Chai <tchaikov@gmail.com>
2022-12-13 11:16:55 +0800

    build: use pkg_search_module(.. IMPORTED_TARGET ..) in Findyaml-cpp
    see also f57253810fd6b2dc09182209e888a7056af70ebf, this change applies
    the same change to FindLibUring.cmake.
    
    Signed-off-by: Kefu Chai <tchaikov@gmail.com>

--- cmake/Findyaml-cpp.cmake
+++ cmake/Findyaml-cpp.cmake
@@ -22,20 +22,25 @@
 
 find_package (PkgConfig REQUIRED)
 
-pkg_search_module (yaml-cpp_PC yaml-cpp)
+pkg_search_module (yaml-cpp IMPORTED_TARGET GLOBAL yaml-cpp)
 
-find_library (yaml-cpp_LIBRARY
-  NAMES yaml-cpp
-  HINTS
-    ${yaml-cpp_PC_LIBDIR}
-    ${yaml-cpp_PC_LIBRARY_DIRS})
+if (yaml-cpp_FOUND)
+  add_library (yaml-cpp::yaml-cpp INTERFACE IMPORTED)
+  target_link_libraries (yaml-cpp::yaml-cpp INTERFACE PkgConfig::yaml-cpp)
+  set (yaml-cpp_LIBRARY ${yaml-cpp_LIBRARIES})
+  set (yaml-cpp_INCLUDE_DIR ${yaml-cpp_INCLUDE_DIRS})
+endif ()
 
-find_path (yaml-cpp_INCLUDE_DIR
-  NAMES yaml-cpp/yaml.h
-  PATH_SUFFIXES yaml-cpp
-  HINTS
-    ${yaml-cpp_PC_INCLUDEDIR}
-    ${yaml-cpp_PC_INCLUDE_DIRS})
+if (NOT yaml-cpp_LIBRARY)
+  find_library (yaml-cpp_LIBRARY
+   NAMES yaml-cpp)
+endif ()
+
+if (NOT yaml-cpp_INCLUDE_DIR)
+ find_path (yaml-cpp_INCLUDE_DIR
+    NAMES yaml-cpp/yaml.h
+    PATH_SUFFIXES yaml-cpp)
+endif ()
 
 mark_as_advanced (
   yaml-cpp_LIBRARY
@@ -47,16 +52,17 @@ find_package_handle_standard_args (yaml-cpp
   REQUIRED_VARS
     yaml-cpp_LIBRARY
     yaml-cpp_INCLUDE_DIR
-  VERSION_VAR yaml-cpp_PC_VERSION)
+  VERSION_VAR yaml-cpp_VERSION)
 
-set (yaml-cpp_LIBRARIES ${yaml-cpp_LIBRARY})
-set (yaml-cpp_INCLUDE_DIRS ${yaml-cpp_INCLUDE_DIR})
+if (yaml-cpp_FOUND)
+  set (yaml-cpp_LIBRARIES ${yaml-cpp_LIBRARY})
+  set (yaml-cpp_INCLUDE_DIRS ${yaml-cpp_INCLUDE_DIR})
+  if (NOT (TARGET yaml-cpp::yaml-cpp))
+    add_library (yaml-cpp::yaml-cpp UNKNOWN IMPORTED)
 
-if (yaml-cpp_FOUND AND NOT (TARGET yaml-cpp::yaml-cpp))
-  add_library (yaml-cpp::yaml-cpp UNKNOWN IMPORTED)
-
-  set_target_properties (yaml-cpp::yaml-cpp
-    PROPERTIES
-      IMPORTED_LOCATION ${yaml-cpp_LIBRARY}
-      INTERFACE_INCLUDE_DIRECTORIES ${yaml-cpp_INCLUDE_DIRS})
+    set_target_properties (yaml-cpp::yaml-cpp
+      PROPERTIES
+        IMPORTED_LOCATION ${yaml-cpp_LIBRARY}
+        INTERFACE_INCLUDE_DIRECTORIES ${yaml-cpp_INCLUDE_DIRS})
+  endif ()
 endif ()
