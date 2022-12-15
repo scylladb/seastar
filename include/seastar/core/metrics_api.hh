364ca84d6e59253b8f4a480b57030d187747f596
Amnon Heiman <amnon@scylladb.com>
2022-12-15 18:46:17 +0200

    Add an API for the metrics layer to manipulate metrics dynamically.
    It is typical for software that builds on top of Seastar report
    per-shard metrics.  With modern hardware, this can quickly grow to a
    large number of metrics that overload the reporting system and the
    collecting server.
    
    One of the main issues around metrics reduction is that many of the
    metrics are only helpful in certain situations.
    
    It will be best to have the option to add metrics freely and choose in
    runtime what to report.
    
    Changing label values lets a metrics-collecting server (e.g.,
    Prometheus) ask for only some of the metrics.
    
    Disabling and enabling the metrics will ensure they will not be
    calculated and reported at all, but the configuration will need to be
    changed when required.
    
    This series adds an option to perform metrics manipulation dynamically.
    It includes adding, removing, and changing labels and also enable,
    disable metrics, and enable and disable the skip_when_empty option.
    
    The general logic follows Prometheus metrics_relabel_config
    configuration.
    
    The relabel config is a vector of rules applied one after the other.
    Rules can be changed at any point, it would first update all the
    existing metrics, and then any new metrics will be updated according to
    the rules.
    
    Here are a few examples:
    
    To add a level label with a value 1, to the reactor_utilization metric:
        std::vector<sm::relabel_config> rl(1);
        rl[0].source_labels = {"__name__"};
        rl[0].target_label = "level";
        rl[0].replacement = "1";
        rl[0].expr = "reactor_utilization";
       set_relabel_configs(rl);
    
    To report only the metrics with the level label equals 1
    
        std::vector<sm::relabel_config> rl(2);
        rl[0].source_labels = {"__name__"};
        rl[0].action = sm::relabel_config::relabel_action::drop;
    
        rl[1].source_labels = {"level"};
        rl[1].expr = "1";
        rl[1].action = sm::relabel_config::relabel_action::keep;
        set_relabel_configs(rl);

--- include/seastar/core/metrics_api.hh
+++ include/seastar/core/metrics_api.hh
@@ -25,6 +25,7 @@
 #include <unordered_map>
 #include <seastar/core/sharded.hh>
 #include <boost/functional/hash.hpp>
+
 /*!
  * \file metrics_api.hh
  * \brief header file for metric API layer (like promehteus or collectd)
@@ -60,6 +61,23 @@ struct hash<seastar::metrics::impl::labels_type> {
 
 namespace seastar {
 namespace metrics {
+struct relabel_config;
+
+/*!
+ * \brief result of metric relabeling
+ *
+ * The result of calling set_relabel_configs.
+ *
+ * metrics_relabeled_due_to_collision the number of metrics that caused conflict
+ * and were relabeled to avoid name collision.
+ *
+ * Non zero value indicates there were name collisions.
+ *
+ */
+struct metric_relabeling_result {
+    size_t metrics_relabeled_due_to_collision;
+};
+
 namespace impl {
 
 /**
@@ -102,6 +120,9 @@ public:
         return _name;
     }
     const labels_type& labels() const {
+        return _labels;
+    }
+    labels_type& labels() {
         return _labels;
     }
     sstring full_name() const;
@@ -161,6 +182,7 @@ struct metric_family_info {
  */
 struct metric_info {
     metric_id id;
+    labels_type original_labels;
     bool enabled;
     skip_when_empty should_skip_when_empty;
 };
@@ -210,6 +232,9 @@ public:
     const metric_info& info() const {
         return _info;
     }
+    metric_info& info() {
+        return _info;
+   }
     metric_function& get_function() {
         return _f;
     }
@@ -326,6 +351,7 @@ class impl {
     shared_ptr<metric_metadata> _metadata;
     std::set<sstring> _labels;
     std::vector<std::vector<metric_function>> _current_metrics;
+    std::vector<relabel_config> _relabel_configs;
 public:
     value_map& get_value_map() {
         return _value_map;
@@ -359,6 +385,12 @@ public:
 
     const std::set<sstring>& get_labels() const noexcept {
         return _labels;
+    }
+
+    future<metric_relabeling_result> set_relabel_configs(const std::vector<relabel_config>& relabel_configs);
+
+    const std::vector<relabel_config>& get_relabel_configs() const noexcept {
+        return _relabel_configs;
     }
 };
 
@@ -395,6 +427,58 @@ struct options : public program_options::option_group {
  * \brief set the metrics configuration
  */
 future<> configure(const options& opts);
+
+/*!
+ * \brief Perform relabeling and operation on metrics dynamically.
+ *
+ * The function would return true if the changes were applied with no conflict
+ * or false, if there was a conflict in the registration.
+ *
+  * The general logic follows Prometheus metrics_relabel_config configuration.
+ * The relabel rules are applied one after the other.
+ * You can add or change a label. you can enable or disable a metric,
+ * in that case the metrics will not be reported at all.
+ * You can turn on and off the skip_when_empty flag.
+ *
+ * Using the Prometheus convention, the metric name is __name__.
+ * Names cannot be changed.
+ *
+ * Import notes:
+ * - The relabeling always starts from the original set of labels the metric
+ *   was created with.
+ * - calling with an empty set will remove the relabel config and will
+ *   return all metrics to their original labels
+ * - To prevent a situation that calling this function would crash the system.
+ *   in a situation where a conflicting metrics name are entered, an additional label
+ *   will be added to the labels with a unique ID.
+ *
+ * A few examples:
+ * To add a level label with a value 1, to the reactor_utilization metric:
+ *  std::vector<sm::relabel_config> rl(1);
+    rl[0].source_labels = {"__name__"};
+    rl[0].target_label = "level";
+    rl[0].replacement = "1";
+    rl[0].expr = "reactor_utilization";
+   set_relabel_configs(rl);
+ *
+ * To report only the metrics with the level label equals 1
+ *
+    std::vector<sm::relabel_config> rl(2);
+    rl[0].source_labels = {"__name__"};
+    rl[0].action = sm::relabel_config::relabel_action::drop;
+
+    rl[1].source_labels = {"level"};
+    rl[1].expr = "1";
+    rl[1].action = sm::relabel_config::relabel_action::keep;
+    set_relabel_configs(rl);
+
+ */
+future<metric_relabeling_result> set_relabel_configs(const std::vector<relabel_config>& relabel_configs);
+/*
+ * \brief return the current relabel_configs
+ * This function returns a vector of the current relabel configs
+ */
+const std::vector<relabel_config>& get_relabel_configs();
 
 }
 }
