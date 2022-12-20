scylladb/seastar

commit 35b2dc3145b636f9476fa3d5e8be9b998a0f4623

build: use pkg_search_module(.. IMPORTED_TARGET ..) in FindLibUring
see also f57253810fd6b2dc09182209e888a7056af70ebf, this change applies
the same change to FindLibUring.cmake.

Signed-off-by: Kefu Chai <tchaikov@gmail.com>

cmake/FindLibUring.cmake
@@ -22,19 +22,24 @@
 
 find_package (PkgConfig REQUIRED)
 
-pkg_search_module (URING_PC liburing)
+pkg_search_module (LibUring IMPORTED_TARGET GLOBAL liburing)
 
-find_library (URING_LIBRARY
-  NAMES uring
-  HINTS
-    ${URING_PC_LIBDIR}
-    ${URING_PC_LIBRARY_DIRS})
+if (LibUring_FOUND)
+  add_library (URING::uring INTERFACE IMPORTED)
+  target_link_libraries (URING::uring INTERFACE PkgConfig::LibUring)
+  set (URING_LIBRARY ${LibUring_LIBRARIES})
+  set (URING_INCLUDE_DIR ${LibUring_INCLUDE_DIRS})
+endif ()
 
-find_path (URING_INCLUDE_DIR
-  NAMES liburing.h
-  HINTS
-    ${URING_PC_INCLUDEDIR}
-    ${URING_PC_INCLUDE_DIRS})
+if (NOT URING_LIBRARY)
+  find_library (URING_LIBRARY
+    NAMES uring)
+endif ()
+
+if (NOT URING_INCLUDE_DIR)
+  find_path (URING_INCLUDE_DIR
+    NAMES liburing.h)
+endif ()
 
 if (URING_INCLUDE_DIR)
   include (CheckStructHasMember)
@@ -51,7 +56,6 @@ mark_as_advanced (
   URING_INCLUDE_DIR
   HAVE_IOURING_FEATURES)
 
-
 include (FindPackageHandleStandardArgs)
 
 find_package_handle_standard_args (LibUring
@@ -59,16 +63,17 @@ find_package_handle_standard_args (LibUring
     URING_LIBRARY
     URING_INCLUDE_DIR
     HAVE_IOURING_FEATURES
-  VERSION_VAR URING_PC_VERSION)
+  VERSION_VAR LibUring_VERSION)
 
-set (URING_LIBRARIES ${URING_LIBRARY})
-set (URING_INCLUDE_DIRS ${URING_INCLUDE_DIR})
+if (LibUring_FOUND)
+  set (URING_LIBRARIES ${URING_LIBRARY})
+  set (URING_INCLUDE_DIRS ${URING_INCLUDE_DIR})
+  if (NOT (TARGET URING::uring))
+    add_library (URING::uring UNKNOWN IMPORTED)
 
-if (LibUring_FOUND AND NOT (TARGET URING::uring))
-  add_library (URING::uring UNKNOWN IMPORTED)
-
-  set_target_properties (URING::uring
-    PROPERTIES
-      IMPORTED_LOCATION ${URING_LIBRARY}
-      INTERFACE_INCLUDE_DIRECTORIES ${URING_INCLUDE_DIRS})
+    set_target_properties (URING::uring
+      PROPERTIES
+        IMPORTED_LOCATION ${URING_LIBRARY}
+        INTERFACE_INCLUDE_DIRECTORIES ${URING_INCLUDE_DIRS})
+  endif ()
 endif ()

commit 26c8e681ad0f18905e8b4d40218e3fe39274fb42

build: export the full path of uring with URING_LIBRARIES
to address the same issue fixed by
3db15b5681c460a759641a3907eb9db84bdcf395. otherwise we'd pass "uring"
to the compiler when linking against liburing, and it would refuse to
work. as "uring" alone is not a valid command line option to the
compiler, "-luring" is.

this change addresses the regression introduced by
35b2dc3145b636f9476fa3d5e8be9b998a0f4623, and partially reverts it.
but this change only uses LibUring_LIBDIR as the HINTS when finding the
library, as LibUring_LIBDIR is more specific and it should be identical
to LibUring_LIBRARY_DIRS, as only a single moduleSpec is passed to
pkg_check_modules().

Signed-off-by: Kefu Chai <tchaikov@gmail.com>

cmake/FindLibUring.cmake
@@ -27,14 +27,12 @@ pkg_search_module (LibUring IMPORTED_TARGET GLOBAL liburing)
 if (LibUring_FOUND)
   add_library (URING::uring INTERFACE IMPORTED)
   target_link_libraries (URING::uring INTERFACE PkgConfig::LibUring)
-  set (URING_LIBRARY ${LibUring_LIBRARIES})
   set (URING_INCLUDE_DIR ${LibUring_INCLUDE_DIRS})
 endif ()
 
-if (NOT URING_LIBRARY)
-  find_library (URING_LIBRARY
-    NAMES uring)
-endif ()
+find_library (URING_LIBRARY
+  NAMES uring
+  HINTS ${URING_LIBDIR})
 
 if (NOT URING_INCLUDE_DIR)
   find_path (URING_INCLUDE_DIR