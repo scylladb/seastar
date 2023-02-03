82442cecee0922a97ee27010d0c867ffb1a68a39
Benny Halevy <bhalevy@scylladb.com>
2023-02-03 20:17:53 +0200

    semaphore_test: test_semaphore_units_splitting: use scope to destroy units
    The test case fails with g++ (GCC) 12.2.1 20221121 (Red Hat 12.2.1-4)
    ```
    tests/unit/semaphore_test.cc(0): Entering test case "test_semaphore_units_splitting"
    src/testing/seastar_test.cc(43): info: check true has passed
    tests/unit/semaphore_test.cc(269): info: check units.count() == 3 has passed
    tests/unit/semaphore_test.cc(270): info: check sm.available_units() == 0 has passed
    tests/unit/semaphore_test.cc(272): info: check sm.available_units() == 0 has passed
    tests/unit/semaphore_test.cc(273): info: check units.count() == 2 has passed
    tests/unit/semaphore_test.cc(274): info: check split.count() == 1 has passed
    tests/unit/semaphore_test.cc(276): info: check sm.available_units() == 1 has passed
    tests/unit/semaphore_test.cc(279): fatal error: in "test_semaphore_units_splitting": critical check sm.available_units() == 0 has failed [2 != 0]
    ```
    
    Avoid reassigning `units` after it is destroyed
    by using a scope to control its life.
    
    And refactor the rest of the test out to a different test case.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- tests/unit/semaphore_test.cc
+++ tests/unit/semaphore_test.cc
@@ -263,17 +263,25 @@ SEASTAR_TEST_CASE(test_with_semaphore) {
 }
 
 SEASTAR_THREAD_TEST_CASE(test_semaphore_units_splitting) {
-    auto sm = semaphore(2);
-    auto units = get_units(sm, 2, 1min).get0();
+    auto sm = semaphore(3);
     {
-        BOOST_REQUIRE_EQUAL(units.count(), 2);
+        auto units = get_units(sm, 3, 1min).get0();
+        BOOST_REQUIRE_EQUAL(units.count(), 3);
         BOOST_REQUIRE_EQUAL(sm.available_units(), 0);
-        auto split = units.split(1);
-        BOOST_REQUIRE_EQUAL(sm.available_units(), 0);
+        {
+            auto split = units.split(1);
+            BOOST_REQUIRE_EQUAL(sm.available_units(), 0);
+            BOOST_REQUIRE_EQUAL(units.count(), 2);
+            BOOST_REQUIRE_EQUAL(split.count(), 1);
+        }
+        BOOST_REQUIRE_EQUAL(sm.available_units(), 1);
     }
-    BOOST_REQUIRE_EQUAL(sm.available_units(), 1);
-    units.~semaphore_units();
-    units = get_units(sm, 2, 1min).get0();
+    BOOST_REQUIRE_EQUAL(sm.available_units(), 3);
+}
+
+SEASTAR_THREAD_TEST_CASE(test_semaphore_invalid_units_splitting) {
+    auto sm = semaphore(3);
+    auto units = get_units(sm, 3, 1min).get0();
     BOOST_REQUIRE_EQUAL(sm.available_units(), 0);
     BOOST_REQUIRE_THROW(units.split(10), std::invalid_argument);
     BOOST_REQUIRE_EQUAL(sm.available_units(), 0);
