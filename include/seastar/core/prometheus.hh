ac082ea2d2ae98fa005d433b8f291b3973fcd037
Kefu Chai <kefu.chai@scylladb.com>
2023-03-07 16:21:01 +0800

    prometheus, http: do not expose httpd::* in seastar
    before this change, all symbols enclosed in seastar::httpd
    namespace are exposed by http/request_parser.hh which is generated
    from request_parser.rl, but this is not quite what we intend.
    
    so in this change, `using namespace httpd;` is removed from
    `request_parser.rl`, and all places assuming the existence of
    symbols exposed by `using namespace httpd;` are updated to
    reference `httpd::*`.
    
    Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>
    
    Closes #1536

--- include/seastar/core/prometheus.hh
+++ include/seastar/core/prometheus.hh
@@ -45,8 +45,8 @@ future<> start(httpd::http_server_control& http_server, config ctx);
 /// \defgroup add_prometheus_routes adds a /metrics endpoint that returns prometheus metrics
 ///    in txt format
 /// @{
-future<> add_prometheus_routes(distributed<http_server>& server, config ctx);
-future<> add_prometheus_routes(http_server& server, config ctx);
+future<> add_prometheus_routes(distributed<httpd::http_server>& server, config ctx);
+future<> add_prometheus_routes(httpd::http_server& server, config ctx);
 /// @}
 }
 }
