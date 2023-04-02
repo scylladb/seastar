4e8bddd750c29f311b54c506e0bca4c3a911f970
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-04-02 14:35:12 -0400

    spelling: percentage
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- scripts/stall-analyser.py
+++ scripts/stall-analyser.py
@@ -23,7 +23,7 @@ to emphasize stall backtraces that are frequent and long.
 Each node in the printed output is preceded with [level#index pct%],
 where `level` is the level of that node in the graph (0 are root nodes),
 `index` is the index in the parent node's list of callers/callees, and
-`pct` is the percantage of this link's `total` time relative to
+`pct` is the percentage of this link's `total` time relative to
 its siblings.
 
 When given an executable, addresses are decoding using `addr2line`
