34e2acf5a5d17c0681b79e7be8acb2ef55af6966
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-04-02 14:44:57 -0400

    spelling: shuffling
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- tests/unit/coroutines_test.cc
+++ tests/unit/coroutines_test.cc
@@ -277,7 +277,7 @@ SEASTAR_TEST_CASE(test_preemption) {
         });
 
     // try to preempt 1000 times. 1 should be enough if not for
-    // task queue shaffling in debug mode which may cause co-routine
+    // task queue shuffling in debug mode which may cause co-routine
     // continuation to run first.
     while(preempted < 1000 && !x) {
         preempted += need_preempt(); 
