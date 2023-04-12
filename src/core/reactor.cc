scylladb/seastar

commit f6a2c0125f80d8ef01a8ef89a87b7ce09172ad25

reactor: add run_in_backround and close
run_in_background allows running background tasks
safely, coupled with close() that wait on them
using a gate.

Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

src/core/reactor.cc
@@ -2403,7 +2403,6 @@ void reactor::at_exit(noncopyable_function<future<> ()> func) {
 
 future<> reactor::run_exit_tasks() {
     _stop_requested.broadcast();
-    _stopping = true;
     stop_aio_eventfd_loop();
     return do_for_each(_exit_funcs.rbegin(), _exit_funcs.rend(), [] (auto& func) {
         return func();
@@ -2413,11 +2412,12 @@ future<> reactor::run_exit_tasks() {
 void reactor::stop() {
     assert(_id == 0);
     _smp->cleanup_cpu();
-    if (!_stopping) {
+    if (!std::exchange(_stopping, true)) {
         // Run exit tasks locally and then stop all other engines
         // in the background and wait on semaphore for all to complete.
         // Finally, set _stopped on cpu 0.
-        (void)run_exit_tasks().then([this] {
+        (void)drain().then([this] {
+          return run_exit_tasks().then([this] {
             return do_with(semaphore(0), [this] (semaphore& sem) {
                 // Stop other cpus asynchronously, signal when done.
                 (void)smp::invoke_on_others(0, [] {
@@ -2432,6 +2432,7 @@ void reactor::stop() {
                     _stopped = true;
                 });
             });
+          });
         });
     }
 }
@@ -2963,6 +2964,30 @@ void reactor::add_urgent_task(task* t) noexcept {
     if (was_empty) {
         activate(*q);
     }
+}
+
+void reactor::run_in_background(future<> f) {
+    try {
+        // _backgroud_gate closed in reactor::close()
+        (void)with_gate(_background_gate, [f = std::move(f)] () mutable {
+            return f.handle_exception([] (std::exception_ptr ex) {
+                seastar_logger.warn("Ignored background task failure: {}", std::move(ex));
+            });
+        });
+    } catch (...) {
+        // Swallow gate_closed_exception in particular
+        seastar_logger.error("run_in_background: {}", std::current_exception());
+    }
+}
+
+future<> reactor::drain() {
+    seastar_logger.debug("reactor::drain");
+    return smp::invoke_on_all([] {
+        if (engine()._background_gate.is_closed()) {
+            return make_ready_future<>();
+        }
+        return engine()._background_gate.close();
+    });
 }
 
 void

include/seastar/core/reactor.hh
@@ -52,6 +52,7 @@
 #include <seastar/core/temporary_buffer.hh>
 #include <seastar/core/thread_cputime_clock.hh>
 #include <seastar/core/timer.hh>
+#include <seastar/core/gate.hh>
 #include <seastar/net/api.hh>
 #include <seastar/util/eclipse.hh>
 #include <seastar/util/log.hh>
@@ -359,6 +360,7 @@ private:
     bool _have_aio_fsync = false;
     bool _kernel_page_cache = false;
     std::atomic<bool> _dying{false};
+    gate _background_gate;
 private:
     static std::chrono::nanoseconds calculate_poll_time();
     static void block_notifier(int);
@@ -563,6 +565,16 @@ public:
 
     void add_task(task* t) noexcept;
     void add_urgent_task(task* t) noexcept;
+
+    void run_in_background(future<> f);
+
+    template <typename Func>
+    void run_in_background(Func&& func) {
+        run_in_background(futurize_invoke(std::forward<Func>(func)));
+    }
+
+    // Waits for all background tasks on all shards
+    static future<> drain();
 
     /// Set a handler that will be called when there is no task to execute on cpu.
     /// Handler should do a low priority work.

tests/unit/futures_test.cc
@@ -2025,3 +2025,15 @@ SEASTAR_TEST_CASE(test_make_exception_future) {
 
     return make_ready_future<>();
 }
+
+// Reproduce use-after-free similar to #1514
+SEASTAR_TEST_CASE(test_run_in_background) {
+    engine().run_in_background([] {
+        return sleep(1ms).then([] {
+            return smp::invoke_on_all([] {
+                return sleep(1ms);
+            });
+        });
+    });
+    return make_ready_future<>();
+}

commit f9ddc898beba79aa03a113f100453827a8b59468

reactor: stop _aio_eventfd loop also on shard other than 0
Fixes a regression introduced in f6a2c0125f.

_aio_eventfd loop stops when it's woken up with _stopping set.
Thus stop_aio_eventfd_loop(), whose job is to stop the loop,
has to be preceded by setting _stopping to true.

In f6a2c0125f the assignment to _stopping was moved to a different place,
which accidentally caused it to happen only on shard 0, while
it has to happen on all shards.
(Because if it doesn't happen on some shard, this shard will hang on exit.)

Fixes #1610

src/core/reactor.cc
@@ -2408,6 +2408,7 @@ void reactor::at_exit(noncopyable_function<future<> ()> func) {
 
 future<> reactor::run_exit_tasks() {
     _stop_requested.broadcast();
+    _stopping = true;
     stop_aio_eventfd_loop();
     return do_for_each(_exit_funcs.rbegin(), _exit_funcs.rend(), [] (auto& func) {
         return func();