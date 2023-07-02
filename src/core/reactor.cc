34971f61a8f866bcf7473d976bc4b3ec8137789b
Kefu Chai <kefu.chai@scylladb.com>
2023-07-02 15:55:18 +0800

    reactor: use structured binding when appropriate
    for better readability.
    
    Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>
    
    Closes #1714

--- src/core/reactor.cc
+++ src/core/reactor.cc
@@ -4346,8 +4346,7 @@ void smp::configure(const smp_options& smp_opts, const reactor_options& reactor_
     //     also pre-resize()-d in advance)
 
     auto alloc_io_queues = [&ioq_topology, &disk_config] (shard_id shard) {
-        for (auto& topo : ioq_topology) {
-            auto& io_info = topo.second;
+        for (auto& [dev, io_info] : ioq_topology) {
             auto group_idx = io_info.shard_to_group[shard];
             std::shared_ptr<io_group> group;
 
@@ -4355,25 +4354,25 @@ void smp::configure(const smp_options& smp_opts, const reactor_options& reactor_
                 std::lock_guard _(io_info.lock);
                 auto& iog = io_info.groups[group_idx];
                 if (!iog) {
-                    struct io_queue::config qcfg = disk_config.generate_config(topo.first, io_info.groups.size());
+                    struct io_queue::config qcfg = disk_config.generate_config(dev, io_info.groups.size());
                     iog = std::make_shared<io_group>(std::move(qcfg), io_info.shards_in_group[group_idx]);
-                    seastar_logger.debug("allocate {} IO group with {} queues, dev {}", group_idx, io_info.shards_in_group[group_idx], topo.first);
+                    seastar_logger.debug("allocate {} IO group with {} queues, dev {}", group_idx, io_info.shards_in_group[group_idx], dev);
                 }
                 group = iog;
             }
 
             io_info.queues[shard] = std::make_unique<io_queue>(std::move(group), engine()._io_sink);
-            seastar_logger.debug("attached {} queue to {} IO group, dev {}", shard, group_idx, topo.first);
+            seastar_logger.debug("attached {} queue to {} IO group, dev {}", shard, group_idx, dev);
         }
     };
 
     auto assign_io_queues = [&ioq_topology] (shard_id shard) {
-        for (auto& topo : ioq_topology) {
-            auto queue = std::move(topo.second.queues[shard]);
+        for (auto& [dev, io_info] : ioq_topology) {
+            auto queue = std::move(io_info.queues[shard]);
             assert(queue);
-            engine()._io_queues.emplace(topo.first, std::move(queue));
+            engine()._io_queues.emplace(dev, std::move(queue));
 
-            auto num_io_groups = topo.second.groups.size();
+            auto num_io_groups = io_info.groups.size();
             if (engine()._num_io_groups == 0) {
                 engine()._num_io_groups = num_io_groups;
             } else if (engine()._num_io_groups != num_io_groups) {
