7faec796afd13e816595e8ee374b01bb85447362
Pavel Emelyanov <xemul@scylladb.com>
2023-08-05 17:32:52 +0300

    fair_queue: Config limit min tokens instead of limit_min_{weight|size}
    Same as previous patch -- this makes f.q. configuration a bit simpler
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- src/core/io_queue.cc
+++ src/core/io_queue.cc
@@ -570,8 +570,9 @@ fair_group::config io_group::make_fair_group_config(const io_queue::config& qcfg
     double min_weight = std::min(io_queue::read_request_base_count, qcfg.disk_req_write_to_read_multiplier);
     double min_size = std::min(io_queue::read_request_base_count, qcfg.disk_blocks_write_to_read_multiplier);
     cfg.min_tokens = min_weight / qcfg.req_count_rate + min_size / qcfg.blocks_count_rate;
-    cfg.limit_min_weight = std::max(io_queue::read_request_base_count, qcfg.disk_req_write_to_read_multiplier);
-    cfg.limit_min_size = std::max(io_queue::read_request_base_count, qcfg.disk_blocks_write_to_read_multiplier) * qcfg.block_count_limit_min;
+    double limit_min_weight = std::max(io_queue::read_request_base_count, qcfg.disk_req_write_to_read_multiplier);
+    double limit_min_size = std::max(io_queue::read_request_base_count, qcfg.disk_blocks_write_to_read_multiplier) * qcfg.block_count_limit_min;
+    cfg.limit_min_tokens = limit_min_weight / qcfg.req_count_rate + limit_min_size / qcfg.blocks_count_rate;
     cfg.weight_rate = qcfg.req_count_rate;
     cfg.size_rate = qcfg.blocks_count_rate;
     cfg.rate_factor = qcfg.rate_factor;
