e347da9453f6953becf9009302305dde8cd41f2b
Oren Leiman <oren.leiman@redpanda.com>
2023-09-26 09:21:29 -0700

    net/tls: Introduce cert_info and accessors
    This commit introduces `struct cert_info` which encapsulates useful
    information about TLS certificates, currently:
    
    - The certificate's serial number (as assigned by the signing CA)
    - The certificate's expiration time
    
    Additionally, this commit adds public API on `tls::certificate_credentials`
    to access this information for loaded certificates AND the loaded trust
    list.
    
    The motivation for this change is to give applications the ability to
    monitor and report on certificate and CA lifetimes on demand, the idea
    being that an application may cache serial numbers and expiries when
    credentials are reloaded, serving up a summary of those details until
    the next reload time.
    
    Signed-off-by: Oren Leiman <oren.leiman@redpanda.com>

--- src/net/tls.cc
+++ src/net/tls.cc
@@ -41,6 +41,7 @@ module;
 
 #include <fmt/core.h>
 #include <fmt/ostream.h>
+#include <seastar/util/defer.hh>
 
 #ifdef SEASTAR_MODULE
 module seastar;
@@ -172,6 +173,15 @@ static void gtls_chk(int res) {
     if (res < 0) {
         throw std::system_error(res, tls::error_category());
     }
+}
+
+static sstring extract_x509_serial(gnutls_x509_crt_t cert) {
+    constexpr size_t serial_max = 128;
+    size_t serial_size{serial_max};
+    sstring serial(sstring::initialized_later{}, serial_size);
+    gtls_chk(gnutls_x509_crt_get_serial(cert, serial.data(), &serial_size));
+    serial.resize(serial_size);
+    return serial;
 }
 
 namespace {
@@ -389,6 +399,49 @@ public:
                 gnutls_certificate_set_x509_simple_pkcs12_mem(_creds, &w,
                         gnutls_x509_crt_fmt_t(fmt), password.c_str()));
     }
+    std::vector<cert_info> get_x509_info() const {
+        gnutls_x509_crt_t *crt_list{};
+        unsigned int crt_list_size{};
+        gtls_chk(gnutls_certificate_get_x509_crt(*this, 0, &crt_list, &crt_list_size));
+        auto cleanup = defer([&crt_list, crt_list_size]() noexcept {
+            for (unsigned int i = 0; i < crt_list_size; ++i) {
+                gnutls_x509_crt_deinit(crt_list[i]);
+            }
+            gnutls_free(crt_list);
+        });
+
+        std::vector<cert_info> result;
+        result.reserve(crt_list_size);
+
+        for (unsigned int i = 0; i < crt_list_size; ++i) {
+            cert_info info = {
+                .serial = extract_x509_serial(crt_list[i]),
+                .expiry = gnutls_x509_crt_get_expiration_time(crt_list[i]),
+            };
+            result.emplace_back(std::move(info));
+        }
+        return result;
+    }
+    std::vector<cert_info> get_x509_trust_list_info() const {
+        gnutls_x509_trust_list_t tlist{};
+        gnutls_certificate_get_trust_list(*this, &tlist);
+        gnutls_x509_trust_list_iter_t iter{};
+        gnutls_x509_crt_t cert{};
+
+        // Size not known up front
+        std::vector<cert_info> result;
+        while (GNUTLS_E_REQUESTED_DATA_NOT_AVAILABLE !=
+               gnutls_x509_trust_list_iter_get_ca(tlist, &iter, &cert)) {
+            cert_info info = {
+                .serial = extract_x509_serial(cert),
+                .expiry = gnutls_x509_crt_get_expiration_time(cert),
+            };
+            result.emplace_back(std::move(info));
+            gnutls_x509_crt_deinit(cert);
+        }
+
+        return result;
+    }
     void dh_params(const tls::dh_params& dh) {
 #if GNUTLS_VERSION_NUMBER >= 0x030506
         auto sec_param = dh._impl->sec_param();
@@ -528,6 +581,32 @@ void tls::certificate_credentials::set_priority_string(const sstring& prio) {
 
 void tls::certificate_credentials::set_dn_verification_callback(dn_callback cb) {
     _impl->set_dn_verification_callback(std::move(cb));
+}
+
+std::optional<std::vector<cert_info>> tls::certificate_credentials::get_cert_info() const noexcept {
+    if (_impl == nullptr) {
+        return std::nullopt;
+    }
+
+    try {
+        auto result = _impl->get_x509_info();
+        return result;
+    } catch (...) {
+        return std::nullopt;
+    }
+}
+
+std::optional<std::vector<cert_info>> tls::certificate_credentials::get_trust_list_info() const noexcept {
+    if (_impl == nullptr) {
+        return std::nullopt;
+    }
+
+    try {
+        auto result = _impl->get_x509_trust_list_info();
+        return result;
+    } catch (...) {
+        return std::nullopt;
+    }
 }
 
 tls::server_credentials::server_credentials()
@@ -978,7 +1057,7 @@ void tls::reloadable_credentials<tls::certificate_credentials>::rebuild(const cr
 }
 
 template <>
-const tls::certificate_credentials &tls::reloadable_credentials<tls::certificate_credentials>::as_certificate_credentials() const noexcept {
+const tls::certificate_credentials& tls::reloadable_credentials<tls::certificate_credentials>::as_certificate_credentials() const noexcept {
     return *this;
 }
 
@@ -989,7 +1068,7 @@ void tls::reloadable_credentials<tls::server_credentials>::rebuild(const credent
 }
 
 template <>
-const tls::certificate_credentials &tls::reloadable_credentials<tls::server_credentials>::as_certificate_credentials() const noexcept{
+const tls::certificate_credentials& tls::reloadable_credentials<tls::server_credentials>::as_certificate_credentials() const noexcept{
     return *this;
 }
 
@@ -2042,7 +2121,6 @@ std::ostream& tls::operator<<(std::ostream& os, const subject_alt_name& a) {
     fmt::print(os, "{}", a);
     return os;
 }
-
 
 }
 
