fc0189f912132639acf619c96f7de7386a1b3fbd
Kefu Chai <kefu.chai@scylladb.com>
2023-11-03 21:22:47 +0800

    build: extrat tri-state-option out
    there are use cases where we need to accept options which cannot
    be represented with a boolean. so extract it out.
    
    Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

---
+++ cmake/TriStateOption.cmake
@@ -0,0 +1,25 @@
+# the "option()" defined by CMake represents a boolean. but somtimes, we want
+# to enable/disable it depending on the CMAKE_BUILD_TYPE, if user leaves the
+# option unset.
+function (tri_state_option option)
+  cmake_parse_arguments (
+    parsed_args
+    ""
+    "OUTPUT"
+    "DEFAULT_BUILD_TYPES"
+    ${ARGN})
+  # generic boolean values passed as string, potentially from configure.py
+  set (True_STRING_VALUES "ON" "yes" "Yes" "YES" "true" "True" "TRUE")
+  set (Default_STRING_VALUES "DEFAULT" "default" "Default")
+  if ("${option}" IN_LIST True_STRING_VALUES)
+    set (${parsed_args_OUTPUT} ON PARENT_SCOPE)
+  elseif ("${option}" IN_LIST Default_STRING_VALUES)
+    if ("${CMAKE_BUILD_TYPE}" IN_LIST ${parsed_args_ENABLED_TYPES})
+      set (${parsed_args_OUTPUT} ON PARENT_SCOPE)
+    else ()
+      set (${parsed_args_OUTPUT} OFF PARENT_SCOPE)
+    endif ()
+  else ()
+    set (${parsed_args_OUTPUT} OFF PARENT_SCOPE)
+  endif ()
+endfunction ()
