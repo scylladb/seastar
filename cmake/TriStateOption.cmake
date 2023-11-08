0a6d88ad7843b09e7256d5ca369a4e214de281b9
Kefu Chai <kefu.chai@scylladb.com>
2023-11-08 11:25:54 +0800

    build: enable tri_state_option to return condition
    the condition is used generator expression's conditional expressions,
    this allows us to set configuration on a per-mode basis. on the caller
    site of `tri_state_option()`, the returned "CONDITION" is used to
    conditionally apply the compiling options.
    
    this change allows us to have better support of Multi-Config generators.
    
    Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

--- cmake/TriStateOption.cmake
+++ cmake/TriStateOption.cmake
@@ -5,21 +5,31 @@ function (tri_state_option option)
   cmake_parse_arguments (
     parsed_args
     ""
-    "OUTPUT"
     "DEFAULT_BUILD_TYPES"
+    "CONDITION"
     ${ARGN})
+
+  if (CMAKE_CONFIGURATION_TYPES)
+    set (all_build_types ${CMAKE_CONFIGURATION_TYPES})
+  else ()
+    set (all_build_types ${CMAKE_BUILD_TYPE})
+  endif ()
+
   # generic boolean values passed as string, potentially from configure.py
   set (True_STRING_VALUES "ON" "yes" "Yes" "YES" "true" "True" "TRUE")
   set (Default_STRING_VALUES "DEFAULT" "default" "Default")
+
   if ("${option}" IN_LIST True_STRING_VALUES)
-    set (${parsed_args_OUTPUT} ON PARENT_SCOPE)
+    set (enabled_types ${all_build_types})
   elseif ("${option}" IN_LIST Default_STRING_VALUES)
-    if ("${CMAKE_BUILD_TYPE}" IN_LIST ${parsed_args_ENABLED_TYPES})
-      set (${parsed_args_OUTPUT} ON PARENT_SCOPE)
-    else ()
-      set (${parsed_args_OUTPUT} OFF PARENT_SCOPE)
-    endif ()
+    set (enabled_types ${parsed_args_DEFAULT_BUILD_TYPES})
   else ()
-    set (${parsed_args_OUTPUT} OFF PARENT_SCOPE)
+    set (enabled_types "")
+  endif ()
+
+  if (enabled_types)
+    set (${parsed_args_CONDITION} "$<IN_LIST:$<CONFIG>,${enabled_types}>" PARENT_SCOPE)
+  else ()
+    set (${parsed_args_CONDITION} OFF PARENT_SCOPE)
   endif ()
 endfunction ()
