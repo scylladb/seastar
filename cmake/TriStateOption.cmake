scylladb/seastar

commit 0a6d88ad7843b09e7256d5ca369a4e214de281b9

build: enable tri_state_option to return condition
the condition is used generator expression's conditional expressions,
this allows us to set configuration on a per-mode basis. on the caller
site of `tri_state_option()`, the returned "CONDITION" is used to
conditionally apply the compiling options.

this change allows us to have better support of Multi-Config generators.

Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

cmake/TriStateOption.cmake
@@ -5,21 +5,31 @@ function (tri_state_option option)
   cmake_parse_arguments (
     parsed_args
     ""
-    "OUTPUT"
     "DEFAULT_BUILD_TYPES"
+    "CONDITION"
     ${ARGN})
+
+  if (CMAKE_CONFIGURATION_TYPES)
+    set (all_build_types ${CMAKE_CONFIGURATION_TYPES})
+  else ()
+    set (all_build_types ${CMAKE_BUILD_TYPE})
+  endif ()
+
   # generic boolean values passed as string, potentially from configure.py
   set (True_STRING_VALUES "ON" "yes" "Yes" "YES" "true" "True" "TRUE")
   set (Default_STRING_VALUES "DEFAULT" "default" "Default")
+
   if ("${option}" IN_LIST True_STRING_VALUES)
-    set (${parsed_args_OUTPUT} ON PARENT_SCOPE)
+    set (enabled_types ${all_build_types})
   elseif ("${option}" IN_LIST Default_STRING_VALUES)
-    if ("${CMAKE_BUILD_TYPE}" IN_LIST ${parsed_args_ENABLED_TYPES})
-      set (${parsed_args_OUTPUT} ON PARENT_SCOPE)
-    else ()
-      set (${parsed_args_OUTPUT} OFF PARENT_SCOPE)
-    endif ()
+    set (enabled_types ${parsed_args_DEFAULT_BUILD_TYPES})
   else ()
-    set (${parsed_args_OUTPUT} OFF PARENT_SCOPE)
+    set (enabled_types "")
+  endif ()
+
+  if (enabled_types)
+    set (${parsed_args_CONDITION} "$<IN_LIST:$<CONFIG>,${enabled_types}>" PARENT_SCOPE)
+  else ()
+    set (${parsed_args_CONDITION} OFF PARENT_SCOPE)
   endif ()
 endfunction ()

CMakeLists.txt
@@ -879,10 +879,10 @@ include (TriStateOption)
 
 tri_state_option (${Seastar_ALLOC_FAILURE_INJECTION}
   DEFAULT_BUILD_TYPES "Dev"
-  OUTPUT ALLOC_FAILURE_INJECTION)
-if (ALLOC_FAILURE_INJECTION)
+  CONDITION condition)
+if (condition)
   target_compile_definitions (seastar
-    PUBLIC SEASTAR_ENABLE_ALLOC_FAILURE_INJECTION)
+    PUBLIC $<${condition}:SEASTAR_ENABLE_ALLOC_FAILURE_INJECTION>)
 endif ()
 
 if (Seastar_TASK_BACKTRACE)
@@ -1015,30 +1015,30 @@ endforeach ()
 
 tri_state_option (${Seastar_DEBUG_SHARED_PTR}
   DEFAULT_BUILD_TYPES "Debug" "Sanitize"
-  OUTPUT DEBUG_SHARED_PTR)
-if (DEBUG_SHARED_PTR)
+  CONDITION condition)
+if (condition)
   target_compile_definitions (seastar
     PUBLIC
-      SEASTAR_DEBUG_SHARED_PTR)
+      $<${condition}:SEASTAR_DEBUG_SHARED_PTR>)
 endif ()
 
 include (CheckLibc)
 
 tri_state_option (${Seastar_STACK_GUARDS}
   DEFAULT_BUILD_TYPES "Debug" "Sanitize" "Dev"
-  OUTPUT STACK_GUARDS)
-if(STACK_GUARDS)
+  CONDITION condition)
+if(condition)
   # check for -fstack-clash-protection together with -Werror, because
   # otherwise clang can soft-fail (return 0 but emit a warning) instead.
   seastar_supports_flag ("-fstack-clash-protection -Werror" StackClashProtection_FOUND)
   if (StackClashProtection_FOUND)
     target_compile_options (seastar
       PUBLIC
-      -fstack-clash-protection)
+        $<${condition}:-fstack-clash-protection>)
   endif ()
   target_compile_definitions (seastar
     PRIVATE
-      SEASTAR_THREAD_STACK_GUARDS)
+      $<${condition}:SEASTAR_THREAD_STACK_GUARDS>)
 endif ()
 
 target_compile_definitions (seastar

commit 8f168a36d07aa3b5c02c6e67fca1b9f3c4a17f41

build: let tri_state_option() accept multi build build types
this addresses a regression introduced by 0a6d88ad, which let
DEFAULT_BUILD_TYPE accept a single value instead multiple values,
and let CONDITION accept multiple values. but we do intend to
pass multiple modes as the DEFAULT_BUILD_TYPE option.

so in this change, the arguments passed to `cmake_parse_arguments`
is transposed to correct this regression.

Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

cmake/TriStateOption.cmake
@@ -5,8 +5,8 @@ function (tri_state_option option)
   cmake_parse_arguments (
     parsed_args
     ""
-    "DEFAULT_BUILD_TYPES"
     "CONDITION"
+    "DEFAULT_BUILD_TYPES"
     ${ARGN})
 
   if (CMAKE_CONFIGURATION_TYPES)