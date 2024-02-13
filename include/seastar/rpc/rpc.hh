abc8690b464479cd122fb93e9c59f953b9f2f66b
Michał Chojnowski <michal.chojnowski@scylladb.com>
2024-02-13 12:55:20 +0100

    rpc: fix a deadlock in connection::send()
    If send_entry() throws (e.g. because compression is unable to allocate
    an output buffer), then the intended error handling never gets to run.
    In particular, the connection isn't aborted, and the promise of outgoing_entry
    is never completed -- the connection is stuck.
    
    To fix this, we make send_entry() noexcept.

--- include/seastar/rpc/rpc.hh
+++ include/seastar/rpc/rpc.hh
@@ -307,7 +307,7 @@ protected:
     snd_buf compress(snd_buf buf);
     future<> send_buffer(snd_buf buf);
     future<> send(snd_buf buf, std::optional<rpc_clock_type::time_point> timeout = {}, cancellable* cancel = nullptr);
-    future<> send_entry(outgoing_entry& d);
+    future<> send_entry(outgoing_entry& d) noexcept;
     future<> stop_send_loop(std::exception_ptr ex);
     future<std::optional<rcv_buf>>  read_stream_frame_compressed(input_stream<char>& in);
     bool stream_check_twoway_closed() const noexcept {
