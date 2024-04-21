fd502556e0d4b2f0ea917aa973111a99d0529a65
Brandon Allard <brandon@redpanda.com>
2024-04-21 16:47:25 -0400

    core: add label to io_threaded_fallbacks to categorize operations

--- src/core/thread_pool.hh
+++ src/core/thread_pool.hh
@@ -27,9 +27,34 @@ namespace seastar {
 
 class reactor;
 
+namespace internal {
+// Reasons for why a function had to be submitted to the thread_pool 
+enum class thread_pool_submit_reason : size_t {
+    // Used for aio operations what would block in `io_submit`.
+    aio_fallback,
+    // Used for file operations that don't have non-blocking alternatives.
+    file_operation,
+    // Used for process operations that don't have non-blocking alternatives.
+    process_operation,
+};
+
+class submit_metrics {
+    uint64_t _counters[static_cast<size_t>(thread_pool_submit_reason::process_operation) + 1]{};
+
+public:
+    void record_reason(thread_pool_submit_reason reason) {
+        ++_counters[static_cast<size_t>(reason)];
+    }
+
+    uint64_t count_for(thread_pool_submit_reason reason) const {
+        return _counters[static_cast<size_t>(reason)];
+    }
+};
+} // namespace internal
+
 class thread_pool {
     reactor& _reactor;
-    uint64_t _aio_threaded_fallbacks = 0;
+    internal::submit_metrics metrics;
     syscall_work_queue inter_thread_wq;
     posix_thread _worker_thread;
     std::atomic<bool> _stopped = { false };
@@ -38,11 +63,11 @@ public:
     explicit thread_pool(reactor& r, sstring thread_name);
     ~thread_pool();
     template <typename T, typename Func>
-    future<T> submit(Func func) noexcept {
-        ++_aio_threaded_fallbacks;
+    future<T> submit(internal::thread_pool_submit_reason reason, Func func) noexcept {
+        metrics.record_reason(reason);
         return inter_thread_wq.submit<T>(std::move(func));
     }
-    uint64_t operation_count() const { return _aio_threaded_fallbacks; }
+    uint64_t count(internal::thread_pool_submit_reason r) const { return metrics.count_for(r); }
 
     unsigned complete() { return inter_thread_wq.complete(); }
     // Before we enter interrupt mode, we must make sure that the syscall thread will properly
