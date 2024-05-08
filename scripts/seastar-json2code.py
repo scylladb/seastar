scylladb/seastar

commit 3f534c77d7b47398e582610e03ad08214fce32be

seastar-json2code: use dict.get() when checking allowMultiple
simpler this way. it also silences following warning:

```
visually indented line with same indent as next logical line [E129]
```

from pycodestyle, see also https://peps.python.org/pep-0008/#indentation.

Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

scripts/seastar-json2code.py
@@ -294,14 +294,13 @@ def add_operation(hfile, ccfile, path, oper):
         else:
             fprint(ccfile, "\n,")
         if is_url:
-            fprint(ccfile, '{', '"/', path_param , '", path_description::url_component_type::FIXED_STRING', '}')
+            component_type = 'FIXED_STRING'
+        elif get_parameter_by_name(oper, path_param).get("allowMultiple",
+                                                         False):
+            component_type = 'PARAM_UNTIL_END_OF_PATH'
         else:
-            path_param_type = get_parameter_by_name(oper, path_param)
-            if ("allowMultiple" in path_param_type and
-                path_param_type["allowMultiple"]):
-                fprint(ccfile, '{', '"', path_param , '", path_description::url_component_type::PARAM_UNTIL_END_OF_PATH', '}')
-            else:
-                fprint(ccfile, '{', '"', path_param , '", path_description::url_component_type::PARAM', '}')
+            component_type = 'PARAM'
+        fprint(ccfile, f'{{"/{path_param}", path_description::url_component_type::{component_type}}}')
     fprint(ccfile, '}')
     fprint(ccfile, ',{')
     enum_definitions = ""

commit c56760dde2aac85c23bf218486e9e22c0369da5b

seastar-json2code: fix the path param handling
this change addresses a regression introduced by
3f534c77, which failed to differentiate the path param from
the non-path param, and always prefixed the param with "/".

Fixes 3f534c77d7b47398e582610e03ad08214fce32be
Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

scripts/seastar-json2code.py
@@ -323,13 +323,14 @@ def add_operation(hfile, ccfile, path, oper):
         else:
             fprint(ccfile, "\n,")
         if is_url:
+            path_param = f"/{path_param}"
             component_type = 'FIXED_STRING'
         elif get_parameter_by_name(oper, path_param).get("allowMultiple",
                                                          False):
             component_type = 'PARAM_UNTIL_END_OF_PATH'
         else:
             component_type = 'PARAM'
-        fprint(ccfile, f'{{"/{path_param}", path_description::url_component_type::{component_type}}}')
+        fprint(ccfile, f'{{"{path_param}", path_description::url_component_type::{component_type}}}')
     fprint(ccfile, '}')
     fprint(ccfile, ',{')
     enum_definitions = ""