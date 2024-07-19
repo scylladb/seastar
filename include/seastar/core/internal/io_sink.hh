c2bdd17aef4c1b8cdf2a91679c89ee9a96ef57b9
Travis Downs <travis.downs@redpanda.com>
2024-07-19 15:31:51 -0400

    io_sink: use iteration instead of indexing
    In io_sink::drain we switch to using iteration over the elements
    rather than indexing with operator[]. This is a precursor to
    changing the type of _pending_io to a type which doesn't support
    indexing.
    
    This change should be mostly performance neutral: for circular_buffer
    iteration seems very slightly faster than indexing, but we also
    have to keep tracking the number of drained items separately, so
    perhaps a wash.
    
    Issue #2355.

--- include/seastar/core/internal/io_sink.hh
+++ include/seastar/core/internal/io_sink.hh
@@ -58,19 +58,17 @@ public:
     // draining should try to drain more
     requires std::is_invocable_r<bool, Fn, internal::io_request&, io_completion*>::value
     size_t drain(Fn&& consume) {
-        size_t pending = _pending_io.size();
         size_t drained = 0;
+        auto it = _pending_io.begin();
 
-        while (pending > drained) {
-            pending_io_request& req = _pending_io[drained];
-
+        for (auto end = _pending_io.end(); it != end; ++it, ++drained) {
+            pending_io_request& req = *it;
             if (!consume(req, req._completion)) {
                 break;
             }
-            drained++;
         }
 
-        _pending_io.erase(_pending_io.begin(), _pending_io.begin() + drained);
+        _pending_io.erase(_pending_io.begin(), it);
         return drained;
     }
 };
