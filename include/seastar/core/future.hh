scylladb/seastar

commit 73e3fbc1bc51daa81fb70ba9c69f670f59ce2592

future: Use a new seastar::nested_exception in finally
The std::nested_exception is designed to be inherited from. The
derived type holds the outer exception and the inner exception is
captured by the std::nested_exception constructor which calls
std::current_exception().

That is not a good match for future::finally. We don't have the
derived type anymore, just a std::exception_ptr.

This patch introduces a seastar::nested_exception that just holds two
std::exception_ptr and uses that in finally.

With this finally actually saves both exceptions.

Fixes #760

Signed-off-by: Rafael Ávila de Espíndola <espindola@scylladb.com>
Message-Id: <20200618005650.936695-1-espindola@scylladb.com>

include/seastar/core/future.hh
@@ -43,6 +43,16 @@
 
 namespace seastar {
 
+struct nested_exception : public std::exception {
+    std::exception_ptr inner;
+    std::exception_ptr outer;
+    nested_exception(std::exception_ptr inner, std::exception_ptr outer) noexcept;
+    nested_exception(nested_exception&&) noexcept;
+    nested_exception(const nested_exception&) noexcept;
+    [[noreturn]] void rethrow_nested() const;
+    virtual const char* what() const noexcept override;
+};
+
 /// \defgroup future-module Futures and Promises
 ///
 /// \brief

src/core/future.cc
@@ -148,30 +148,31 @@ void future_state_base::ignore() noexcept {
     }
 }
 
+nested_exception::nested_exception(std::exception_ptr inner, std::exception_ptr outer) noexcept
+    : inner(std::move(inner)), outer(std::move(outer)) {}
+
+nested_exception::nested_exception(nested_exception&&) noexcept = default;
+
+nested_exception::nested_exception(const nested_exception&) noexcept = default;
+
+const char* nested_exception::what() const noexcept {
+    return "seastar::nested_exception";
+}
+
+[[noreturn]] void nested_exception::rethrow_nested() const {
+    std::rethrow_exception(outer);
+}
+
 future_state_base::future_state_base(nested_exception_marker, future_state_base&& old) noexcept {
     if (!old.failed()) {
         new (this) future_state_base(current_exception_future_marker());
         return;
     } else {
-        //
-        // Encapsulate the current exception into the
-        // std::nested_exception because the current libstdc++
-        // implementation has a bug requiring the value of a
-        // std::throw_with_nested() parameter to be of a polymorphic
-        // type.
-        //
-        std::nested_exception f_ex;
-        try {
-            std::rethrow_exception(std::move(old).get_exception());
-        } catch (...) {
-            try {
-                std::throw_with_nested(f_ex);
-            } catch (...) {
-                new (this) future_state_base(current_exception_future_marker());
-                return;
-            }
-        }
-        __builtin_unreachable();
+        std::exception_ptr inner = std::current_exception();
+        std::exception_ptr outer = std::move(old).get_exception();
+        nested_exception nested{std::move(inner), std::move(outer)};
+        std::exception_ptr np = std::make_exception_ptr<nested_exception>(std::move(nested));
+        new (this) future_state_base(std::move(np));
     }
 }
 

tests/unit/futures_test.cc
@@ -190,15 +190,17 @@ SEASTAR_TEST_CASE(test_finally_exception) {
         throw std::runtime_error("foo");
     }).finally([] {
         throw std::runtime_error("bar");
-    }).handle_exception_type([](const std::nested_exception &ex) {
-        // Unfortunately the current implementation loses the outer
-        // exception.
-        auto* outer = dynamic_cast<const std::runtime_error*>(&ex);
-        BOOST_REQUIRE_EQUAL(outer, nullptr);
+    }).handle_exception_type([](const nested_exception &ex) {
+        BOOST_REQUIRE_EQUAL(ex.what(), "seastar::nested_exception");
         try {
-            ex.rethrow_nested();
+            std::rethrow_exception(ex.inner);
         } catch (std::runtime_error &inner) {
             BOOST_REQUIRE_EQUAL(inner.what(), "bar");
+        }
+        try {
+            ex.rethrow_nested();
+        } catch (std::runtime_error &outer) {
+            BOOST_REQUIRE_EQUAL(outer.what(), "foo");
         }
     });
 }

commit 878e06947296fb1d5e1494d555269c71f86e36c9

future: correctly document nested exception type emitted by finally()
Since commit 73e3fbc1bc51 ("future: Use a new seastar::nested_exception in
finally", 2020-06-23), future::finally() propagates a
seastar::nested_exception instance rather than an std::nested_exception
instance, in case both the original future, and the cleanup appended by
finally(), resolve with exceptions. Update the documentation.

Fixes: 73e3fbc1bc51
Signed-off-by: Laszlo Ersek <laszlo.ersek@scylladb.com>

include/seastar/core/future.hh
@@ -1632,7 +1632,7 @@ public:
      * If the original return value or the callback return value is an
      * exceptional future it will be propagated.
      *
-     * If both of them are exceptional - the std::nested_exception exception
+     * If both of them are exceptional - the seastar::nested_exception exception
      * with the callback exception on top and the original future exception
      * nested will be propagated.
      */