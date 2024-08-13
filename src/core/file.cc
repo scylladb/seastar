e37e07db8815d2ad17e72f72c939cc921d498a1e
Patryk Wrobel <patryk.wrobel@scylladb.com>
2024-08-13 11:54:40 +0200

    file.cc: submit discards via the I/O queue
    In the past the discard requests were invoked directly
    from 'posix_file_impl::discard()' function.
    
    In order to route the requests via the I/O queue, instead
    of directly calling 'punch_hole()' function, the requests
    are queued and later executed by the I/O queue.
    
    Signed-off-by: Patryk Wrobel <patryk.wrobel@scylladb.com>

--- src/core/file.cc
+++ src/core/file.cc
@@ -268,7 +268,9 @@ posix_file_impl::fcntl_short(int op, uintptr_t arg) noexcept {
 
 future<>
 posix_file_impl::discard(uint64_t offset, uint64_t length) noexcept {
-    return engine().punch_hole(_fd, offset, length);
+    internal::maybe_priority_class_ref io_priority_class;
+    auto req = internal::io_request::make_discard(_fd, offset, length);
+    return _io_queue.submit_io_discard(internal::priority_class(io_priority_class), length, std::move(req), nullptr).discard_result();
 }
 
 future<>
