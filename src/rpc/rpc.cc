f8b4a51f8b02813a73753178a0aff93553fa23ea
Avi Kivity <avi@scylladb.com>
2024-09-29 21:02:52 +0300

    rpc: drop unnecessaty includes to boost libraries
    Reduces dependency load.

--- src/rpc/rpc.cc
+++ src/rpc/rpc.cc
@@ -8,6 +8,7 @@
 #include <boost/range/adaptor/map.hpp>
 #include <boost/range/adaptor/transformed.hpp>
 #include <boost/algorithm/string.hpp>
+#include <boost/range/numeric.hpp>
 
 #if FMT_VERSION >= 90000
 template <> struct fmt::formatter<seastar::rpc::streaming_domain_type> : fmt::ostream_formatter {};
