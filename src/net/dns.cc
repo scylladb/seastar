scylladb/seastar

commit 7139445c5af424fb27ae7809409f4e10a29d0b9a

dns: use struct typedef for `_channel`
as suggested by its comment:
```c
/* Legacy typedef, don't use, you can't specify "const" */
typedef struct ares_channeldata *ares_channel;

/* Current main channel typedef */
typedef struct ares_channeldata  ares_channel_t;
```

this change has no impact so far, as we don't pass the `&_channel`
around. the constness is more important to the c-ares library. but
let's use the suggested typedef, the name is more consistent with other
typedefs provided by c-ares library.

Signed-off-by: Kefu Chai <kefu.chai@scylladb.com>

src/net/dns.cc
@@ -266,7 +266,7 @@ private:
     socket_map _sockets;
     network_stack & _stack;
 
-    ares_channel _channel = {};
+    ares_channel_t* _channel;
     uint64_t _calls = 0;
     std::chrono::milliseconds _timeout;
     timer<> _timer;

commit 65817e6c8a3c92373e81bfda07da4a574dbec028

dns: Support c-ares before 1.22
ares_channel_t has been introduced in c-ares 1.22 (November 2023). This
version is not yet in most Linux distributions with long version cycles,
e.g. Debian.

A simple and easy to maintain solution is to provide a compatibility
typedef.

This fixes build failure on older c-ares, which was introduced in
7139445c5af424fb27ae7809409f4e10a29d0b9a

src/net/dns.cc
@@ -55,6 +55,11 @@ std::ostream& operator<<(std::ostream& os, const opt_family& f) {
 
 }
 
+#if (ARES_VERSION < 0x011600)
+// ares_channel_t is only present since c-ares 1.22.0 (November 2023)
+typedef struct ares_channeldata ares_channel_t;
+#endif
+
 #if FMT_VERSION >= 90000
 template <> struct fmt::formatter<seastar::net::opt_family> : fmt::ostream_formatter {};
 #endif