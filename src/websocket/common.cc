8e1d790fdbc8e11ad77adef271849b4cf743c7b0
Povilas Kanapickas <povilas@radix.lt>
2024-11-25 17:13:23 +0200

    websocket: Add support for parsing messages coming from server

--- src/websocket/common.cc
+++ src/websocket/common.cc
@@ -63,7 +63,7 @@ future<websocket_parser::consumption_result_t> websocket_parser::operator()(
 
             // https://datatracker.ietf.org/doc/html/rfc6455#section-5.1
             // We must close the connection if data isn't masked.
-            if ((!_header->masked) ||
+            if (((!_header->masked) && (!_is_client)) ||
                 // RSVX must be 0
                 (_header->rsv1 | _header->rsv2 | _header->rsv3) ||
                 // Opcode must be known.
@@ -93,7 +93,9 @@ future<websocket_parser::consumption_result_t> websocket_parser::operator()(
                 _payload_length = consume_be<uint64_t>(input);
             }
 
-            _masking_key = consume_be<uint32_t>(input);
+            if (_header->get_masked()) {
+                _masking_key = consume_be<uint32_t>(input);
+            }
             _buffer = {};
             _state = parsing_state::payload;
         } else {
@@ -130,7 +132,9 @@ future<websocket_parser::consumption_result_t> websocket_parser::operator()(
                           _result.get_write() + _consumed_payload_length);
                 data.trim_front(consumed_bytes);
             }
-            remove_mask(_result, _payload_length);
+            if (!_is_client) {
+                remove_mask(_result, _payload_length);
+            }
             _consumed_payload_length = 0;
             _state = parsing_state::flags_and_payload_data;
             return websocket_parser::stop(std::move(data));
