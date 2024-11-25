5c238d7aa9f561d46bf6acce4dd2c4819aefcd23
Povilas Kanapickas <povilas@radix.lt>
2024-11-25 17:13:17 +0200

    websocket: Extract code not specific to server to separate file
    This will allow to implement Websocket client that reuses existing code.

--- include/seastar/websocket/server.hh
+++ include/seastar/websocket/server.hh
@@ -25,148 +25,16 @@
 #include <functional>
 
 #include <seastar/http/request_parser.hh>
-#include <seastar/core/seastar.hh>
 #include <seastar/core/sstring.hh>
 #include <seastar/net/api.hh>
 #include <seastar/core/gate.hh>
-#include <seastar/core/queue.hh>
 #include <seastar/core/when_all.hh>
+#include <seastar/websocket/common.hh>
 
 namespace seastar::experimental::websocket {
 
-using handler_t = std::function<future<>(input_stream<char>&, output_stream<char>&)>;
-
-class server;
-
-/// \defgroup websocket WebSocket
 /// \addtogroup websocket
 /// @{
-
-/*!
- * \brief an error in handling a WebSocket connection
- */
-class exception : public std::exception {
-    std::string _msg;
-public:
-    exception(std::string_view msg) : _msg(msg) {}
-    virtual const char* what() const noexcept {
-        return _msg.c_str();
-    }
-};
-
-/*!
- * \brief Possible type of a websocket frame.
- */
-enum opcodes {
-    CONTINUATION = 0x0,
-    TEXT = 0x1,
-    BINARY = 0x2,
-    CLOSE = 0x8,
-    PING = 0x9,
-    PONG = 0xA,
-    INVALID = 0xFF,
-};
-
-struct frame_header {
-    static constexpr uint8_t FIN = 7;
-    static constexpr uint8_t RSV1 = 6;
-    static constexpr uint8_t RSV2 = 5;
-    static constexpr uint8_t RSV3 = 4;
-    static constexpr uint8_t MASKED = 7;
-
-    uint8_t fin : 1;
-    uint8_t rsv1 : 1;
-    uint8_t rsv2 : 1;
-    uint8_t rsv3 : 1;
-    uint8_t opcode : 4;
-    uint8_t masked : 1;
-    uint8_t length : 7;
-    frame_header(const char* input) {
-        this->fin = (input[0] >> FIN) & 1;
-        this->rsv1 = (input[0] >> RSV1) & 1;
-        this->rsv2 = (input[0] >> RSV2) & 1;
-        this->rsv3 = (input[0] >> RSV3) & 1;
-        this->opcode = input[0] & 0b1111;
-        this->masked = (input[1] >> MASKED) & 1;
-        this->length = (input[1] & 0b1111111);
-    }
-    // Returns length of the rest of the header.
-    uint64_t get_rest_of_header_length() {
-        size_t next_read_length = sizeof(uint32_t); // Masking key
-        if (length == 126) {
-            next_read_length += sizeof(uint16_t);
-        } else if (length == 127) {
-            next_read_length += sizeof(uint64_t);
-        }
-        return next_read_length;
-    }
-    uint8_t get_fin() {return fin;}
-    uint8_t get_rsv1() {return rsv1;}
-    uint8_t get_rsv2() {return rsv2;}
-    uint8_t get_rsv3() {return rsv3;}
-    uint8_t get_opcode() {return opcode;}
-    uint8_t get_masked() {return masked;}
-    uint8_t get_length() {return length;}
-
-    bool is_opcode_known() {
-        //https://datatracker.ietf.org/doc/html/rfc6455#section-5.1
-        return opcode < 0xA && !(opcode < 0x8 && opcode > 0x2);
-    }
-};
-
-class websocket_parser {
-    enum class parsing_state : uint8_t {
-        flags_and_payload_data,
-        payload_length_and_mask,
-        payload
-    };
-    enum class connection_state : uint8_t {
-        valid,
-        closed,
-        error
-    };
-    using consumption_result_t = consumption_result<char>;
-    using buff_t = temporary_buffer<char>;
-    // What parser is currently doing.
-    parsing_state _state;
-    // State of connection - can be valid, closed or should be closed
-    // due to error.
-    connection_state _cstate;
-    sstring _buffer;
-    std::unique_ptr<frame_header> _header;
-    uint64_t _payload_length;
-    uint64_t _consumed_payload_length = 0;
-    uint32_t _masking_key;
-    buff_t _result;
-
-    static future<consumption_result_t> dont_stop() {
-        return make_ready_future<consumption_result_t>(continue_consuming{});
-    }
-    static future<consumption_result_t> stop(buff_t data) {
-        return make_ready_future<consumption_result_t>(stop_consuming(std::move(data)));
-    }
-    uint64_t remaining_payload_length() const {
-        return _payload_length - _consumed_payload_length;
-    }
-
-    // Removes mask from payload given in p.
-    void remove_mask(buff_t& p, size_t n) {
-        char *payload = p.get_write();
-        for (uint64_t i = 0, j = 0; i < n; ++i, j = (j + 1) % 4) {
-            payload[i] ^= static_cast<char>(((_masking_key << (j * 8)) >> 24));
-        }
-    }
-public:
-    websocket_parser() : _state(parsing_state::flags_and_payload_data),
-                         _cstate(connection_state::valid),
-                         _payload_length(0),
-                         _masking_key(0) {}
-    future<consumption_result_t> operator()(temporary_buffer<char> data);
-    bool is_valid() { return _cstate == connection_state::valid; }
-    bool eof() { return _cstate == connection_state::closed; }
-    opcodes opcode() const;
-    buff_t result();
-};
 
 /*!
  * \brief a WebSocket connection
--- src/websocket/server.cc
+++ src/websocket/server.cc
@@ -32,7 +32,6 @@
 
 namespace seastar::experimental::websocket {
 
-static sstring magic_key_suffix = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
 static sstring http_upgrade_reply_template =
     "HTTP/1.1 101 Switching Protocols\r\n"
     "Upgrade: websocket\r\n"
@@ -41,18 +40,6 @@ static sstring http_upgrade_reply_template =
     "Sec-WebSocket-Accept: ";
 
 static logger wlogger("websocket");
-
-opcodes websocket_parser::opcode() const {
-    if (_header) {
-        return opcodes(_header->opcode);
-    } else {
-        return opcodes::INVALID;
-    }
-}
-
-websocket_parser::buff_t websocket_parser::result() {
-    return std::move(_result);
-}
 
 void server::listen(socket_address addr, listen_options lo) {
     _listeners.push_back(seastar::listen(addr, lo));
@@ -196,106 +183,6 @@ future<> connection::read_http_upgrade_request() {
             return _write_buf.flush();
         });
     });
-}
-
-future<websocket_parser::consumption_result_t> websocket_parser::operator()(
-        temporary_buffer<char> data) {
-    if (data.size() == 0) {
-        // EOF
-        _cstate = connection_state::closed;
-        return websocket_parser::stop(std::move(data));
-    }
-    if (_state == parsing_state::flags_and_payload_data) {
-        if (_buffer.length() + data.size() >= 2) {
-            // _buffer.length() is less than 2 when entering this if body due to how
-            // the rest of code is structured. The else branch will never increase
-            // _buffer.length() to >=2 and other paths to this condition will always
-            // have buffer cleared.
-            assert(_buffer.length() < 2);
-
-            size_t hlen = _buffer.length();
-            _buffer.append(data.get(), 2 - hlen);
-            data.trim_front(2 - hlen);
-            _header = std::make_unique<frame_header>(_buffer.data());
-            _buffer = {};
-
-            // https://datatracker.ietf.org/doc/html/rfc6455#section-5.1
-            // We must close the connection if data isn't masked.
-            if ((!_header->masked) ||
-                    // RSVX must be 0
-                    (_header->rsv1 | _header->rsv2 | _header->rsv3) ||
-                    // Opcode must be known.
-                    (!_header->is_opcode_known())) {
-                _cstate = connection_state::error;
-                return websocket_parser::stop(std::move(data));
-            }
-            _state = parsing_state::payload_length_and_mask;
-        } else {
-            _buffer.append(data.get(), data.size());
-            return websocket_parser::dont_stop();
-        }
-    }
-    if (_state == parsing_state::payload_length_and_mask) {
-        size_t const required_bytes = _header->get_rest_of_header_length();
-        if (_buffer.length() + data.size() >= required_bytes) {
-            if (_buffer.length() < required_bytes) {
-                size_t hlen = _buffer.length();
-                _buffer.append(data.get(), required_bytes - hlen);
-                data.trim_front(required_bytes - hlen);
-            }
-            _payload_length = _header->length;
-            char const *input = _buffer.data();
-            if (_header->length == 126) {
-                _payload_length = consume_be<uint16_t>(input);
-            } else if (_header->length == 127) {
-                _payload_length = consume_be<uint64_t>(input);
-            }
-
-            _masking_key = consume_be<uint32_t>(input);
-            _buffer = {};
-            _state = parsing_state::payload;
-        } else {
-            _buffer.append(data.get(), data.size());
-            return websocket_parser::dont_stop();
-        }
-    }
-    if (_state == parsing_state::payload) {
-        if (data.size() < remaining_payload_length()) {
-            // data has insufficient data to complete the frame - consume data.size() bytes
-            if (_result.empty()) {
-                _result = temporary_buffer<char>(remaining_payload_length());
-                _consumed_payload_length = 0;
-            }
-            std::copy(data.begin(), data.end(), _result.get_write() + _consumed_payload_length);
-            _consumed_payload_length += data.size();
-            return websocket_parser::dont_stop();
-        } else {
-            // data has sufficient data to complete the frame - consume remaining_payload_length()
-            auto consumed_bytes = remaining_payload_length();
-            if (_result.empty()) {
-                // Try to avoid memory copies in case when network packets contain one or more full
-                // websocket frames.
-                if (consumed_bytes == data.size()) {
-                    _result = std::move(data);
-                    data = temporary_buffer<char>(0);
-                } else {
-                    _result = data.share();
-                    _result.trim(consumed_bytes);
-                    data.trim_front(consumed_bytes);
-                }
-            } else {
-                std::copy(data.begin(), data.begin() + consumed_bytes,
-                          _result.get_write() + _consumed_payload_length);
-                data.trim_front(consumed_bytes);
-            }
-            remove_mask(_result, _payload_length);
-            _consumed_payload_length = 0;
-            _state = parsing_state::flags_and_payload_data;
-            return websocket_parser::stop(std::move(data));
-        }
-    }
-    _cstate = connection_state::error;
-    return websocket_parser::stop(std::move(data));
 }
 
 future<> connection::handle_ping() {
