9fd5a1cc67449764aee5f34ce277e7fea8423624
Nadav Har'El <nyh@scylladb.com>
2025-04-08 22:16:05 +0300

    http: generalize reply's Content-Type setting
    For setting a content type on a reply in the HTTP server, we had two
    different methods - one set_mime_type() which accepted only full MIME
    types (e.g., "text/html"), and one set_content_type() which took a
    file extension (e.g, "html") and looked it up in a short map we had,
    like a tiny version of /etc/mime.types.
    
    But some reply methods like write_body() and done() only called
    set_content_type() meaning they could only set content types from that
    short list of known types - and not any general MIME type.
    This forced Seastar users to jump through hoops to use additional mime
    types - for example to call write_body() on a wrong type and then call
    set_mime_type() to override it, or patch the extension map to add more
    and more types to it.
    
    This patch fixes this problem for good: With this patch, we no longer
    have two different reply methods - there is just one, set_content_type().
    The name set_mime_type() remains a deprecated alias.
    The merged set_content_type() can recognize *either* a full-fledged MIME
    type like "text/html", or if there is no slash in the name - it is looked
    up in the list of extensions (e.g., "html").
    
    While doing this, I also changed the set_content_type() from taking
    a "const sstring&" like in the old days, to the more modern
    std::string_view. I believe this will not cause problems for
    applications, as a const sstring& can be implicitly converted into an
    std::string_view so old code should continue to compile.
    
    Fixes #964

--- src/http/mime_types.cc
+++ src/http/mime_types.cc
@@ -10,10 +10,16 @@
 
 #ifdef SEASTAR_MODULE
 module;
+#endif
+
+#include <string_view>
+
+#ifdef SEASTAR_MODULE
 module seastar;
 #else
 #include <seastar/http/mime_types.hh>
 #endif
+
 
 namespace seastar {
 
@@ -40,7 +46,7 @@ struct mapping {
         { "proto", "application/vnd.google.protobuf; proto=io.prometheus.client.MetricFamily; encoding=delimited"},
 };
 
-const char* extension_to_type(const sstring& extension)
+const char* extension_to_type(std::string_view extension)
 {
     for (mapping m : mappings) {
         if (extension == m.extension) {
