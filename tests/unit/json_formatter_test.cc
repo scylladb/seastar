59807f735c2c13c5cadcf0d11894bfb4548cb5b8
Benny Halevy <bhalevy@scylladb.com>
2025-06-11 15:47:38 +0300

    json: json_elements: add generate_array
    Add a helper function to generate a json array gently
    by consuming its items from a coroutine generator.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- tests/unit/json_formatter_test.cc
+++ tests/unit/json_formatter_test.cc
@@ -32,7 +32,7 @@
 using namespace seastar;
 using namespace json;
 
-SEASTAR_TEST_CASE(test_simple_values) {
+SEASTAR_TEST_CASE(test_values) {
     BOOST_CHECK_EQUAL("3", formatter::to_json(3));
     BOOST_CHECK_EQUAL("3", formatter::to_json(3.0));
     BOOST_CHECK_EQUAL("3.5", formatter::to_json(3.5));
@@ -86,6 +86,13 @@ struct object_json : public json_base {
       subject = e.subject;
       values = e.values;
     }
+
+    object_json(object_json&& e) noexcept {
+        register_params();
+        subject = std::move(e.subject);
+        values = std::move(e.values);
+    }
+
 };
 
 SEASTAR_TEST_CASE(test_jsonable) {
@@ -120,7 +127,6 @@ void formatter_check_expected(sstring expected, F f, bool close = true) {
     BOOST_CHECK_EQUAL(expected, result);
 }
 
-
 SEASTAR_THREAD_TEST_CASE(test_stream_range_as_array) {
     sstring expected = R"([{"subject":"1","values":[1]}, {"subject":"2","values":[2]}, {"subject":"3","values":[3]}])";
     formatter_check_expected(expected, [] (auto& out) {
@@ -134,6 +140,30 @@ SEASTAR_THREAD_TEST_CASE(test_stream_range_as_array) {
         mapper(std::move(out)).get();
     }, false);
 }
+
+#if SEASTAR_API_LEVEL >= 8
+
+SEASTAR_THREAD_TEST_CASE(test_generate_array) {
+    sstring expected = R"([{"subject":"1","values":[1]}, {"subject":"2","values":[2]}, {"subject":"3","values":[3]}])";
+    auto generate_json_values = [] () -> coroutine::experimental::generator<object_json> {
+        for (int i : {1,2,3}) {
+            object_json obj;
+            obj.subject = std::to_string(i);
+            obj.values.push(i);
+            co_yield obj;
+        }
+    };
+
+    // Copy the streamer function on purpose to ensure it doesn't cause
+    // use-after-stack-return.
+    auto streamer = generate_array(generate_json_values());
+
+    formatter_check_expected(expected, [streamer = std::move(streamer)] (auto& out) {
+        streamer(std::move(out)).get();
+    }, false);
+}
+
+#endif // SEASTAR_API_LEVEL >= 8
 
 SEASTAR_THREAD_TEST_CASE(formatter_write) {
 
