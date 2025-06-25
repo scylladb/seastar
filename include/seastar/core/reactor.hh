1e638142670db895fe4429db4ef05cdcbb7c9c77
Pavel Emelyanov <xemul@scylladb.com>
2025-06-25 11:31:05 +0300

    reactor: Move pop_active_task_queue() into task_queue_group
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- include/seastar/core/reactor.hh
+++ include/seastar/core/reactor.hh
@@ -302,6 +302,7 @@ private:
         void activate(task_queue*);
 
         void insert_active_task_queue(task_queue* tq);
+        task_queue* pop_active_task_queue(sched_clock::time_point now);
     };
 
     std::array<std::unique_ptr<task_queue>, max_scheduling_groups()> _task_queues;
@@ -414,7 +415,6 @@ private:
     bool have_more_tasks() const;
     bool posix_reuseport_detect();
     void run_some_tasks();
-    task_queue* pop_active_task_queue(sched_clock::time_point now);
     void insert_activating_task_queues();
     void account_runtime(task_queue& tq, sched_clock::duration runtime);
     future<> rename_scheduling_group_specific_data(scheduling_group sg);
