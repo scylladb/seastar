1f44f3821972af30cf5a50f13baf338e917caa5b
Pavel Emelyanov <xemul@scylladb.com>
2025-06-26 17:28:20 +0300

    reactor: Make task_queue_group be sched_entity too
    Finally, the task_queue_group can be schedulable by upper groups. For
    now there's only one group -- the reactor root one. It's initialized
    without parent and not to let it "activate" itself after child group
    wakeup, mark it as always active.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- src/core/reactor.cc
+++ src/core/reactor.cc
@@ -939,7 +939,8 @@ static sstring shorten_name(const sstring& name, size_t length) {
     return shortname;
 }
 
-reactor::task_queue_group::task_queue_group()
+reactor::task_queue_group::task_queue_group(task_queue_group* p, float shares)
+        : sched_entity(p, shares)
 {
 }
 
@@ -1054,6 +1055,7 @@ reactor::reactor(std::shared_ptr<seastar::smp> smp, alien::instance& alien, unsi
     , _task_quota_timer(file_desc::timerfd_create(CLOCK_MONOTONIC, TFD_CLOEXEC))
     , _id(id)
     , _cpu_stall_detector(internal::make_cpu_stall_detector())
+    , _cpu_sched(nullptr, 0)
     , _thread_pool(std::make_unique<thread_pool>(*this, seastar::format("syscall-{}", id))) {
     /*
      * The _backend assignment is here, not on the initialization list as
