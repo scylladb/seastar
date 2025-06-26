a169fce1a97a600aaebc84526c3de0f72162780f
Pavel Emelyanov <xemul@scylladb.com>
2025-06-26 12:28:57 +0300

    reactor: Move active bit to sched_entity
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- include/seastar/core/reactor.hh
+++ include/seastar/core/reactor.hh
@@ -272,11 +272,11 @@ private:
         int64_t _vruntime = 0;
         float _shares;
         int64_t _reciprocal_shares_times_2_power_32;
+        bool _active = false;
     };
 
     struct task_queue final : public sched_entity {
         explicit task_queue(unsigned id, sstring name, sstring shortname, float shares);
-        bool _active = false;
         const uint8_t _id;
         sched_clock::time_point _ts; // to help calculating wait/starve-times
         sched_clock::duration _runtime = {};
