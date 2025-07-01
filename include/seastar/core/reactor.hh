7e34790fa435dab6067e2a3967ba17e8e4f141a5
Pavel Emelyanov <xemul@scylladb.com>
2025-07-01 13:28:47 +0300

    reactor: Move set_shares() to sched_entity()
    Shares are already there, time to move set_shares() next to it, so that
    task_queue_groups also have a way to update it.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- include/seastar/core/reactor.hh
+++ include/seastar/core/reactor.hh
@@ -279,6 +279,8 @@ private:
         sched_clock::duration _starvetime = {};
         sched_clock::duration _time_spent_on_task_quota_violations = {};
 
+        void set_shares(float shares) noexcept;
+
         int64_t to_vruntime(sched_clock::duration runtime) const;
     };
 
@@ -292,7 +294,6 @@ private:
         // chars are used.
         static constexpr size_t shortname_size = 4;
         sstring _shortname;
-        void set_shares(float shares) noexcept;
         struct indirect_compare;
         seastar::metrics::metric_groups _metrics;
         bool run_tasks();
