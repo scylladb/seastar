scylladb/seastar

commit 7a91062375c9b13456713708f10365a118a0d999

treewide: replace assert with SEASTAR_ASSERT
This prevents the need to undefined NDEBUG in seastar source and
prevents headers from conditionally excluding these checks.

src/util/log.cc
@@ -123,6 +123,11 @@ namespace seastar {
 
 namespace internal {
 
+[[noreturn]] void assert_fail(const char* msg, const char* file, int line, const char* func) {
+    printf("%s:%u: %s: Assertion `%s` failed.\n", file, line, func, msg);
+    __builtin_trap();
+}
+
 void log_buf::free_buffer() noexcept {
     if (_own_buf) {
         delete[] _begin;

commit 2279a2872d1ca5932d64183730f6cce0f7818bec

util: make SEASTAR_ASSERT() failure generate SIGABRT
Recently commit 7a91062 introduced SEASTAR_ASSERT to replace assert()
and BOOST_ASSERT() - but unlike the latters it ignores NDEBUG and
unconditionally checks the condition.

When the new SEASTAR_ASSERT() fails, it called __builtin_trap(), which
generates a SIGILL (Illegal Instruction) signal. This is surprising and
counter-intutive, because C / C++ developers are used to assertion
failures always generating SIGABRT, not SIGILL.

Fix this by calling std::terminate() instead of __builtin_trap().

Fixes #2832

Signed-off-by: Nadav Har'El <nyh@scylladb.com>

src/util/log.cc
@@ -125,7 +125,7 @@ namespace internal {
 
 [[noreturn]] void assert_fail(const char* msg, const char* file, int line, const char* func) {
     printf("%s:%u: %s: Assertion `%s` failed.\n", file, line, func, msg);
-    __builtin_trap();
+    std::terminate();
 }
 
 void log_buf::free_buffer() noexcept {