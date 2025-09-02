0272004169a3e1d50b92228adddbb4476bfe773c
Ernest Zaslavsky <ernest.zaslavsky@scylladb.com>
2025-09-02 12:02:21 +0300

    http: initial implementation of `retry_strategy`
    Add basic `retry_strategy` to inform the HTTP client whether to retry a
    request and determine retry timing. Includes support for delay before
    retrying.

---
+++ src/http/retry_strategy.cc
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2025-present ScyllaDB
+ */
+
+/*
+ * SPDX-License-Identifier: LicenseRef-ScyllaDB-Source-Available-1.0
+ */
+
+#ifdef SEASTAR_MODULE
+module;
+#endif
+
+#include <chrono>
+#include <coroutine>
+#include <gnutls/gnutls.h>
+
+#ifdef SEASTAR_MODULE
+module seastar;
+#else
+#include <seastar/http/exception.hh>
+#include <seastar/http/retry_strategy.hh>
+#include <seastar/util/short_streams.hh>
+
+#endif
+
+using namespace std::chrono_literals;
+
+namespace seastar {
+extern logger http_log;
+
+namespace http::experimental {
+logger rs_logger("default_http_retry_strategy");
+
+extern bool is_retryable_exception(std::exception_ptr ex);
+
+default_retry_strategy::default_retry_strategy(unsigned max_retries)
+    : _max_retries(max_retries) {
+}
+
+default_retry_strategy::default_retry_strategy()
+    : default_retry_strategy(1) {
+}
+
+future<bool> default_retry_strategy::should_retry(std::exception_ptr error, unsigned attempted_retries) const {
+    if (attempted_retries >= get_max_retries()) {
+        rs_logger.warn("Retries exhausted. Retry# {}", attempted_retries);
+        co_return false;
+    }
+
+    co_return is_retryable_exception(error);
+}
+
+unsigned default_retry_strategy::get_max_retries() const noexcept {
+    return _max_retries;
+}
+
+future<bool> no_retry_strategy::should_retry(std::exception_ptr error, unsigned attempted_retries) const {
+    return make_ready_future<bool>(false);
+}
+
+unsigned no_retry_strategy::get_max_retries() const noexcept {
+    return 0;
+}
+}
+}
