7de5bd3d1939ce370d5fb2d85c12a9f908f935a3
Pavel Emelyanov <xemul@scylladb.com>
2025-09-23 17:47:08 +0300

    io_queue: Destroy priority class data with scheduling group
    When a scheduling group is destroyed, the IO queue's priority_class_data
    that corresponds to it stays intact. It's not actually a leak, as when
    the reactor is destroyed all class data objects are collected.
    
    However, if another scheduling group under the same ID is created, the
    IO queue will re-use the dangling priority class for the newly created
    group, but its name and shares won't match the new group until rename
    or shares update. And it's not nice that IO class remains alive anyway.
    
    Since scheduling group destruction must happen after all tasks from it
    are completed and picked up, the class destruction code may also assume
    that no queued or in-flight requests exist.
    
    Test included.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- src/core/reactor.cc
+++ src/core/reactor.cc
@@ -4988,6 +4988,9 @@ reactor::destroy_scheduling_group(scheduling_group sg) noexcept {
     }).then( [this, sg] () {
         get_sg_data(sg).queue_is_initialized = false;
         _task_queues[sg._id].reset();
+        for (auto&& queue : _io_queues) {
+            queue.second->destroy_priority_class(internal::priority_class(sg));
+        }
     });
 
 }
