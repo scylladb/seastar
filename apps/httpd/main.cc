8e262fc27e279c918f731b62f6caf5144f353e16
Avi Kivity <avi@scylladb.com>
2025-10-29 21:46:35 +0200

    apps: httpd: add --load-balancing-algorithm
    Useful for, well, testing load balancing algorithms. Can be used
    with curl's --local-port option, and a local haproxy to test
    the proxy protocol.

--- apps/httpd/main.cc
+++ apps/httpd/main.cc
@@ -34,6 +34,7 @@
 #include <seastar/core/print.hh>
 #include <seastar/net/inet_address.hh>
 #include <seastar/util/defer.hh>
+#include <seastar/net/api.hh>
 #include "../lib/stop_signal.hh"
 
 namespace bpo = boost::program_options;
@@ -80,6 +81,9 @@ int main(int ac, char** av) {
     app_template app;
 
     app.add_options()("port", bpo::value<uint16_t>()->default_value(10000), "HTTP Server port");
+    app.add_options()("load-balancing-algorithm",
+            bpo::value<std::string>()->default_value("connection_distribution"),
+            "Load balancing algorithm: connection_distribution, port, proxy_protocol_v2_and_port, fixed");
     app.add_options()("prometheus_port", bpo::value<uint16_t>()->default_value(9180), "Prometheus port. Set to zero in order to disable.");
     app.add_options()("prometheus_address", bpo::value<sstring>()->default_value("0.0.0.0"), "Prometheus address");
     app.add_options()("prometheus_prefix", bpo::value<sstring>()->default_value("seastar_httpd"), "Prometheus metrics prefix");
@@ -132,7 +136,24 @@ int main(int ac, char** av) {
             server->set_routes(set_routes).get();
             server->set_routes([rb](routes& r){rb->set_api_doc(r);}).get();
             server->set_routes([rb](routes& r) {rb->register_function(r, "demo", "hello world application");}).get();
-            server->listen(port).get();
+
+            auto lba_str = config["load-balancing-algorithm"].as<std::string>();
+            server_socket::load_balancing_algorithm lba;
+            if (lba_str == "connection_distribution") {
+                lba = server_socket::load_balancing_algorithm::connection_distribution;
+            } else if (lba_str == "port") {
+                lba = server_socket::load_balancing_algorithm::port;
+            } else if (lba_str == "proxy_protocol_v2_and_port") {
+                lba = server_socket::load_balancing_algorithm::proxy_protocol_v2_and_port;
+            } else if (lba_str == "fixed") {
+                lba = server_socket::load_balancing_algorithm::fixed;
+            } else {
+                throw std::runtime_error("Invalid load balancing algorithm: " + lba_str);
+            }
+
+            listen_options lo;
+            lo.lba = lba;
+            server->listen(socket_address{net::inet_address{}, port}, lo).get();
 
             std::cout << "Seastar HTTP server listening on port " << port << " ...\n";
 
