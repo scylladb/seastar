154bbb9f8782b5d881653de16d37d04757573aaa
Lewis Chan <baiwfg2@gmail.com>
2025-12-24 19:47:54 +0800

    demos:improve the output of demo_with_io_intent() in file_demo
    During my test, the assertion when `cancelled`=false is never
    checked, so add a simple sleep call betwen f2 request and
    cancel action, so that f2 has chance of being ready before
    request with io_intent is cancelled. In this case `cancelled`
    is still false
    
    Interestingly even we give wait 0ms, f2 is still fulfilled.
    I suppose that's because even sleep with 0 also triggers one
    reactor scheduling, letting io request get the chance to run
    
    Signed-off-by: Lewis Chan <baiwfg2@gmail.com>
    
    Closes scylladb/seastar#3178

--- demos/file_demo.cc
+++ demos/file_demo.cc
@@ -35,12 +35,15 @@
 #include <seastar/core/sstring.hh>
 #include <seastar/core/temporary_buffer.hh>
 #include <seastar/core/loop.hh>
+#include <seastar/core/sleep.hh>
 #include <seastar/core/io_intent.hh>
 #include <seastar/util/assert.hh>
 #include <seastar/util/log.hh>
 #include <seastar/util/tmp_file.hh>
 
 using namespace seastar;
+namespace bpo = boost::program_options;
+using namespace std::chrono_literals;
 
 constexpr size_t aligned_size = 4096;
 
@@ -169,9 +172,9 @@ future<> demo_with_file_close_on_failure() {
 
 static constexpr size_t half_aligned_size = aligned_size / 2;
 
-future<> demo_with_io_intent() {
+future<> demo_with_io_intent(std::chrono::duration<double> d) {
     fmt::print("\nDemonstrating demo_with_io_intent():\n");
-    return tmp_dir::do_with_thread([] (tmp_dir& t) {
+    return tmp_dir::do_with_thread([=] (tmp_dir& t) {
         sstring filename = (t.get_path() / "testfile.tmp").native();
         auto f = open_file_dma(filename, open_flags::rw | open_flags::create).get();
 
@@ -192,6 +195,10 @@ future<> demo_with_io_intent() {
         auto f1 = f.dma_write(0, wbuf_n.get(), half_aligned_size);
         auto f2 = f.dma_write(half_aligned_size, wbuf_n.get() + half_aligned_size, half_aligned_size, &intent);
 
+        if (d >= std::chrono::duration<double>::zero()) {
+            sleep(std::chrono::duration_cast<std::chrono::microseconds>(d)).get();
+        }
+
         fmt::print("  cancel the 2nd overwriting\n");
         intent.cancel();
 
@@ -199,17 +206,18 @@ future<> demo_with_io_intent() {
         f1.get();
 
         bool cancelled = false;
+        size_t nwritten = 0;
         try {
-            f2.get();
+            nwritten = f2.get();
             // The file::dma_write doesn't preemt, but if it
             // suddenly will, the 2nd write will pass before
             // the intent would be cancelled
-            fmt::print("    2nd write won the race with cancellation\n");
+            fmt::print("    2nd write won the race with cancellation, written: {}\n", nwritten);
         } catch (cancelled_error& ex) {
             cancelled = true;
         }
 
-        fmt::print("  verifying data...\n");
+        fmt::print("  cancel: {}, verifying data...\n", cancelled);
         auto rbuf = allocate_aligned_buffer<unsigned char>(aligned_size, aligned_size);
         f.dma_read(0, rbuf.get(), aligned_size).get();
 
@@ -221,17 +229,22 @@ future<> demo_with_io_intent() {
             SEASTAR_ASSERT(!memcmp(rbuf.get() + half_aligned_size, wbuf.get() + half_aligned_size, half_aligned_size));
         } else {
             // ... or with new if the cancellation didn't happen
-            SEASTAR_ASSERT(!memcmp(rbuf.get() + half_aligned_size, wbuf.get() + half_aligned_size, half_aligned_size));
+            SEASTAR_ASSERT(!memcmp(rbuf.get() + half_aligned_size, wbuf_n.get() + half_aligned_size, nwritten));
         }
     });
 }
 
 int main(int ac, char** av) {
     app_template app;
-    return app.run(ac, av, [] {
-        return demo_with_file().then([] {
-            return demo_with_file_close_on_failure().then([] {
-                return demo_with_io_intent();
+    app.add_options()
+            ("wait", bpo::value<std::int64_t>()->default_value(-1), "wait milliseconds before cancelling the IO intent, not sleep by default")
+    ;
+    return app.run(ac, av, [&] {
+        auto&& config = app.configuration();
+        auto duration = std::chrono::duration<double>(config["wait"].as<std::int64_t>() * 1ms);
+        return demo_with_file().then([=] {
+            return demo_with_file_close_on_failure().then([=] {
+                return demo_with_io_intent(duration);
             });
         });
     });
