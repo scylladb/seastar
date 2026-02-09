309ab6791c8b324d1fb69dd0eaff6ccfe9e62167
Pavel Emelyanov <xemul@scylladb.com>
2026-02-09 19:04:30 +0300

    reactor: Move _dying bit to epoll backend
    It's only used by this backend to stop task quota timer thread.
    No need to keep it on generic reactor part.
    
    Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

--- include/seastar/core/reactor.hh
+++ include/seastar/core/reactor.hh
@@ -364,7 +364,6 @@ private:
     sched_clock::time_point _start_time = now();
     output_stream<char>::batch_flush_list_t _flush_batching;
     std::atomic<bool> _sleeping alignas(seastar::cache_line_size){0};
-    std::atomic<bool> _dying{false};
     gate _background_gate;
 
     inline auto& get_sg_data(const scheduling_group& sg) {
