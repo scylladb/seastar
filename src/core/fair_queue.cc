scylladb/seastar

commit ab61e44b04d6753e078b6a26c958b8154cc9c5e7

fair_queue: Implement hierarchical priority_entry::pop_front()
The method extracts the front entry from the queue. It's supposed to be
called after top() one, and just grabs the front class_data and pops its
front request. Also it updates the front class_data's accumulator value
and pushes the class back in priority_queue, so that it gets re-sorted
according to its new accumulator.

The hierarchical version does the same for several levels of the tree,
starting from root and ending at the leaf entry (the class_data thing).
The difficulty here is that the request capacity to update the
accumulator with is only present in the leaf node, but pop_front()
starts at the top. To make it work, the new algo first pops the front
entry of the group and gets the request capacity from it as return
value. Then it updates its priority_queue and returns the capacity up
the stack to upper group.

This also slightly changes the way class is pop-ed-and-push-ed. Before
the change the pop_front() used generic pop/push helpers that also
updated the "queued" bit for class. The patched implementation
open-codes the pop/push helpers and doesn't update the "queued" bit if
the entry in question remains in the queue.

Also, the class_data::pure_accumulated and fair_queue::queued_capacity
counters update is moved to respective levels, as they are not present
in the generic priority_entry.

Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

src/core/fair_queue.cc
@@ -114,10 +114,18 @@ public:
     }
 
     fair_queue_entry* top() override;
+    std::pair<bool, capacity_t> pop_front() override;
 };
 
 fair_queue_entry* fair_queue::priority_class_data::top() {
     return (_plugged && !_queue.empty()) ? &_queue.front() : nullptr;
+}
+
+std::pair<bool, fair_queue_entry::capacity_t> fair_queue::priority_class_data::pop_front() {
+    auto req_cap = _queue.front()._capacity;
+    _pure_accumulated += req_cap;
+    _queue.pop_front();
+    return std::make_pair(_queue.empty(), req_cap);
 }
 
 bool fair_queue::class_compare::operator() (const priority_entry_ptr& lhs, const priority_entry_ptr& rhs) const noexcept {
@@ -269,30 +277,35 @@ fair_queue_entry* fair_queue::priority_class_group_data::top() {
 }
 
 void fair_queue::pop_front() {
-    auto& h = *reinterpret_cast<priority_class_data*>(_root._children.top());
+    auto [empty, req_cap] = _root.pop_front();
+    _queued_capacity += req_cap;
+}
 
-    _root._last_accumulated = std::max(h._accumulated, _root._last_accumulated);
-    pop_priority_class(h);
+std::pair<bool, fair_queue_entry::capacity_t> fair_queue::priority_class_group_data::pop_front() {
+    auto& h = *_children.top();
+    _children.pop();
 
-    auto& req = h._queue.front();
-    h._queue.pop_front();
+    auto [empty, req_cap] = h.pop_front();
+
+    _last_accumulated = std::max(h._accumulated, _last_accumulated);
 
     // Usually the cost of request is tens to hundreeds of thousands. However, for
     // unrestricted queue it can be as low as 2k. With large enough shares this
     // has chances to be translated into zero cost which, in turn, will make the
     // class show no progress and monopolize the queue.
-    auto req_cap = req._capacity;
     auto req_cost  = std::max(req_cap / h._shares, (capacity_t)1);
     h._accumulated += req_cost;
-    h._pure_accumulated += req_cap;
-    _queued_capacity -= req_cap;
 
     // signed overflow check to make push_priority_class_from_idle math work
     SEASTAR_ASSERT(h._accumulated < std::numeric_limits<signed_capacity_t>::max() - req_cost);
 
-    if (!h._queue.empty()) {
-        push_priority_class(h);
+    if (empty) {
+        h._queued = false;
+    } else {
+        _children.push(&h);
     }
+
+    return std::make_pair(_children.empty(), req_cap);
 }
 
 }

include/seastar/core/fair_queue.hh
@@ -176,6 +176,7 @@ private:
         {}
     public:
         virtual fair_queue_entry* top() = 0;
+        virtual std::pair<bool, capacity_t> pop_front() = 0;
     };
 
     using clock_type = std::chrono::steady_clock;
@@ -212,6 +213,7 @@ private:
         priority_class_group_data(priority_class_group_data&&) = delete;
 
         fair_queue_entry* top() override;
+        std::pair<bool, capacity_t> pop_front() override;
 
         void reserve(size_t len) {
             _children.reserve(len);

commit 01a4c6bbd93d37e7ba7c037b945d34ffe95b5edc

fair_queue: Subtract from _queued_capacity on pop_front()
When a request is withdrawn from fair queue, the total queued capacity
counter should be decreased, not increased.

Bug introduced by ab61e44b.

Signed-off-by: Pavel Emelyanov <xemul@scylladb.com>

src/core/fair_queue.cc
@@ -274,7 +274,7 @@ fair_queue_entry* fair_queue::priority_class_group_data::top() {
 
 void fair_queue::pop_front() {
     auto [empty, req_cap] = _root.pop_front();
-    _queued_capacity += req_cap;
+    _queued_capacity -= req_cap;
 }
 
 std::pair<bool, fair_queue_entry::capacity_t> fair_queue::priority_class_group_data::pop_front() {