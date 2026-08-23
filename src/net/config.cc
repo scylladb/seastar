9b21495d93148267d086863581c424ccd96c8a6a
Łukasz Kurowski <crackcomm@gmail.com>
2026-08-23 04:55:36 +0200

    net,rpc: use std views instead of boost range utilities Replace boost::adaptors/map_keys and make_iterator_range with std::views::keys and std::ranges::subrange; drop dead boost includes in tls_gnutls.cc and net/config.cc.

--- src/net/config.cc
+++ src/net/config.cc
@@ -20,19 +20,16 @@
  */
 
 
-#include <boost/algorithm/cxx11/any_of.hpp>
-#include <boost/algorithm/cxx11/none_of.hpp>
-#include <boost/next_prior.hpp>
 #include <yaml-cpp/yaml.h>
 #include <istream>
 #include <optional>
+#include <ranges>
 #include <unordered_map>
 #include <string>
 
 #include <seastar/net/config.hh>
 #include <seastar/core/format.hh>
 
-using namespace boost::algorithm;
 
 namespace seastar {
 namespace net {
@@ -68,7 +65,7 @@ namespace net {
         }
 
         // check each device: when dhcp is off, all of ip, gw, nm must be specified
-        if (any_of(device_configs, [](std::pair<std::string, device_config> p) {
+        if (std::ranges::any_of(device_configs, [](std::pair<std::string, device_config> p) {
                 return !p.second.ip_cfg.dhcp
                     && (p.second.ip_cfg.ip.empty() || p.second.ip_cfg.gateway.empty()
                            || p.second.ip_cfg.netmask.empty());
@@ -78,7 +75,7 @@ namespace net {
         }
 
         // check each device: dhcp cannot be used together with static ip/gw/nm
-        if (any_of(device_configs, [](std::pair<std::string, device_config> p) {
+        if (std::ranges::any_of(device_configs, [](std::pair<std::string, device_config> p) {
                 return p.second.ip_cfg.dhcp
                     && (!p.second.ip_cfg.ip.empty() || !p.second.ip_cfg.gateway.empty()
                            || !p.second.ip_cfg.netmask.empty());
@@ -99,7 +96,7 @@ struct convert<seastar::net::device_config> {
         // test for unsupported key
 
         for (auto&& item : node) {
-            if (none_of(seastar::net::config_keys, [&item](std::string s) {
+            if (std::ranges::none_of(seastar::net::config_keys, [&item](std::string s) {
                     return s == item.first.as<std::string>();
                 })) {
                 throw seastar::net::config_exception(
